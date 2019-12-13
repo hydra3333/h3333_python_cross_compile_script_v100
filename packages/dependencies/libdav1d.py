2b645fee98da4512b2a3666c08aaac1d477fb0b9
hydra3333 <hydra3333@gmail.com>
2019-12-14 01:28:13 +1030

    initial updates to libdav1d

--- packages/dependencies/libdav1d.py
+++ packages/dependencies/libdav1d.py
@@ -9,12 +9,15 @@
 		'--prefix={target_prefix} '
 		'--libdir={target_prefix}/lib '
 		'--default-library=static '
-		'--buildtype=plain '
+		#'--buildtype=plain # 2019.12.13
 		'--backend=ninja '
 		'-Denable_tests=false '
 		'-Denable_tools=false '
 		'--buildtype=release '
 		'--cross-file={meson_env_file} ./ ..'
-  ,
+    ,
+  	'run_post_patch' : [ # 2019.12.13
+		'sed -i.bak \'s/sdl2_dependency.found()/false/\' ../tools/meson.build'   # 2019.12.13 # 2019.08.07 turn off building of tool dav1dplay.exe since it won't link. A Nod to JB MABS. 
+	],  # 2019.12.13
 	'_info' : { 'version' : None, 'fancy_name' : 'dav1d (library)' },
 }
