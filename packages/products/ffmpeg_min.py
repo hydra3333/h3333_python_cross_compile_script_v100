5b6368c03084073e450e4812afc5d3709325156e
hydra3333 <hydra3333@gmail.com>
2019-12-13 18:33:04 +1030

    initial ffmpeg configs and add OpenCL stuff; 3 of 3

--- packages/products/ffmpeg_min.py
+++ packages/products/ffmpeg_min.py
@@ -4,5 +4,5 @@
 	'rename_folder' : 'ffmpeg_min',
 	'configure_options' : '!VAR(ffmpeg_min_config)VAR! !VAR(ffmpeg_nonfree)VAR! --prefix={output_prefix}/ffmpeg_min_git.installed --enable-sdl --disable-shared --enable-static',
 	'depends_on' : [ 'ffmpeg_depends_min', 'ffmpeg_depends_nonfree', 'sdl2'],
-	'_info' : { 'version' : None, 'fancy_name' : 'ffmpeg (static, min)' },
+	'_info' : { 'version' : None, 'fancy_name' : 'ffmpeg (static, min) free with OpenCL' }, # 2019.12.13 clarify it means free
 }
