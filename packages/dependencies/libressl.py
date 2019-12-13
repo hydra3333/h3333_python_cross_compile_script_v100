5eab19fe5770521864c78b15dbc797044d0c9eb0
hydra3333 <hydra3333@gmail.com>
2019-12-13 14:21:57 +1030

    libressl to use local patch (if uncommented)

--- packages/dependencies/libressl.py
+++ packages/dependencies/libressl.py
@@ -6,7 +6,7 @@
 	# 'patches' : [
 		# ( 'https://raw.githubusercontent.com/shinchiro/mpv-winbuild-cmake/master/packages/libressl-0001-ignore-compiling-test-and-man-module.patch', '-p1' ),
 		# ( 'https://raw.githubusercontent.com/shinchiro/mpv-winbuild-cmake/master/packages/libressl-0002-tls-revert-Add-tls-tls_keypair.c-commit.patch', '-p1' ),
-		# ( 'https://raw.githubusercontent.com/DeadSix27/misc_patches/master/libressl/libressl-0001-rename-timegm-for-mingw-compat.patch', '-p1' ),
+		# ( 'https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/patches/ressl/libressl-0001-rename-timegm-for-mingw-compat.patch', '-p1' ),
 	# ],
 	'_info' : { 'version' : None, 'fancy_name' : 'libressl' },
 }
