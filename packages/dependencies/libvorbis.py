48bec48037708f852df7a2fd4cc3b204aa165f48
hydra3333 <hydra3333@gmail.com>
2019-12-14 09:11:50 +1030

    add comments to libvorbis

--- packages/dependencies/libvorbis.py
+++ packages/dependencies/libvorbis.py
@@ -4,6 +4,10 @@
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release',
+	#'run_post_install': ( # 2019.12.13 see if it still needss this
+	#	'sed -i.bak \'s/Libs: -L${{libdir}} -lvorbisenc/Libs: -L${{libdir}} -lvorbisenc -lvorbis -logg/\' "{pkg_config_path}/vorbisenc.pc"',  # 2019.12.13 see if it still needs this, probably not
+	#	'sed -i.bak \'s/Libs: -L${{libdir}} -lvorbis/Libs: -L${{libdir}} -lvorbis -logg/\' "{pkg_config_path}/vorbis.pc"',  # 2019.12.13 see if it still needs this, probably not
+	#),
 	'regex_replace': {
 		'post_install': [
 			# {
