ca6e540d45c95d2bdc932d766d5763d81385449e
hydra3333 <hydra3333@gmail.com>
2019-12-14 01:24:43 +1030

    extra options in libvpx

--- packages/dependencies/libvpx.py
+++ packages/dependencies/libvpx.py
@@ -9,11 +9,13 @@
 		'--enable-vp9-highbitdepth --enable-vp9-postproc --enable-coefficient-range-checking --enable-postproc-visualizer '
 		'--enable-error-concealment --enable-better-hw-compatibility '
 		'--enable-multi-res-encoding --enable-vp9-temporal-denoising '
-		'--disable-tools --disable-docs --disable-examples --disable-install-docs --disable-unit-tests --disable-decode-perf-tests --disable-encode-perf-tests --as=yasm'
+		'--disable-tools --disable-docs --disable-examples --disable-install-docs --disable-unit-tests --disable-decode-perf-tests --disable-encode-perf-tests '
+        '--disable-avx512 --as=nasm' # # 2019.12.13 removed --as=yasm, added --disable-avx512
 	,
 	'env_exports' : {
 		'CROSS' : '{cross_prefix_bare}',
 	},
+    'custom_cflag' : '{original_cflags}', # 2019.12.13
 	'cflag_addition' : '-fno-asynchronous-unwind-tables',
 	'patches' : [
 		( 'vpx/vpx_160_semaphore.patch', '-p1' ),
