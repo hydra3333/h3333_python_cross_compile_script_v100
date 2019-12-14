76ecdc614a0594b545cca9d06cddd12c2e0c1a2f
hydra3333 <hydra3333@gmail.com>
2019-12-14 20:31:43 +1030

    add it to disabled too, for the time being

--- packages/dependencies/_disabled/zenlib.py
+++ packages/dependencies/_disabled/zenlib.py
@@ -5,13 +5,8 @@
 	'url' : 'https://github.com/MediaArea/ZenLib.git',
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --enable-static --disable-shared --enable-shared=no',
 	'run_post_configure' : [
-		'sed -i.bak 's/ -DSIZE_T_IS_LONG//g' Makefile',
+		'sed -i.bak \'s/ -DSIZE_T_IS_LONG//g\' Makefile',
 	],
-	# 'patches' : (
-		# ('fcurl_gitibzen-1-fixes.patch', '-p1','../../..'),
-	# ),
-	# 'run_post_patch' : [
-		# 'sed -i.bak '/#include <windows.h>/ a\#include <time.h>' ../../../Source/ZenLib/Ztring.cpp',
-	# ],
-	'_info' : { 'version' : 'git (v4.35)', 'fancy_name' : 'zenlib' },
+
+	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'zenlib' },
 }
