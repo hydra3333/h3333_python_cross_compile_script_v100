df973a6a7a280c7d0607e0c8567d141b3789100b
hydra3333 <hydra3333@gmail.com>
2019-12-14 17:05:00 +1030

    initial updates to wavpack

--- packages/dependencies/wavpack.py
+++ packages/dependencies/wavpack.py
@@ -1,6 +1,6 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/dbry/WavPack.git',
-	'configure_options' : '{autoconf_prefix_options}',
+	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static', # 2019.12.13
 	'_info' : { 'version' : None, 'fancy_name' : 'wavpack' },
 }
