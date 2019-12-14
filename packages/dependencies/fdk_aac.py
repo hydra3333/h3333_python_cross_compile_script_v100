1ded551f8bddcda0e5aaed3319e3654c0a1e1680
hydra3333 <hydra3333@gmail.com>
2019-12-14 17:44:27 +1030

    initial updates to fdk_aac

--- packages/dependencies/fdk_aac.py
+++ packages/dependencies/fdk_aac.py
@@ -4,6 +4,6 @@
 		'autoreconf -fiv',
 	],
 	'url' : 'https://github.com/mstorsjo/fdk-aac.git',
-	'configure_options' : '{autoconf_prefix_options}',
+	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static', # 2019.12.13
 	'_info' : { 'version' : None, 'fancy_name' : 'fdk-aac' },
 }
