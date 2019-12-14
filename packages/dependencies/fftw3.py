aafc61d17019ab5c735090c6fbdd2832a4ece5ef
hydra3333 <hydra3333@gmail.com>
2019-12-14 13:53:54 +1030

    Add files via upload
    Deadsix27's cmake version wouldn't build for me !   No idea why.
        error: 'ALIGNMENTA' undeclared (first use in this function); did you mean 'ALIGNEDA'?
        error: 'ALIGNMENT' undeclared (first use in this function); did you mean 'ALIGNED'?
    
    So, have reverted to my known good build using old make,
    
    And added fftw3_dll.py (etc) so that when we build this dependency, it build relevant DLLs (only) which can be distributed into and used by other windows apps on the PC.

--- packages/dependencies/fftw3.py
+++ packages/dependencies/fftw3.py
@@ -1,33 +1,51 @@
 {
-	'repo_type' : 'archive',
-	'download_locations' : [
-		{ 'url' : 'http://fftw.org/fftw-3.3.8.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/fftw-3.3.8.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303' }, ], },
-	],
-    'env_exports' : {
-		'CFLAGS'   : '{original_cflags}', # 2019.12.13
-		'CXXFLAGS' : '{original_cflags}', # 2019.12.13
-		'CPPFLAGS' : '{original_cflags}', # 2019.12.13
-		'LDFLAGS'  : '{original_cflags}', # 2019.12.13
-	},
-	'cflag_addition': '-DWITH_OUR_MALLOC',
-	'conf_system' : 'cmake',
-	'source_subfolder' : '_build',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release '
-		'-DBUILD_TESTS=OFF '
-		'-DENABLE_THREADS=ON '
-		'-DENABLE_FLOAT=OFF '
-		'-DENABLE_LONG_DOUBLE=OFF '
-		'-DENABLE_QUAD_PRECISION=OFF '
-		'-DENABLE_SSE=ON '
-		'-DENABLE_SSE2=ON '
-		'-DENABLE_AVX=ON '
-		'-DENABLE_AVX2=ON '
-        '-DBUILD_DOC=OFF ' # 2019.12.13
-# 2019.12.13 NOTE: in a separate .py dependency to build the shared .DLLs, 
-#                  we MUST use "-DBUILD_SHARED_LIBS=1 -DWINDOWS_F77_MANGLING=ON"
-#            In some things "-lfftw" must be changed to "-lfftw3", however also note the ".pc" change below (check libramplerate, rubberbabd)
-	,
+    #----------------------------------------------------------------------------
+    # Deadsix27's cmake version wouldn't build for me !!!   No idea why.
+    #    error: 'ALIGNMENTA' undeclared (first use in this function); did you mean 'ALIGNEDA'?
+    #    error: 'ALIGNMENT' undeclared (first use in this function); did you mean 'ALIGNED'?
+    # So revert to my known good build using old make
+	#'repo_type' : 'archive',
+	#'download_locations' : [
+	#	{ 'url' : 'http://fftw.org/fftw-3.3.8.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303' }, ], },
+	#	{ 'url' : 'https://fossies.org/linux/misc/fftw-3.3.8.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303' }, ], },
+	#],
+    #'cflag_addition': '-DWITH_OUR_MALLOC',
+	#'conf_system' : 'cmake',
+	#'source_subfolder' : '_build',
+	#'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release '
+	#	'-DBUILD_TESTS=OFF '
+	#	'-DENABLE_THREADS=ON '
+	#	'-DENABLE_FLOAT=OFF ' # 2019.12.13 ?? SSE requires SINGLE (FLOAT) precision and does not build without it ??? also, the default build is DOULBLE (no separate flag for it)
+	#	'-DENABLE_LONG_DOUBLE=OFF '
+	#	'-DENABLE_QUAD_PRECISION=OFF '
+	#	'-DENABLE_SSE=ON '
+	#	'-DENABLE_SSE2=ON '
+	#	'-DENABLE_AVX=ON '
+	#	'-DENABLE_AVX2=ON '
+    #   '-DBUILD_DOC=OFF ' # 2019.12.13
+    #   # In some things "-lfftw" must be changed to "-lfftw3", however also note the ".pc" change below (check libramplerate, rubberbabd)
+	#,
+    #
+    # 2019.12.13  'fftw3' from my build, it works.
+		'repo_type' : 'archive',
+		'download_locations' : [
+			#UPDATECHECKS: http://fftw.org/download.html
+			{ "url" : "http://fftw.org/fftw-3.3.8.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303" }, ], },
+			{ "url" : "https://fossies.org/linux/misc/fftw-3.3.8.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303" }, ], },
+		],
+        'env_exports' : {
+			'CFLAGS'   : '{original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
+			'CXXFLAGS' : '{original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
+			'CPPFLAGS' : '{original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
+			'LDFLAGS'  : '{original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
+		},
+		'configure_options': '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static '
+                             '--disable-silent-rules --disable-doc '
+                             '--disable-alloca --with-our-malloc --with-windows-f77-mangling '
+                             '--enable-threads --with-combined-threads '
+                             '--disable-float --disable-long-double -disable-quad-precision ' # 2019.12.13 so the default is "DOUBLE"
+                             '--enable-sse2 --enable-avx --enable-avx2 --disable-altivec --disable-vsx --disable-neon ', # 2019.12.13 removed --enable-sse as SSE only builds with "FLOAT/SINGLE"
+    #
 	'regex_replace': {
 		'post_patch': [
 			{
@@ -38,5 +56,5 @@
 		],
 	},
 	'update_check' : { 'url' : 'ftp://ftp.fftw.org/pub/fftw/', 'type' : 'ftpindex', 'regex' : r'fftw-(?P<version_num>[\d.]+)\.tar\.gz' },
-	'_info' : { 'version' : '3.3.8', 'fancy_name' : 'fftw' },
+	'_info' : { 'version' : '3.3.8', 'fancy_name' : 'fftw3' },
 }
