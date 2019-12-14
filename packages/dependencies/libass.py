4d02e9e6cec43ffa736a8bd8e4b1c6fcab681441
hydra3333 <hydra3333@gmail.com>
2019-12-14 15:13:03 +1030

    initial changes to libass

--- packages/dependencies/libass.py
+++ packages/dependencies/libass.py
@@ -3,7 +3,7 @@
 	'url' : 'https://github.com/libass/libass.git',
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --enable-silent-rules',
     'run_post_install': [
-		'sed -i.bak \'s/-lass -lm/-lass -lfribidi -lfreetype -lexpat -lm/\' "{pkg_config_path}/libass.pc"', #-lfontconfig # 2018.12.13
+		'sed -i.bak \'s/-lass -lm/-lass -lfribidi -lfreetype -lexpat -lm/\' "{pkg_config_path}/libass.pc"', #-lfontconfig # 2018.12.13 # 2019.12.13
 	],
 	'depends_on' : [ 'expat', 'fontconfig', 'freetype', 'iconv', 'libfribidi'],
 	'_info' : { 'version' : None, 'fancy_name' : 'libass' },
