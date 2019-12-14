e22661700b8bf50588612fc9beb3238f2658ef50
hydra3333 <hydra3333@gmail.com>
2019-12-14 20:41:08 +1030

    initial changes to libssh2

--- packages/dependencies/libssh2.py
+++ packages/dependencies/libssh2.py
@@ -1,9 +1,9 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/libssh2/libssh2.git',
-	'configure_options' : '{autoconf_prefix_options} --disable-examples-build ',
+	'configure_options' : '{autoconf_prefix_options} --disable-examples-build --disable-shared --enable-static --disable-examples-build --with-crypto=openssl ', # 2019.12.13
 	'depends_on' : [
-		'zlib', #'libressl'
+		'zlib', 'libressl', # 2019.12.13
 	],
 	'env_exports' : {
 		'LIBS' : '-lcrypt32' # Otherwise: libcrypto.a(e_capi.o):e_capi.c:(.text+0x476d): undefined reference to `__imp_CertFreeCertificateContext'
