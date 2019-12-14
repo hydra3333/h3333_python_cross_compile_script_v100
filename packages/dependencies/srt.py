651c28747c25bfdd0285d1bc8208a04d2c5bf83c
hydra3333 <hydra3333@gmail.com>
2019-12-14 16:54:16 +1030

    initial changes to srt

--- packages/dependencies/srt.py
+++ packages/dependencies/srt.py
@@ -4,6 +4,7 @@
 	'source_subfolder' : '_build',
 	'conf_system' : 'cmake',
 	'depends_on' : [ 'gettext', 'gnutls' ],
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_STATIC=1 -DUSE_STATIC_LIBSTDCXX=1 -DUSE_ENCLIB=gnutls -DENABLE_SHARED=0',
-	'_info' : { 'version' : None, 'fancy_name' : 'libmysofa' },
+	#'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_STATIC=1 -DUSE_STATIC_LIBSTDCXX=1 -DUSE_ENCLIB=gnutls -DENABLE_SHARED=0', # 2019.12.13
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_STATIC=1 -DUSE_STATIC_LIBSTDCXX=1 -DUSE_ENCLIB=gnutls -DUSE_GNUTLS=1 -DHAICRYPT_USE_OPENSSL_EVP=0 -DHAICRYPT_USE_OPENSSL_AES=0 -DENABLE_SUFLIP=0 -DENABLE_EXAMPLES=0 -DENABLE_SHARED=0', # 2019.12.13
+	'_info' : { 'version' : None, 'fancy_name' : 'srt' }, # it is actually srt 
 }
