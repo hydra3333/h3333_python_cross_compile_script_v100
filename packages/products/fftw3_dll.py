1308b533df7a67a097ea64a2895720994a61fce6
hydra3333 <hydra3333@gmail.com>
2019-12-15 09:40:41 +1030

    fftw3 DLL try not to build a top level ;)

--- packages/products/fftw3_dll.py
+++ packages/products/fftw3_dll.py
@@ -1,5 +1,11 @@
-{
-	'is_dep_inheriter' : True,
+{ # 2019.12.13
+	'repo_type' : 'archive',
+	'do_not_bootstrap' : True,
+	'needs_configure' : False,
+	'needs_make' : False,
+	'needs_make_install' : False,
+	'recursive_git' : False,
+	#'is_dep_inheriter' : True,
 	'depends_on' : [
 		'fftw3_dll_single', 'fftw3_dll_double', 'fftw3_dll_ldouble', 'fftw3_dll_quad', 
 	],
