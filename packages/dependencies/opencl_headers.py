ea1970dd30ef3577bed31bda5661cbc4295b734c
hydra3333 <hydra3333@gmail.com>
2019-12-15 09:04:53 +1030

    fix opencl-headers

--- packages/dependencies/opencl_headers.py
+++ packages/dependencies/opencl_headers.py
@@ -3,7 +3,7 @@
 	'url' : 'https://github.com/KhronosGroup/OpenCL-Headers.git',
 	'run_post_patch' : [
 		#'if [ ! -f "already_ran_make_install" ] ; then if [ ! -d "{target_prefix}/include/CL" ] ; then mkdir "{target_prefix}/include/CL" ; fi ; fi', # 2019.12.13 commented out
-        if [ ! -d "{target_prefix}/include/CL" ] ; then mkdir -pv "{target_prefix}/include/CL" ; fi   # 2019.12.13 always try to create the folder
+        'if [ ! -d "{target_prefix}/include/CL" ] ; then mkdir -pv "{target_prefix}/include/CL" ; fi'   # 2019.12.13 always try to create the folder
 		#'if [ ! -f "already_ran_make_install" ] ; then cp -rfv CL/*.h "{target_prefix}/include/CL/" ; fi', # 2019.12.13 always commented out
         'cp -rfv CL/*.h "{target_prefix}/include/CL/"', # 2019.12.13 always copy the file
 		'if [ ! -f "already_ran_make_install" ] ; then touch already_ran_make_install ; fi',
