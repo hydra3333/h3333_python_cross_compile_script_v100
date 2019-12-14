964186251744d8a175715837c4cef6a8a60be7a9
hydra3333 <hydra3333@gmail.com>
2019-12-14 21:10:50 +1030

    Add files via upload

--- packages/dependencies/libfile.py
+++ packages/dependencies/libfile.py
@@ -7,13 +7,13 @@
 		( 'libfile/file-win32.patch', '-p1' ),
 	],
 	'configure_options' : '{autoconf_prefix_options} --enable-fsect-man5',
-	'depends_on' : [ 'mingw-libgnurx' ],  # ,'libfile_local' ],
 	'env_exports' : { 'TARGET_CFLAGS' : '{original_cflags}' },
 	'run_post_patch' : [
 		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/seccomp.c',
 		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/compress.c',
 		'autoreconf -fiv' 
 	],
+	'depends_on' : [ 'mingw-libgnurx', 'libfile_local' ], # 2019.12.13
 	'flipped_path' : True,
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : None, 'fancy_name' : 'file' },
