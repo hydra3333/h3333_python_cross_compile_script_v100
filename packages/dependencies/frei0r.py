882b6a78389317cf723dfdd5b0828ec18b56b0e2
hydra3333 <hydra3333@gmail.com>
2019-12-14 16:27:31 +1030

    initial updates for frie0r

--- packages/dependencies/frei0r.py
+++ packages/dependencies/frei0r.py
@@ -1,15 +1,17 @@
 {
 	'repo_type' : 'archive',
-	'download_locations' : [
+	'download_locations' : [ # 2019.12.13 1.7.0 is newer than mine at 1.6.1
 		{ 'url' : 'https://files.dyne.org/frei0r/frei0r-plugins-1.7.0.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '1b1ff8f0f9bc23eed724e94e9a7c1d8f0244bfe33424bb4fe68e6460c088523a' }, ], },
 		{ 'url' : 'https://cdn.netbsd.org/pub/pkgsrc/distfiles/frei0r-plugins-1.7.0.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '1b1ff8f0f9bc23eed724e94e9a7c1d8f0244bfe33424bb4fe68e6460c088523a' }, ], },
 	],
 	'depends_on' : [ 'dlfcn-win32', ],
 	'conf_system' : 'cmake',
 	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DWITHOUT_OPENCV=YES',
-	# 'run_post_patch' : [
+	'run_post_patch' : [
 	# 	'sed -i.bak "s/find_package (Cairo)//g" CMakeLists.txt',
-	# ],
+    #   'sed -i.bak "s/-arch i386//" CMakeLists.txt', # 2019.12.13 this sed not needed either
+        'sed -i.bak "s/VERSION 1.6.1/VERSION 1.7.0/" CMakeLists.txt', # 2013.12.13 why didna they bump it ?
+	],
 	'update_check' : { 'url' : 'https://files.dyne.org/frei0r/releases/', 'type' : 'httpindex', 'regex' : r'frei0r-plugins-(?P<version_num>[\d.]+)\.tar\.gz' },
 	'_info' : { 'version' : '1.7.0', 'fancy_name' : 'frei0r-plugins' },
 }
