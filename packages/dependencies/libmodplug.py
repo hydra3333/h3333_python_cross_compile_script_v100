6f9945d767036bbf382a549f908331ebf5196e67
hydra3333 <hydra3333@gmail.com>
2019-12-14 17:34:13 +1030

    initial libmodplug changes

--- packages/dependencies/libmodplug.py
+++ packages/dependencies/libmodplug.py
@@ -1,12 +1,20 @@
 {
 	'repo_type' : 'git',
 	'url': 'https://github.com/Konstanty/libmodplug.git',
+    #'download_locations' : [ # 2019.12.13 this was the pre-GIT download location
+	#	#UPDATECHECKS: https://sourceforge.net/projects/modplug-xmms/files/libmodplug/
+	#	{ "url" : "https://ftp.openbsd.org/pub/OpenBSD/distfiles/libmodplug-0.8.9.0.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de" }, ], },
+	#	{ "url" : "https://sourceforge.net/projects/modplug-xmms/files/libmodplug/0.8.9.0/libmodplug-0.8.9.0.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de" }, ], },
+	#],
 	'conf_system' : 'cmake',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix}',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} --enable-static --disable-shared',  # 2019.12.13
 	'source_subfolder': '_build',
 	'patches': [
 		('modplug/0001-modplug-mingw-workaround.patch', '-p1', '..'), # to avoid setting -DLIBMODPLUG_STATIC
 	],
+    #'run_post_install': ( # 2019.12.13 pewrhaps not needed in latest git ?
+	#	'sed -i.bak \'s/-lmodplug.*/-lmodplug -lstdc++/\' "{pkg_config_path}/libmodplug.pc"', # huh ?? c++?  # 2019.12.13
+	#),
 	'regex_replace': {
 		'post_patch': [
 			{
