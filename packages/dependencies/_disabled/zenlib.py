5d484f94546299a46bfd4f548340bcb86483d246
hydra3333 <hydra3333@gmail.com>
2019-12-14 20:32:36 +1030

    Add files via upload

--- packages/dependencies/_disabled/zenlib.py
+++ packages/dependencies/_disabled/zenlib.py
@@ -5,7 +5,7 @@
 	'url' : 'https://github.com/MediaArea/ZenLib.git',
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --enable-static --disable-shared --enable-shared=no',
 	'run_post_configure' : [
-		'sed -i.bak \'s/ -DSIZE_T_IS_LONG//g\' Makefile',
+		'sed -i.bak \'s/ -DSIZE_T_IS_LONG//g\' Makefile', # 2019.12.13
 	],
 
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'zenlib' },
