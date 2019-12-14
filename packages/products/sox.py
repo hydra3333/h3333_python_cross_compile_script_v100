65e6c8613b3f6708ae76b780d6f6f69d7e41c1f1
hydra3333 <hydra3333@gmail.com>
2019-12-14 19:43:22 +1030

    initial changes to product sox

--- packages/products/sox.py
+++ packages/products/sox.py
@@ -2,8 +2,11 @@
 	'repo_type' : 'git',
 	'rename_folder' : 'sox_git',
 	'url' : 'git://git.code.sf.net/p/sox/code',
-	'configure_options' : '--host={target_host} --prefix={output_prefix}/sox_git.installed --disable-shared --enable-static --without-gsm',
-	'run_post_patch' : [
+	'configure_options' : '--host={target_host} --prefix={output_prefix}/sox_git.installed --disable-shared --enable-static --without-gsm --disable-examples', # 2019.12.13
+	'env_exports' : { # 2019.12.13
+		'LIBS'   : '-lFLAC -lFLAC++',
+	},
+    'run_post_patch' : [
 		'autoreconf -fiv',
 		'if [ -f "{target_prefix}/lib/libgsm.a" ] ; then mv {target_prefix}/lib/libgsm.a {target_prefix}/lib/libgsm.a.disabled ; fi',
 		'if [ -d "{target_prefix}/include/gsm" ] ; then mv {target_prefix}/include/gsm {target_prefix}/include/gsm.disabled ; fi',
@@ -13,7 +16,7 @@
 		'if [ -d "{target_prefix}/include/gsm.disabled" ] ; then mv {target_prefix}/include/gsm.disabled {target_prefix}/include/gsm ; fi',
 	],
 	'depends_on' : [
-		'libvorbis', 'gettext', 'libsndfile', 'libpng',
+		'libvorbis', 'gettext', 'libsndfile', 'libpng', 'libopus', 'libflac', # 2019.12.13
 	],
 	'_info' : { 'version' : None, 'fancy_name' : 'SoX' },
 }
