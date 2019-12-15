1fc516e58278201dc779ed017e11d9b2215b8c74
hydra3333 <hydra3333@gmail.com>
2019-12-15 16:18:19 +1030

    updated zlib

--- packages/dependencies/zlib.py
+++ packages/dependencies/zlib.py
@@ -1,11 +1,37 @@
-{
+{ # 2019.12.13
 	'repo_type' : 'git',
 	'url' : 'https://github.com/madler/zlib.git',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
+	'env_exports' : { # 2019.12.13
+		'AR' : '{cross_prefix_bare}ar',
+		'CC' : '{cross_prefix_bare}gcc',
+		'PREFIX' : '{target_prefix}',
+		'RANLIB' : '{cross_prefix_bare}ranlib',
+		'LD'     : '{cross_prefix_bare}ld',
+		'STRIP'  : '{cross_prefix_bare}strip',
+		'CXX'    : '{cross_prefix_bare}g++',
+	},
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DINSTALL_PKGCONFIG_DIR="{target_prefix}/lib/pkgconfig" -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release',
 	'patches' : [
 		('zlib/0001-mingw-workarounds.patch', '-p1', '..'),
 	],
 	'_info' : { 'version' : None, 'fancy_name' : 'zlib' },
 }
+# 2019.12.13 old:
+#	'zlib' : {
+#		'repo_type' : 'git',
+#		'url' : 'https://github.com/madler/zlib.git',
+#		'env_exports' : {
+#			'AR' : '{cross_prefix_bare}ar',
+#			'CC' : '{cross_prefix_bare}gcc',
+#			'PREFIX' : '{target_prefix}',
+#			'RANLIB' : '{cross_prefix_bare}ranlib',
+#			'LD'     : '{cross_prefix_bare}ld',
+#			'STRIP'  : '{cross_prefix_bare}strip',
+#			'CXX'    : '{cross_prefix_bare}g++',
+#		},
+#		'configure_options': '--static --prefix={target_prefix}',
+#		'build_options': '{make_prefix_options} ARFLAGS=rcs',
+#		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'zlib' },
+#	},
