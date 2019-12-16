dfed5d48261a48f4ab04cd57add15f0059088f7f
hydra3333 <hydra3333@gmail.com>
2019-12-16 13:45:58 +1030

    Add files via upload

--- packages/dependencies/libgpg_error.py
+++ packages/dependencies/libgpg_error.py
@@ -5,7 +5,7 @@
 		'recursive_git' : True,
 		'url' : 'git://git.gnupg.org/libgpg-error.git', # https://git.gnupg.org/ # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgpg-error.git;a=summary
 		'configure_options': '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --disable-rpath --disable-doc --disable-tests --with-libiconv-prefix={target_prefix}', # --with-libintl=no --with-libpth=no',
-		'custom_cflag' : ' -fstack-protector-all -D_FORTIFY_SOURCE=2 ', # 2019.12.13 it fails to build with anythinf other than this, eg it crashes with -O3 and -fstack-protector-all
+		'custom_cflag' : ' -D_FORTIFY_SOURCE=2 ', # 2019.12.13 it fails to build with anythinf other than this, eg it crashes with -O3 and -fstack-protector-all
 		'run_post_patch' : (
 			'autoreconf -fiv',
 		),
