d886cb48d7b55613f8f29c47f74f66392b95c24d
hydra3333 <hydra3333@gmail.com>
2019-12-16 19:50:35 +1030

    fix libvpx options

--- packages/dependencies/libvpx.py
+++ packages/dependencies/libvpx.py
@@ -9,8 +9,9 @@
 		'--enable-vp9-highbitdepth --enable-vp9-postproc --enable-coefficient-range-checking --enable-postproc-visualizer '
 		'--enable-error-concealment --enable-better-hw-compatibility '
 		'--enable-multi-res-encoding --enable-vp9-temporal-denoising '
-		'--disable-tools --disable-docs --disable-examples --disable-install-docs --disable-unit-tests --disable-decode-perf-tests --disable-encode-perf-tests '
-        '--disable-avx512 --as=nasm' # # 2019.12.13 removed --as=yasm, added --disable-avx512
+		'--enable-tools --disable-docs --enable-examples --disable-install-docs ' # 2019.12.13 enable tools and examples
+		'--disable-unit-tests --disable-decode-perf-tests --disable-encode-perf-tests '
+        '--disable-avx512 --as=nasm ' # 2013.13.13 back to --as=nasm rather than yasm # 2019.12.13 removed --as=yasm, added --disable-avx512
 	,
 	'env_exports' : {
 		'CROSS' : '{cross_prefix_bare}',
@@ -22,3 +23,27 @@
 	],
 	'_info' : { 'version' : None, 'fancy_name' : 'libvpx' },
 }
+# 2019.12.13 old:
+#	'libvpx' : {
+#		'repo_type' : 'git',
+#		'url' : 'https://chromium.googlesource.com/webm/libvpx',
+#		'configure_options':
+#			'--target={bit_name2}-{bit_name_win}-gcc '
+#			'--prefix={target_prefix} --disable-shared '
+#			'--enable-static --enable-webm-io --enable-vp9 '
+#			'--enable-vp8 --enable-runtime-cpu-detect '
+#			'--enable-vp9-highbitdepth --enable-vp9-postproc --enable-coefficient-range-checking '
+#			'--enable-error-concealment --enable-better-hw-compatibility '
+#			'--enable-multi-res-encoding --enable-vp9-temporal-denoising '
+#			'--enable-tools --disable-docs --enable-examples --disable-install-docs --disable-unit-tests --disable-decode-perf-tests --disable-encode-perf-tests --disable-avx512 --as=nasm' # 2018.11.23 --enable-tools --enable-examples --disable-avx512 not --as=yasm
+#		,
+#		'env_exports' : {
+#			'CROSS' : '{cross_prefix_bare}',
+#		},
+#		#'custom_cflag' : '-fno-asynchronous-unwind-tables {original_cflags}',
+#		'custom_cflag' : '{original_cflags}',
+#		'patches': (
+#			( 'https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v02/master/patches/vpx_160_semaphore.patch', '-p1' ),
+#		),
+#		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libvpx' },
+#	},
