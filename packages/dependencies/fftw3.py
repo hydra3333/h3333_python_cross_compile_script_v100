aa10f1298f3d16e3c0591fae5dd9ad564bd1a535
hydra3333 <hydra3333@gmail.com>
2019-12-17 13:31:14 +1030

    update fftw3 and fftw3 DLL

--- packages/dependencies/fftw3.py
+++ packages/dependencies/fftw3.py
@@ -27,34 +27,41 @@
 	#,
     #
     # 2019.12.13  'fftw3' from my build, it works.
-		'repo_type' : 'archive',
-		'download_locations' : [
-			#UPDATECHECKS: http://fftw.org/download.html
-			{ "url" : "http://fftw.org/fftw-3.3.8.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303" }, ], },
-			{ "url" : "https://fossies.org/linux/misc/fftw-3.3.8.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303" }, ], },
-		],
-        'env_exports' : {
-			'CFLAGS'   : '{original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
-			'CXXFLAGS' : '{original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
-			'CPPFLAGS' : '{original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
-			'LDFLAGS'  : '{original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
-		},
-		'configure_options': '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static '
-                             '--disable-silent-rules --disable-doc '
-                             '--disable-alloca --with-our-malloc --with-windows-f77-mangling '
-                             '--enable-threads --with-combined-threads '
-                             '--disable-float --disable-long-double -disable-quad-precision ' # 2019.12.13 so the default is "DOUBLE"
-                             '--enable-sse2 --enable-avx --enable-avx2 --disable-altivec --disable-vsx --disable-neon ', # 2019.12.13 removed --enable-sse as SSE only builds with "FLOAT/SINGLE"
-    #
+	'repo_type' : 'archive',
+	'download_locations' : [
+		#UPDATECHECKS: http://fftw.org/download.html
+		{ "url" : "http://fftw.org/fftw-3.3.8.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303" }, ], },
+		{ "url" : "https://fossies.org/linux/misc/fftw-3.3.8.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303" }, ], },
+	],
+	'rename_folder' : 'fftw3_for_ffmpeg',
+	'configure_options': '--host={target_host} --prefix={target_prefix} '
+						 '--disable-shared --enable-static '
+                            '--disable-silent-rules --disable-doc '
+                            '--disable-alloca --with-our-malloc --with-windows-f77-mangling '
+                            '--enable-threads --with-combined-threads '
+                            '--disable-float --disable-long-double -disable-quad-precision ' # 2019.12.13 so the default is "DOUBLE"
+                            '--enable-sse2 --enable-avx --enable-avx2 --disable-altivec --disable-vsx --disable-neon ' # 2019.12.13 removed --enable-sse as SSE only builds with "FLOAT/SINGLE"
+	,
 	'regex_replace': {
 		'post_patch': [
 			{
 				0: r'fftw\${{PREC_SUFFIX}}\.pc',
 				1: r'fftw3${{PREC_SUFFIX}}.pc',
-				'in_file': '../CMakeLists.txt'
+				'in_file': './CMakeLists.txt'
 			},
 		],
 	},
 	'update_check' : { 'url' : 'ftp://ftp.fftw.org/pub/fftw/', 'type' : 'ftpindex', 'regex' : r'fftw-(?P<version_num>[\d.]+)\.tar\.gz' },
 	'_info' : { 'version' : '3.3.8', 'fancy_name' : 'fftw3' },
 }
+# 2019.12.13 old:
+#	'fftw3' : {
+#		'repo_type' : 'archive',
+#		'download_locations' : [
+#			#UPDATECHECKS: http://fftw.org/download.html
+#			{ "url" : "http://fftw.org/fftw-3.3.8.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303" }, ], },
+#			{ "url" : "https://fossies.org/linux/misc/fftw-3.3.8.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303" }, ], },
+#		],
+#		'configure_options': '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static',
+#		'_info' : { 'version' : '3.3.8', 'fancy_name' : 'fftw3' },
+#	},
--- packages/dependencies/fftw3_dll_double.py
+++ packages/dependencies/fftw3_dll_double.py
@@ -5,26 +5,21 @@
 		{ "url" : "http://fftw.org/fftw-3.3.8.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303" }, ], },
 		{ "url" : "https://fossies.org/linux/misc/fftw-3.3.8.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "6113262f6e92c5bd474f2875fa1b01054c4ad5040f6b0da7c03c98821d9ae303" }, ], },
 	],
-    'env_exports' : {
-		'CFLAGS'   : '{original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
-		'CXXFLAGS' : '{original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
-		'CPPFLAGS' : '{original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
-		'LDFLAGS'  : '{original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
-	},
+	'rename_folder' : 'fftw3_dll_double',
 	'configure_options': '--host={target_host} --prefix={product_prefix}/fftw3_dll_double --exec-prefix={product_prefix}/fftw3_dll_double '
                          '--disable-shared --enable-static '
                          '--disable-silent-rules --disable-doc '
                          '--disable-alloca --with-our-malloc --with-windows-f77-mangling '
                          '--enable-threads --with-combined-threads '
                          '--disable-float --disable-long-double -disable-quad-precision ' # 2019.12.13 the default is "DOUBLE"
-                         '--enable-sse2 --enable-avx --enable-avx2 --disable-altivec --disable-vsx --disable-neon ', # 2019.12.13 removed --enable-sse as SSE only builds with "FLOAT/SINGLE"
-    #
+                         '--enable-sse2 --enable-avx --enable-avx2 --disable-altivec --disable-vsx --disable-neon ' # 2019.12.13 removed --enable-sse as SSE only builds with "FLOAT/SINGLE"
+    ,
 	'regex_replace': {
 		'post_patch': [
 			{
 				0: r'fftw\${{PREC_SUFFIX}}\.pc',
 				1: r'fftw3${{PREC_SUFFIX}}.pc',
-				'in_file': '../CMakeLists.txt'
+				'in_file': './CMakeLists.txt'
 			},
 		],
 	},
@@ -32,5 +27,5 @@
 		'ls -alR {product_prefix}/fftw3_dll_double/bin',
 	),
 	'update_check' : { 'url' : 'ftp://ftp.fftw.org/pub/fftw/', 'type' : 'ftpindex', 'regex' : r'fftw-(?P<version_num>[\d.]+)\.tar\.gz' },
-	'_info' : { 'version' : '3.3.8', 'fancy_name' : 'fftw3 DLL single' },
+	'_info' : { 'version' : '3.3.8', 'fancy_name' : 'fftw3_dll_double' },
 }
