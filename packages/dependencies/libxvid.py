fcad1a89eff37a3a19e7fbad4e5b815516e3afd8
hydra3333 <hydra3333@gmail.com>
2019-12-17 15:37:20 +1030

    revert to xvid 1.3.5 since 1.3.6 download fails https certificate validation

--- packages/dependencies/libxvid.py
+++ packages/dependencies/libxvid.py
@@ -1,9 +1,10 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		#{ "url" : "https://downloads.xvid.org/downloads/xvidcore-1.3.5.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "165ba6a2a447a8375f7b06db5a3c91810181f2898166e7c8137401d7fc894cf0" }, ], },
-		{ 'url' : 'https://downloads.xvid.org/downloads/xvidcore-1.3.6.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '89315b536935b8fd66b702afe47361562a86ff49b77da51b0aff4c4642d4f8f3' }, ], },
-		#{ 'url' : 'https://fossies.org/linux/misc/xvidcore-1.3.6.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '89315b536935b8fd66b702afe47361562a86ff49b77da51b0aff4c4642d4f8f3' }, ], },
+		{ "url" : "https://downloads.xvid.org/downloads/xvidcore-1.3.5.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "165ba6a2a447a8375f7b06db5a3c91810181f2898166e7c8137401d7fc894cf0" }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/xvidcore-1.3.5.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '89315b536935b8fd66b702afe47361562a86ff49b77da51b0aff4c4642d4f8f3' }, ], },
+		# 1.3.6 download currently fails https certificate validation error at download !!
+		#{ 'url' : 'https://downloads.xvid.org/downloads/xvidcore-1.3.6.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '89315b536935b8fd66b702afe47361562a86ff49b77da51b0aff4c4642d4f8f3' }, ], },
 	],
 	'folder_name' : 'xvidcore',
 	'rename_folder' : 'xvidcore',
