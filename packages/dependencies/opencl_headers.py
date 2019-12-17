30866965d14b006522c1f7c533522ece633d0bb2
hydra3333 <hydra3333@gmail.com>
2019-12-17 17:17:30 +1030

    misssong comma

--- packages/dependencies/opencl_headers.py
+++ packages/dependencies/opencl_headers.py
@@ -3,9 +3,9 @@
 	'url' : 'https://github.com/KhronosGroup/OpenCL-Headers.git',
 	'run_post_patch' : [
 		#'if [ ! -f "already_ran_make_install" ] ; then if [ ! -d "{target_prefix}/include/CL" ] ; then mkdir "{target_prefix}/include/CL" ; fi ; fi', # 2019.12.13 commented out
-        'if [ ! -d "{target_prefix}/include/CL" ] ; then mkdir -pv "{target_prefix}/include/CL" ; fi'   # 2019.12.13 always try to create the folder
 		#'if [ ! -f "already_ran_make_install" ] ; then cp -rfv CL/*.h "{target_prefix}/include/CL/" ; fi', # 2019.12.13 always commented out
-        'cp -rfv CL/*.h "{target_prefix}/include/CL/"', # 2019.12.13 always copy the file
+		'if [ ! -d "{target_prefix}/include/CL" ] ; then mkdir -pv "{target_prefix}/include/CL" ; fi',   # 2019.12.13 always try to create the folder
+        'cp -rfv CL/*.h "{target_prefix}/include/CL/"', # 2019.12.13 always copy the files
 		'if [ ! -f "already_ran_make_install" ] ; then touch already_ran_make_install ; fi',
 	],
 	'needs_make' : False,
@@ -13,3 +13,20 @@
 	'needs_configure' : False,
 	'_info' : { 'version' : None, 'fancy_name' : 'OpenCL Headers for OpenCL ICD Loader' },
 }
+# 2019.12.13 old:
+#	'opencl_headers' : {
+#		'repo_type' : 'git',
+#		'url' : 'https://github.com/KhronosGroup/OpenCL-Headers.git',
+#		'run_post_patch' : (
+#			#'if [ ! -f "already_ran_make_install" ] ; then if [ ! -d "{target_prefix}/include/CL" ] ; then mkdir -pv "{target_prefix}/include/CL" ; fi ; fi',
+#			#'if [ ! -f "already_ran_make_install" ] ; then cp -rfv CL/*.h "{target_prefix}/include/CL/" ; fi',
+#			#'if [ ! -f "already_ran_make_install" ] ; then touch already_ran_make_install ; fi',
+#			'if [ ! -d "{target_prefix}/include/CL" ] ; then mkdir -pv "{target_prefix}/include/CL" ; fi',
+#			'cp -rfv CL/*.h "{target_prefix}/include/CL/"',
+#			'touch already_ran_make_install',
+#		),
+#		'needs_make':False,
+#		'needs_make_install':False,
+#		'needs_configure':False,
+#		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'OpenCL-Headers' },
+#	},
