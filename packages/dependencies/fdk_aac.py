acfd0c9bdb9036603fda13db0488747b8ab06a47
hydra3333 <hydra3333@gmail.com>
2019-12-17 17:59:39 +1030

    Add files via upload

--- packages/dependencies/fdk_aac.py
+++ packages/dependencies/fdk_aac.py
@@ -5,7 +5,7 @@
 	],
 	'url' : 'https://github.com/mstorsjo/fdk-aac.git',
 	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static', # 2019.12.13
-	'_info' : { 'version' : 'git (master), 'fancy_name' : 'fdk-aac' },
+	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'fdk-aac' },
 }
 # 2019.12.13 old:
 #	'fdk_aac' : {
