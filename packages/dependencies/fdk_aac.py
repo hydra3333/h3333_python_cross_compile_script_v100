1fd1059d95619dd95f84c343dc19d97dd8a7584e
hydra3333 <hydra3333@gmail.com>
2019-12-17 17:48:54 +1030

    version

--- packages/dependencies/fdk_aac.py
+++ packages/dependencies/fdk_aac.py
@@ -5,5 +5,15 @@
 	],
 	'url' : 'https://github.com/mstorsjo/fdk-aac.git',
 	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static', # 2019.12.13
-	'_info' : { 'version' : None, 'fancy_name' : 'fdk-aac' },
+	'_info' : { 'version' : 'git (master), 'fancy_name' : 'fdk-aac' },
 }
+# 2019.12.13 old:
+#	'fdk_aac' : {
+#		'repo_type' : 'git',
+#		'run_post_patch': [
+#			'autoreconf -fiv',
+#		],
+#		'url' : 'https://github.com/mstorsjo/fdk-aac.git',
+#		'configure_options': '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static',
+#		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'fdk-aac' },
+#	},
