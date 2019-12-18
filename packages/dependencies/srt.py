ea62d847e4f34d6e1b95e8c7a2180b69a682854a
hydra3333 <hydra3333@gmail.com>
2019-12-18 12:38:56 +1030

    see what happens to srt

--- packages/dependencies/srt.py
+++ packages/dependencies/srt.py
@@ -5,6 +5,19 @@
 	'conf_system' : 'cmake',
 	'depends_on' : [ 'gettext', 'gnutls' ],
 	#'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_STATIC=1 -DUSE_STATIC_LIBSTDCXX=1 -DUSE_ENCLIB=gnutls -DENABLE_SHARED=0', # 2019.12.13
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_STATIC=1 -DUSE_STATIC_LIBSTDCXX=1 -DUSE_ENCLIB=gnutls -DUSE_GNUTLS=1 -DHAICRYPT_USE_OPENSSL_EVP=0 -DHAICRYPT_USE_OPENSSL_AES=0 -DENABLE_SUFLIP=0 -DENABLE_EXAMPLES=0 -DENABLE_SHARED=0', # 2019.12.13
-	'_info' : { 'version' : None, 'fancy_name' : 'srt' }, # it is actually srt 
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DCMAKE_BUILD_TYPE=Release -D_WIN32_WINNT=0x600 -DENABLE_SHARED=0 -DENABLE_STATIC=1 -DUSE_STATIC_LIBSTDCXX=1 -DUSE_ENCLIB=gnutls -DUSE_GNUTLS=1 -DHAICRYPT_USE_OPENSSL_EVP=0 -DHAICRYPT_USE_OPENSSL_AES=0 -DENABLE_SUFLIP=0 -DENABLE_EXAMPLES=0 ', # 2019.12.13
+	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'srt' }, # it is actually srt 
 }
+# 2019.12.13 old:
+#	'libsrt' : { # 2019.05.10
+#		'repo_type' : 'git',
+#		'url' : 'https://github.com/Haivision/srt.git',
+#		#'branch' : 'cdeb47133e400ee89552ab3cf766d79deb392408', # undo # 2019.07.31 freeze srt since next commit breaks with fatal error: openssl/bio.h: No such file or directory | #include <openssl/bio.h>
+#		'source_subfolder' : '_build',
+#		'conf_system' : 'cmake',
+#		'patches' : (
+#		),
+#		'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DCMAKE_BUILD_TYPE=Release -D_WIN32_WINNT=0x600 -DENABLE_SHARED=off -DENABLE_STATIC=on -DUSE_STATIC_LIBSTDCXX=on -DUSE_GNUTLS=on -DENABLE_SUFLIP=off -DENABLE_EXAMPLES=off -DHAICRYPT_USE_OPENSSL_EVP=off -DHAICRYPT_USE_OPENSSL_AES=off',
+#		'depends_on' : [ 'gettext', 'gnutls' ],
+#		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libsrt' },
+#	},
