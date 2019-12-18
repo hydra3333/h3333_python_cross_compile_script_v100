24c3da13ad6bfe6d63a6b83c72af4a3f873a85e3
hydra3333 <hydra3333@gmail.com>
2019-12-18 12:46:56 +1030

    Add files via upload

--- packages/dependencies/srt.py
+++ packages/dependencies/srt.py
@@ -3,9 +3,9 @@
 	'url' : 'https://github.com/Haivision/srt.git',
 	'source_subfolder' : '_build',
 	'conf_system' : 'cmake',
-	'depends_on' : [ 'gettext', 'gnutls' ],
 	#'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_STATIC=1 -DUSE_STATIC_LIBSTDCXX=1 -DUSE_ENCLIB=gnutls -DENABLE_SHARED=0', # 2019.12.13
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DCMAKE_BUILD_TYPE=Release -D_WIN32_WINNT=0x600 -DENABLE_SHARED=0 -DENABLE_STATIC=1 -DUSE_STATIC_LIBSTDCXX=1 -DUSE_ENCLIB=gnutls -DUSE_GNUTLS=1 -DHAICRYPT_USE_OPENSSL_EVP=0 -DHAICRYPT_USE_OPENSSL_AES=0 -DENABLE_SUFLIP=0 -DENABLE_EXAMPLES=0 ', # 2019.12.13
+	'depends_on' : [ 'gettext', 'gnutls' ],
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'srt' }, # it is actually srt 
 }
 # 2019.12.13 old:
