40fa79eb688875a2983d0182adf3d160c5d4e0ce
hydra3333 <hydra3333@gmail.com>
2019-12-19 14:12:54 +1030

    fix libmodplug

--- packages/dependencies/libmodplug.py
+++ packages/dependencies/libmodplug.py
@@ -7,7 +7,7 @@
 	#	{ "url" : "https://sourceforge.net/projects/modplug-xmms/files/libmodplug/0.8.9.0/libmodplug-0.8.9.0.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de" }, ], },
 	#],
 	'conf_system' : 'cmake',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} --enable-static --disable-shared',  # 2019.12.13
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} DMODPLUG_STATIC=1 -DBUILD_SHARED_LIBS=0',  # 2019.12.13
 	'source_subfolder': '_build',
 	'patches': [
 		('modplug/0001-modplug-mingw-workaround.patch', '-p1', '..'), # to avoid setting -DLIBMODPLUG_STATIC
@@ -28,3 +28,18 @@
 	},
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libmodplug' },
 }
+# 2019.12.13 old:
+#	'libmodplug' : {
+#		'repo_type' : 'archive',
+#		'download_locations' : [
+#			#UPDATECHECKS: https://sourceforge.net/projects/modplug-xmms/files/libmodplug/
+#			{ "url" : "https://ftp.openbsd.org/pub/OpenBSD/distfiles/libmodplug-0.8.9.0.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de" }, ], },
+#			{ "url" : "https://sourceforge.net/projects/modplug-xmms/files/libmodplug/0.8.9.0/libmodplug-0.8.9.0.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de" }, ], },
+#		],
+#		'configure_options': '--host={target_host} --prefix={target_prefix} --enable-static --disable-shared',
+#		'run_post_install': (
+#			# unfortunately this sed isn't enough, though I think it should be [so we add --extra-libs=-lstdc++ to FFmpegs configure] https://trac.ffmpeg.org/ticket/1539
+#			'sed -i.bak \'s/-lmodplug.*/-lmodplug -lstdc++/\' "{pkg_config_path}/libmodplug.pc"', # huh ?? c++?
+#		),
+#		'_info' : { 'version' : '0.8.9.0', 'fancy_name' : 'libmodplug' },
+#	},
