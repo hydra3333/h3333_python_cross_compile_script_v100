71ecefe8f9b9615595e21fa3bdeeedefec9d6de3
hydra3333 <hydra3333@gmail.com>
2019-12-19 19:14:16 +1030

    fix a hash

--- packages/dependencies/libbs2b.py
+++ packages/dependencies/libbs2b.py
@@ -44,7 +44,7 @@
 #			{ "url" : "http://sourceforge.mirrorservice.org/b/bs/bs2b/libbs2b/3.1.0/libbs2b-3.1.0.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "6aaafd81aae3898ee40148dd1349aab348db9bfae9767d0e66e0b07ddd4b2528" }, ], },
 #		],
 #		'configure_options': '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static',
-3		'custom_cflag' : '{original_cflags}', # 2019.10.19 D_FORTIFY_SOURCE=0' # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
+#		'custom_cflag' : '{original_cflags}', # 2019.10.19 D_FORTIFY_SOURCE=0' # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
 #		'env_exports' : {
 #			"ac_cv_func_malloc_0_nonnull" : "yes", # fixes undefined reference to `rpl_malloc'
 #			'PKGCONFIG' : 'pkg-config',
