c9a4609df1cdf0b8e7ef5d38d55f9f532047bb5e
hydra3333 <hydra3333@gmail.com>
2019-12-21 01:20:40 +1030

    one more try to avert the build error

--- packages/dependencies/libssh2.py
+++ packages/dependencies/libssh2.py
@@ -2,7 +2,7 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/libssh2/libssh2.git',
 	#'configure_options' : '{autoconf_prefix_options} --disable-examples-build --disable-shared --enable-static --disable-examples-build --with-crypto=openssl ', # 2019.12.13
-	'configure_options' : ' --with-sysroot="{target_sub_prefix}" --host="{target_host}" --prefix="{target_prefix}" --disable-examples-build --disable-shared --enable-static --disable-examples-build --with-crypto=openssl ', # 2019.12.13
+	'configure_options' : ' --host="{target_host}" --prefix="{target_prefix}" --disable-examples-build --disable-shared --enable-static --disable-examples-build --with-crypto=openssl ', # 2019.12.13
 	'env_exports' : {
 		'LIBS' : '-lcrypt32' # Otherwise: libcrypto.a(e_capi.o):e_capi.c:(.text+0x476d): undefined reference to `__imp_CertFreeCertificateContext'
 	},
