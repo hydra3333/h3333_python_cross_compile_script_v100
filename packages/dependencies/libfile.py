2bba16f481f57c853c4e0362902a8eeb5f60b72e
hydra3333 <hydra3333@gmail.com>
2019-12-20 17:27:40 +1030

    Add files via upload

--- packages/dependencies/libfile.py
+++ packages/dependencies/libfile.py
@@ -1,11 +1,13 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/file/file.git',
+	#'branch' : '24c9c086cd7c55b7b0a003a145b32466468e2608',
+	#'depth_git' : 0,
 	'rename_folder' : 'libfile.git',
 	'patches' : [
 		( 'libfile/file-win32.patch', '-p1' ),
 	],
-	'configure_options' : '{autoconf_prefix_options} --enable-fsect-man5',
+	'configure_options' : '{autoconf_prefix_options} -disable-shared --enable-static --enable-fsect-man5',
 	'env_exports' : { 'TARGET_CFLAGS' : '{original_cflags}' },
 	'run_post_patch' : [
 		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/seccomp.c',
--- packages/dependencies/libfile_local.py
+++ packages/dependencies/libfile_local.py
@@ -1,20 +1,26 @@
 {
 	'repo_type' : 'git',
-	'branch' : '24c9c086cd7c55b7b0a003a145b32466468e2608',
-	'depth_git' : 0,
 	'url' : 'https://github.com/file/file.git',
+	#'branch' : '24c9c086cd7c55b7b0a003a145b32466468e2608',
+	#'depth_git' : 0,	
 	'rename_folder' : 'libfile_local.git',
-	'configure_options' : '--prefix={target_prefix} --disable-shared --enable-static --enable-fsect-man5',
+	'patches' : [
+		( 'libfile/file-win32.patch', '-p1' ),
+	],
+	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static --enable-fsect-man5',
 	'needs_make' : False,
 	'env_exports' : { 'TARGET_CFLAGS' : '{original_cflags}' },
-	'run_post_patch' : [ 'autoreconf -fiv' ],
+	'run_post_patch' : [ 
+		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/seccomp.c',
+		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/compress.c',
+		'autoreconf -fiv' ],
 	'update_check' : { 'type' : 'git', },
-	'_info' : { 'version' : 'git (24c9c086cd7c55b7b0a003a145b32466468e2608)', 'fancy_name' : 'libfile (bootstrap)' },
+	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libfile (bootstrap)' },
 }
 # 2019.12.13 old:
 #	'libfile_local' : { # the local variant is for bootstrapping, please make sure to always keep both at the same commit, otherwise it could fail.
 #		'repo_type' : 'git',
-#		'branch' : '24c9c086cd7c55b7b0a003a145b32466468e2608', #'bf8b5f2cf7ce59ae2170e7f2fb026182c4dddcdc', # '4091ea8660a4355b0379564dc901e06bdcdc8c50', #'42d9a8a34607e8b0336b4c354cd5e7e7692bfec7',
+#		#'branch' : '24c9c086cd7c55b7b0a003a145b32466468e2608', #'bf8b5f2cf7ce59ae2170e7f2fb026182c4dddcdc', # '4091ea8660a4355b0379564dc901e06bdcdc8c50', #'42d9a8a34607e8b0336b4c354cd5e7e7692bfec7',
 #		'url' : 'https://github.com/file/file.git',
 #		'rename_folder' : 'libfile_local.git',
 #		'configure_options' : '--prefix={target_prefix} --disable-shared --enable-static --enable-fsect-man5',
