7e47538962cdeb7a37067e1fe76037a7182d10cc
hydra3333 <hydra3333@gmail.com>
2019-12-21 16:58:14 +1030

    see if they build without this error
      CCLD     file.exe
    make[3]: Leaving directory '/home/u/Desktop/_working/workdir/x86_64/libfile.git/src'
    make[2]: Leaving directory '/home/u/Desktop/_working/workdir/x86_64/libfile.git/src'
    Making all in magic
    make[2]: Entering directory '/home/u/Desktop/_working/workdir/x86_64/libfile.git/magic'
    Cannot use the installed version of file (5.32) to
    cross-compile file 5.38
    Please install file 5.38 locally first
    Makefile:823: recipe for target 'magic.mgc' failed
    make[2]: *** [magic.mgc] Error 1
    make[2]: Leaving directory '/home/u/Desktop/_working/workdir/x86_64/libfile.git/magic'
    Makefile:401: recipe for target 'all-recursive' failed
    make[1]: *** [all-recursive] Error 1
    make[1]: Leaving directory '/home/u/Desktop/_working/workdir/x86_64/libfile.git'
    Makefile:333: recipe for target 'all' failed
    make: *** [all] Error 2

--- packages/dependencies/libfile.py
+++ packages/dependencies/libfile.py
@@ -1,13 +1,15 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/file/file.git',
-	# 'branch' : '24c9c086cd7c55b7b0a003a145b32466468e2608',
+	'depth_git' : 0,
+	'branch' : '24c9c086cd7c55b7b0a003a145b32466468e2608',
 	'rename_folder' : 'libfile.git',
 	'patches' : [
 		( 'libfile/file-win32.patch', '-p1' ),
 	],
-	'configure_options' : '{autoconf_prefix_options} --enable-fsect-man5',
-	'depends_on' : [ 'mingw-libgnurx' ],  # ,'libfile_local' ],
+	'configure_options' : '{autoconf_prefix_options} -disable-shared --enable-static --enable-fsect-man5 --disable-silent-rules',
+	#'depends_on' : [ 'mingw-libgnurx' ],  # ,'libfile_local' ],
+	'depends_on' : [ 'mingw-libgnurx', 'libfile_local' ],
 	'env_exports' : { 'TARGET_CFLAGS' : '{original_cflags}' },
 	'run_post_patch' : [
 		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/seccomp.c',
@@ -16,31 +18,8 @@
 	],
 	'flipped_path' : True,
 	'update_check' : { 'type' : 'git', },
-	'_info' : { 'version' : None, 'fancy_name' : 'file' },
+	'_info' : { 'version' : 'git', 'fancy_name' : 'file' },
 }
-#{
-#	'repo_type' : 'git',
-#	'branch' : '24c9c086cd7c55b7b0a003a145b32466468e2608',
-#	#'branch' : '4020d6819dd155ab2780ce6daa9e18e76621a190',
-#	'depth_git' : 0,
-#	'rename_folder' : 'libfile.git',
-#	'patches' : [
-#		( 'libfile/file-win32.patch', '-p1' ),
-#	],
-#	'configure_options' : '{autoconf_prefix_options} -disable-shared --enable-static --enable-fsect-man5',
-#	'needs_make' : True,
-#	'env_exports' : { 'TARGET_CFLAGS' : '{original_cflags}' },
-#	'run_post_patch' : [
-#		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/seccomp.c',
-#		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/compress.c',
-#		'autoreconf -fiv' 
-#	],
-#	'depends_on' : [ 'mingw-libgnurx', 'libfile_local' ],
-#	'flipped_path' : True,
-#	'update_check' : { 'type' : 'git', },
-#	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'file' },
-#}
-#
 # 2019.12.13 old:
 #	'libfile' : {
 #		'repo_type' : 'git',
@@ -61,3 +40,16 @@
 #		'flipped_path' : True,
 #		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'file' },
 #	},
+#
+#AC_CONFIG_HEADERS([config.h])
+#[  --disable-elf            disable builtin ELF support],
+#[  --disable-elf-core       disable ELF core file support],
+#[AS_HELP_STRING([--disable-zlib], [disable zlib compression support @<:@default=auto@:>@])])
+#[AS_HELP_STRING([--disable-bzlib], [disable bz2lib compression support @<:@default=auto@:>@])])
+#[AS_HELP_STRING([--disable-xzlib], [disable liblzma/xz compression support @<:@default=auto@:>@])])
+#[AS_HELP_STRING([--disable-libseccomp], [disable libseccomp sandboxing @<:@default=auto@:>@])])
+#[  --enable-fsect-man5      enable file formats in man section 5],
+#[  --disable-warnings	disable compiler warnings],
+#if test "$MINGW" = 1; then
+#  AC_CHECK_LIB(gnurx,regexec,,AC_MSG_ERROR([libgnurx is required to build file(1) with MinGW]))
+#fi
--- packages/dependencies/libfile_local.py
+++ packages/dependencies/libfile_local.py
@@ -1,37 +1,21 @@
 {
 	'repo_type' : 'git',
+	'depth_git' : 0,
 	'branch' : '24c9c086cd7c55b7b0a003a145b32466468e2608',
 	'url' : 'https://github.com/file/file.git',
 	'rename_folder' : 'libfile_local.git',
-	'configure_options' : '--prefix={target_prefix} --disable-shared --enable-static --enable-fsect-man5',
+	'configure_options' : '--prefix={target_prefix} --disable-shared --enable-static --enable-fsect-man5 --disable-silent-rules',
 	'needs_make' : False,
 	'env_exports' : { 'TARGET_CFLAGS' : '{original_cflags}' },
-	'run_post_patch' : [ 'autoreconf -fiv' ],
+	'run_post_patch' : [
+		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/seccomp.c',
+		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/compress.c',
+		'autoreconf -fiv' 
+	],
+	'depends_on' : [ 'mingw-libgnurx', ],
 	'update_check' : { 'type' : 'git', },
-	'_info' : { 'version' : None, 'fancy_name' : 'libfile (bootstrap)' },
+	'_info' : { 'version' : 'git', 'fancy_name' : 'libfile (bootstrap)' },
 }
-#{
-#	'repo_type' : 'git',
-#	'url' : 'https://github.com/file/file.git',
-#	'branch' : '24c9c086cd7c55b7b0a003a145b32466468e2608',
-#	#'branch' : '4020d6819dd155ab2780ce6daa9e18e76621a190',
-#	'depth_git' : 0,	
-#	'rename_folder' : 'libfile_local.git',
-#	'patches' : [
-#		( 'libfile/file-win32.patch', '-p1' ),
-#	],
-#	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static --enable-fsect-man5',
-#	'needs_make' : False,
-#	'env_exports' : { 'TARGET_CFLAGS' : '{original_cflags}' },
-#	'run_post_patch' : [ 
-#		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/seccomp.c',
-#		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/compress.c',
-#		'autoreconf -fiv' ],
-#	'depends_on' : [ 'mingw-libgnurx', ],
-#	'update_check' : { 'type' : 'git', },
-#	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libfile_local (bootstrap)' },
-#}
-#
 # 2019.12.13 old:
 #	'libfile_local' : { # the local variant is for bootstrapping, please make sure to always keep both at the same commit, otherwise it could fail.
 #		'repo_type' : 'git',
@@ -44,3 +28,16 @@
 #		'run_post_patch' : [ 'autoreconf -fiv' ],
 #		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libfile (bootstrap)' },
 #	},
+#
+#AC_CONFIG_HEADERS([config.h])
+#[  --disable-elf            disable builtin ELF support],
+#[  --disable-elf-core       disable ELF core file support],
+#[AS_HELP_STRING([--disable-zlib], [disable zlib compression support @<:@default=auto@:>@])])
+#[AS_HELP_STRING([--disable-bzlib], [disable bz2lib compression support @<:@default=auto@:>@])])
+#[AS_HELP_STRING([--disable-xzlib], [disable liblzma/xz compression support @<:@default=auto@:>@])])
+#[AS_HELP_STRING([--disable-libseccomp], [disable libseccomp sandboxing @<:@default=auto@:>@])])
+#[  --enable-fsect-man5      enable file formats in man section 5],
+#[  --disable-warnings	disable compiler warnings],
+#if test "$MINGW" = 1; then
+#  AC_CHECK_LIB(gnurx,regexec,,AC_MSG_ERROR([libgnurx is required to build file(1) with MinGW]))
+#fi
