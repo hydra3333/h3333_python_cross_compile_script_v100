dbe9b3f71cee956b97172a6abfc003a8eee95af3
hydra3333 <hydra3333@gmail.com>
2019-12-22 13:57:23 +1030

    Add files via upload

--- packages/dependencies/rtmpdump.py
+++ packages/dependencies/rtmpdump.py
@@ -2,14 +2,15 @@
 	'repo_type' : 'git',
 	'url' : 'https://git.ffmpeg.org/rtmpdump.git',
 	'needs_configure' : False,
-	'install_options' : 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lpthread -lgnutls -lhogweed -lnettle -lgmp -lcrypt32 -lws2_32 -lwinmm -lz -liconv -lintl -liconv -lssp" OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}', # 2019.12.13
-	'build_options' : 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lgnutls -lhogweed -lnettle -lgmp -lcrypt32 -lws2_32 -lwinmm -lz -liconv -lintl -liconv -lssp"	OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}',  # 2019.12.13
+	#'install_options' : 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lpthread -lgnutls -lhogweed -lnettle -lgmp -lcrypt32 -lws2_32 -lwinmm -lz -liconv -lintl -liconv -lssp" OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}', # 2019.12.13
+	#'build_options'   : 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lpthread -lgnutls -lhogweed -lnettle -lgmp -lcrypt32 -lws2_32 -lwinmm -lz -liconv -lintl -liconv -lssp"	OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}',  # 2019.12.13
+	'install_options' : 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lpthread -lgnutls -lhogweed -lnettle -lgmp -lcrypt32 -lwinmm -lz -liconv -lintl -liconv -lssp" OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}', # 2019.12.13
+	'build_options'   : 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lpthread -lgnutls -lhogweed -lnettle -lgmp -lcrypt32 -lwinmm -lz -liconv -lintl -liconv -lssp"	OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}',  # 2019.12.13
     'run_post_install' :( # 2019.12.13
 	#	'sed -i.bak \'s/-lrtmp -lz/-lrtmp -lwinmm -lz/\' "{pkg_config_path}/librtmp.pc"',
         'sed -i.bak \'s/-lrtmp -lz/-lrtmp -lwinmm -lz -lintl -liconv -lssp/\' "{pkg_config_path}/librtmp.pc"', # 2019.12.13
 	),
 	'depends_on' : [
-		#'gnutls', 'zlib', # 2019.12.13
         'iconv', 'gnutls', 'zlib', 'gettext', 'libgcrypt', # 2019.12.13
 	],
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'rtmpdump' },
@@ -21,7 +22,7 @@
 #		'needs_configure': False,
 #		# doesn't compile with openssl1.1
 #		'install_options': 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lgnutls -lhogweed -lnettle -lgmp -lcrypt32 -lwinmm -lz -liconv -lintl -liconv -lssp" OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}', # 2018.11.23 added -lwinmm  -liconv -lintl -liconv
-#		'build_options': 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lgnutls -lhogweed -lnettle -lgmp -lcrypt32 -lwinmm -lz -liconv -lintl -liconv -lssp"	OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}', # 2018.11.23 added -lwinmm  -liconv -lintl -liconv
+#		  'build_options': 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lgnutls -lhogweed -lnettle -lgmp -lcrypt32 -lwinmm -lz -liconv -lintl -liconv -lssp"	OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}', # 2018.11.23 added -lwinmm  -liconv -lintl -liconv
 #		'env_exports' : {
 #			'CFLAGS'   : ' {original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
 #			'CXXFLAGS' : ' {original_cflags}', # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
