7e235757eedeeec700d8701c8db8daaed1c89fa7
hydra3333 <hydra3333@gmail.com>
2019-12-22 23:11:39 +1030

    Add files via upload

--- packages/products/fftw3_dll.py
+++ packages/products/fftw3_dll.py
@@ -7,7 +7,12 @@
 	'recursive_git' : False,
 	'is_dep_inheriter' : True, # try without, for it to be a product and the script runs
 	'depends_on' : [
-		'fftw3_dll_single', 'fftw3_dll_double', 'fftw3_dll_ldouble', 'fftw3_dll_quad', 
+		'fftw3_dll_single', 'fftw3_dll_double', 'fftw3_dll_ldouble', # 'fftw3_dll_quad' missing library 
+		#                                                            # *** Warning: This system cannot link to static lib archive
+		#	                                                         # (snip)/libquadmath.la.
+		#	                                                         # *** I have the capability to make that library automatically link in when
+		#	                                                         # *** you link to this library.  But I can only do this if you have a
+		#	                                                         # *** shared version of the library, which you do not appear to have.
 	],
 	'_info' : { 'version' : '3.3.8', 'fancy_name' : 'fftw3 shared DLLs' },
 }
