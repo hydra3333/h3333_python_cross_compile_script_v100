8a9445e4df532ecf2dada7fc354a1561eab56db6
hydra3333 <hydra3333@gmail.com>
2019-12-23 17:30:39 +1030

    Add files via upload

--- packages/products/youtube-dl.py
+++ packages/products/youtube-dl.py
@@ -6,7 +6,8 @@
 		'sed -i.bak \'s/pandoc.*/touch youtube-dl.1/g\' Makefile', # "disables" doc, the pandoc requirement is so annoyingly big..
 	],
 	'run_post_install' : [
-		'if [ -f "{output_prefix}/youtube-dl_git.installed/bin/youtube-dl" ] ; then mv "{output_prefix}/youtube-dl_git.installed/bin/youtube-dl" "{output_prefix}/youtube-dl_git.installed/bin/youtube-dl.py" ; fi',
+		#'if [ -f "{output_prefix}/youtube-dl_git.installed/bin/youtube-dl" ] ; then mv "{output_prefix}/youtube-dl_git.installed/bin/youtube-dl" "{output_prefix}/youtube-dl_git.installed/bin/youtube-dl.py" ; fi',
+		'mv -fv "{output_prefix}/youtube-dl_git.installed/bin/youtube-dl" "{output_prefix}/youtube-dl_git.installed/bin/youtube-dl.py"',
 	],
 	'build_options' : 'youtube-dl',
 	'patches' : [
