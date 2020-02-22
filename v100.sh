2930362928a167426094a0226dfc8cd8402435b4
hydra3333 <hydra3333@gmail.com>
2020-02-22 11:17:13 +1030

    Add files via upload

--- v100.sh
+++ v100.sh
@@ -16,6 +16,9 @@ curl -4 -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Cache-Control: ma
 rm -f "h3333_v100.001.sh"
 curl -4 -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Cache-Control: max-age=0' "https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.001.sh" --retry 50 -L --output "h3333_v100.001.sh" --fail
 
+rm -f "h3333_v100.001.sh"
+curl -4 -H 'Pragma: no-cache' -H 'Cache-Control: no-cache' -H 'Cache-Control: max-age=0' "https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.001_ff.sh" --retry 50 -L --output "h3333_v100.001_ff.sh" --fail
+
 sudo chmod +777 -R *
 
 ls -al
