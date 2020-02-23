82d8228a44c953ec6113c693903fee4c37e2ffdd
hydra3333 <hydra3333@gmail.com>
2020-02-23 10:33:58 +1030

    Add files via upload

--- packages/variables.py
+++ packages/variables.py
@@ -1,20 +1,17 @@
 {
 	'ffmpeg_min_config' : # the base for all ffmpeg configurations.
 		'--arch={bit_name2} '
-		#'--target-os=mingw32 '
-        '--target-os={target_OS} ' # 2019.12.13 to enable mingw64 for 64-bit target ... {bit_name3} won't yield "mingw32" 
+		'--target-os=mingw32 '
 		'--cross-prefix={cross_prefix_bare} '
 		'--pkg-config=pkg-config '
 		'--pkg-config-flags=--static '
-        '--disable-shared --enable-static ' # 2019.10.31 - I ENFORCE NOTHING BUT STATIC, disable shared !
 		'--disable-w32threads '
-        '--enable-pthreads ' # 2019.12.13
 		'--enable-cross-compile '
 		'--target-exec=wine '
 		'--enable-runtime-cpudetect '
 		'--enable-gpl '
 		'--enable-version3 '
-		'--extra-version=Hydra3333/python_cross_compile_script_v100 '
+		'--extra-version=DeadSix27/python_cross_compile_script '
 
 		# Misc.
 		'--enable-pic '
@@ -27,7 +24,7 @@
 		'--enable-libfribidi '
 		'--enable-libbluray '
 		'--enable-libcdio '
-		'--enable-avisynth ' # 2019.12.13 reconsider this, possibly disable it
+		'--enable-avisynth '
 		'--enable-vapoursynth ' #maybe works?
 		'--enable-librtmp '
 		'--enable-libcaca '
@@ -35,14 +32,13 @@
 		'--enable-libxml2 '
 		'--enable-gmp '
 		'--enable-gnutls ' # nongpl: openssl,libtls(libressl)
-        #'--enable-avresample ' # deprecated. 2018.11.23 ... but LSW depends on it # 2019.11.19 HolyWu's lsw does not need avresample as it uses libswresample
 
 		# Video/Picture Libs
 		'--enable-libzimg '
 		'--enable-libx264 '
-		#'--enable-libopenh264 ' # 2019.12.13 disable libopenh264
+		'--enable-libopenh264 '
 		'--enable-libx265 '
-		# '--enable-libkvazaar ' #2019.12.13 disable libkvazaar
+		'--enable-libkvazaar '
 		'--enable-libvpx '
 		'--enable-libdav1d '
 		'--enable-libaom '
@@ -69,7 +65,6 @@
 		# HW Decoders
 		'--enable-ffnvcodec '
 		'--enable-cuvid '
-        '--enable-cuda-nvcc ' # 2019.10.31 MADE IT TO FREE # 2019.12.13 added it back in
 		'--enable-opengl '
 		'--enable-d3d11va '
 		'--enable-nvenc '
@@ -77,8 +72,8 @@
 		'--enable-dxva2 '
 		'--enable-libmfx '
 		'--enable-amf '
-        '--enable-opencl ' # 2019.12.13, added it, not sure why it wasn't in any of the configs ? depends on opencl_icd
-		#'--enable-vulkan ' 
+		# 20120.02.23 add CUDA stuff 
+		'--enable-cuda-nvcc '
 	,
 
 	'ffmpeg_nonfree': '--enable-nonfree --enable-libfdk-aac --enable-decklink', # --enable-cuda-sdk # nonfree stuff
@@ -100,26 +95,21 @@
 		'--enable-libgme '
 		'--enable-libflite '
 		#'--enable-sdl '
-		#'--enable-libdavs2 '
-		#'--enable-libxavs '
-		#'--enable-libxavs2 '
+		'--enable-libdavs2 '
+		'--enable-libxavs '
+		'--enable-libxavs2 '
 		'--enable-libopenmpt '
 		'--enable-libmysofa '
 		'--enable-libvidstab '
 		'--enable-libmodplug '
-		'--disable-schannel ' # 2019.12.13 deadsix27 had this commented out, un-comment it
+		#'--disable-schannel '
 		#'--disable-gcrypt '
-		#'--enable-ladspa ' # 2019.12.13 deadsix27 had this commented out, review it later
+		#'--enable-ladspa '
 		# '--enable-libcodec2 ' # Requires https://github.com/traviscross/freeswitch/tree/master/libs/libcodec2, too lazy to split that off.
 		# '--enable-libvmaf '
 		# '--extra-libs="-lpsapi" '
 		# '--extra-libs="-liconv" ' # -lschannel #-lsecurity -lz -lcrypt32 -lintl -liconv -lpng -loleaut32 -lstdc++ -lspeexdsp -lpsapi
 		# '--extra-cflags="-DLIBTWOLAME_STATIC" '
 		# '--extra-cflags="-DMODPLUG_STATIC" '
-        '--extra-libs="-lpsapi -lintl -liconv -lssp" ' # 2019.12.13 add  -lssp for -fstack-protector-all
-        '--extra-cflags="-DLIBTWOLAME_STATIC" ' # 2019.12.13 addded back in
-		'--extra-cflags="-DMODPLUG_STATIC"  ' # 2019.12.13 addded back in
-		'--extra-cflags="-DLIBXML_STATIC" ' # 2019.12.13 addded back in
-		'--extra-cflags="-DGLIB_STATIC_COMPILATION" ' # 2019.12.13 addded back in
 	,
 }
