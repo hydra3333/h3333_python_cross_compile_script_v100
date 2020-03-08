d3c7e4d74225579d9395f33a4d2d3962df3878c9
hydra3333 <hydra3333@gmail.com>
2020-03-08 13:24:45 +1030

    pcre to 10.44

--- packages/dependencies/pcre.py
+++ packages/dependencies/pcre.py
@@ -1,10 +1,9 @@
 { # pcre added 2019.12.13  ... superseded by pcre2 ? Nope, not for glib2
 	'repo_type' : 'archive',
 	'download_locations' : [
-	                                                                                                            
-		#{ "url" : "https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "91e762520003013834ac1adb4a938d53b22a216341c061b0cf05603b290faf6b" }, ], },
-		#{ "url" : "https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "ae236dc25d7e0e738a94e103218e0085eb02ff9bd98f637b6e061a48decdb433" }, ], },
-		{ "url" : "https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "0b8e7465dc5e98c757cc3650a20a7843ee4c3edf50aaf60bb33fd879690d2c73" }, ], },
+		#{ "url" : "https://ftp.pcre.org/pub/pcre/pcre-8.43.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "0b8e7465dc5e98c757cc3650a20a7843ee4c3edf50aaf60bb33fd879690d2c73" }, ], },
+		{ "url" : "https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "19108658b23b3ec5058edc9f66ac545ea19f9537234be1ec62b714c84399366d" }, ], },
+		{ "url" : "https://fossies.org/linux/misc/pcre-8.44.tar.bz2", "hashes" : [ { "type" : "sha256", "sum" : "19108658b23b3ec5058edc9f66ac545ea19f9537234be1ec62b714c84399366d" }, ], },
 	],
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --enable-unicode-properties --enable-utf --enable-pcre8 --enable-pcre16 --enable-pcre32 --enable-pcregrep-libz --enable-pcregrep-libbz2 --enable-newline-is-anycrlf --disable-pcre2test-libedit --disable-pcretest-libreadline --enable-jit ', 
 	'depends_on' : [
