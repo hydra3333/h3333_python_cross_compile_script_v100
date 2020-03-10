1df9c4369b0690ef84ea2e5976a8bd53ac554916
hydra3333 <hydra3333@gmail.com>
2020-03-10 18:33:16 +1030

    Add files via upload

--- v100.sh
+++ v100.sh
@@ -11,6 +11,12 @@ sudo apt upgrade -y
 sudo apt install -y apt-utils debconf debconf-utils
 sudo apt install -y curl wget nano rsync curl wget nano
 
+sudo apt install -y locales
+sudo locale-gen en_AU.UTF-8
+sudo update-locale LANG='en_AU.UTF-8' LANGUAGE='en_AU:en' LC_ALL='en_AU.UTF-8'
+export LANG='en_AU.UTF-8' LANGUAGE='en_AU:en' LC_ALL='en_AU.UTF-8'
+
+
 curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.setup.sh -O
 curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.001.sh -O
 curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.001_ff.sh -O
