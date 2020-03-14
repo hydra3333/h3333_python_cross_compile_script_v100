fc5cfa18f8f9aa3b54eb52cea651091942c0ef5d
hydra3333 <hydra3333@gmail.com>
2020-03-14 14:28:40 +1030

    sdl2 try release-2.0.12

--- packages/dependencies/sdl2.py
+++ packages/dependencies/sdl2.py
@@ -2,7 +2,8 @@
 	'repo_type' : 'mercurial',
 	'url' : 'https://hg.libsdl.org/SDL',
 	'depth_git' : 0,
-	#'branch' : 'aa6c25160e67', # see if it fixes: error: 'SDL_Joystick' {aka 'struct _SDL_Joystick'} has no member named 'player_index'
+	#'branch' : 'aa6c25160e67', # 2020.03.14 this old commit works
+	'branch' : '355a4f94a782', # Release 2.0.12
 	'folder_name' : 'sdl2_hg',
 	'source_subfolder': '_build',
 	'conf_system' : 'cmake',
