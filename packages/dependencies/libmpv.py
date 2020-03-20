c115eaeaf0709343f2e099d818dbceba4d969db8
hydra3333 <hydra3333@gmail.com>
2020-03-20 12:59:04 +1030

    Add files via upload

--- packages/dependencies/libmpv.py
+++ packages/dependencies/libmpv.py
@@ -9,33 +9,46 @@
 	#	'TARGET'  : '{target_host}',
 	#	'LDFLAGS' : '-ld3d11',
 	#},
-		'env_exports' : {
+	'env_exports' : {
 		'DEST_OS' : '{bit_name_win}', #'DEST_OS' : 'win32',
 		'TARGET'  : '{target_host}',
 		'PKG_CONFIG' : 'pkg-config',
 		#'LDFLAGS': '-Wl,-Bdynamic -lvulkan-1 -fstack-protector-strong' # see my 'custom_ldflag' instead
 	},
 	'custom_cflag'  : ' -O3 ',
-	'custom_ldflag' : ' -Wl,-Bdynamic -lvulkan-1 -fstack-protector-strong -ld3d11 ',
-	'run_post_patch' : [ # 2020.03.19 not sure about this, it is not in mpv.py
-		'cp -nv "/usr/bin/pkg-config" "{cross_prefix_full}pkg-config"',
-		'sed -i.bak "s/encoder_encode/mpv_encoder_encode/" common/encode_lavc.h', # Dirty work-around for xavs2, no idea how else to fix this.
-		'sed -i.bak "s/encoder_encode/mpv_encoder_encode/" video/out/vo_lavc.c',  #
-		'sed -i.bak "s/encoder_encode/mpv_encoder_encode/" audio/out/ao_lavc.c',  #
-		'sed -i.bak "s/encoder_encode/mpv_encoder_encode/" common/encode_lavc.c', #
-	],
+	'custom_ldflag' : ' -Wl,-Bdynamic -lvulkan-1 -fstack-protector-strong -ld3d11 ', # 2020.03.19 added -ld3d11 per from libmpv.py
+	#'run_post_patch' : [ # 2020.03.19 not sure about this, it is not in mpv.py
+	#	'cp -nv "/usr/bin/pkg-config" "{cross_prefix_full}pkg-config"',
+	#	'sed -i.bak "s/encoder_encode/mpv_encoder_encode/" common/encode_lavc.h', # Dirty work-around for xavs2, no idea how else to fix this.
+	#	'sed -i.bak "s/encoder_encode/mpv_encoder_encode/" video/out/vo_lavc.c',  #
+	#	'sed -i.bak "s/encoder_encode/mpv_encoder_encode/" audio/out/ao_lavc.c',  #
+	#	'sed -i.bak "s/encoder_encode/mpv_encoder_encode/" common/encode_lavc.c', #
+	#],
 	'configure_options' :
+		'--force '
 		'--enable-libmpv-shared '
+		'--enable-static-build '
 		'--disable-debug-build '
+		'--disable-html-build '
+		'--disable-android '
+		'--disable-egl-android '
+		'--disable-tvos '
+		'--disable-swift '
+		'--enable-iconv '
+		'--enable-zlib '
+		'--enable-zimg '
+		'--enable-libavdevice '
+		'--enable-cuda-hwaccel '
+		'--enable-cuda-interop '
 		'--prefix={target_prefix} '
-		'--enable-sdl2 '
+		'--enable-sdl2 ' # 2020.03.19 added 
 		'--enable-rubberband '
 		'--enable-lcms2 '
-		#'--enable-openal '
-		'--enable-dvdread '
+		# '--enable-openal '
+		# '--enable-dvdread ' # commented out, no such option
 		'--enable-dvdnav '
 		'--enable-libbluray '
-		#'--enable-egl-angle-lib '
+		#'--enable-egl-angle-lib ' # not maintained anymore apparently, crashes for me anyway.
 		'--disable-xv '
 		'--disable-alsa '
 		'--disable-pulse '
@@ -45,43 +58,44 @@
 		'--disable-wayland-protocols '
 		'--disable-wayland-scanner '
 		'--enable-cdda '
-		'--enable-libass '
+		#'--enable-libass ' # commented out, no such option
 		'--enable-lua '
 		'--enable-vapoursynth '
-		'--enable-encoding '
 		'--enable-uchardet '
 		'--enable-vulkan '
 		'--enable-libplacebo '
 		'--enable-libarchive '
 		'--enable-javascript '
 		'--disable-manpage-build '
-		'--enable-pdf-build '
+		'--disable-pdf-build '
 		'TARGET={target_host} '
-		'DEST_OS={bit_name_win} ' # 2020.03.19 changed from win32 '
+		'DEST_OS={bit_name_win} ' # 2020.03.19 changed from 'DEST_OS=win32 '
 	,
 	'depends_on' : [
-		'libffmpeg', 
-		'zlib',
-		'iconv',
-		'python3_libs', 
-		'vapoursynth_libs',
-		'sdl2', 
-		'luajit', 
-		'rubberband',
-		'lcms2', 
-		'libdvdread',
-		'libdvdnav', 
-		'libbluray', 
-		#'openal', 
-		'libass', 
-		'libcdio-paranoia', 
-		'libjpeg-turbo', 
-		'uchardet', 
-		'libarchive', 
-		'mujs', 
-		'shaderc', 
+		'opencl_icd',
 		'vulkan_loader',
+		'zlib',
+		'libzimg',
+		'iconv',
+		'python3_libs',
+		'vapoursynth_libs',
+		'sdl2',
+		'luajit',
+		'rubberband',
+		'lcms2',
+		'libdvdread',
+		'libdvdnav',
+		'libbluray',
+		#'openal',
+		'libass',
+		'libcdio-paranoia',
+		'libjpeg-turbo',
+		'uchardet',
+		'libarchive',
+		'mujs',
+		'shaderc',
 		'libplacebo',
+		'libffmpeg',
 	],
 	# Dirty hack, so far I've found no way to get -Wl,-Bdynamic into the .pc file or mpv itself without the use of LDFLAGS...
 	'regex_replace': {
