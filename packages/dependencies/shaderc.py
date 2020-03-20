636a86124ff415a00e44de97e6714c2ee956a753
hydra3333 <hydra3333@gmail.com>
2020-03-20 12:19:27 +1030

    Add files via upload

--- packages/dependencies/shaderc.py
+++ packages/dependencies/shaderc.py
@@ -2,7 +2,7 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/google/shaderc.git',
 	'depth_git': 0, # 2020.03.11 per deadsix27 stay on last working commit
-	# 'branch': 'f53792645f0696b8954cfdb3c213f96799dd89b2', # 2020.03.19 comment out
+	'branch': 'f53792645f0696b8954cfdb3c213f96799dd89b2', # 2020.03.19 uncomment again # 2020.03.19 comment out
 	'configure_options' :
 		'cmake .. {cmake_prefix_options} '
 		'-DCMAKE_BUILD_TYPE=Release '
