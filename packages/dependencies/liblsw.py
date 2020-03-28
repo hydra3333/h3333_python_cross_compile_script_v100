0958dc716f4f85e930ec3b1683035556e37f3e11
hydra3333 <hydra3333@gmail.com>
2020-03-28 14:45:59 +1030

    Add files via upload

--- packages/dependencies/liblsw.py
+++ packages/dependencies/liblsw.py
@@ -2,13 +2,21 @@
 		'repo_type' : 'git',
 		'url' : 'https://github.com/HolyWu/L-SMASH-Works.git', # 2019.11.19 swap to HolyWu's fork as it seems more updated
 		'depth_git' : 0,
-		'branch' : '86f757d4096de3abdd1970202dc33fbaa8c8b640',
+		#'branch' : 'a2fd2e0c625a33b2f1af356689f2091bb677c9e0', #'86f757d4096de3abdd1970202dc33fbaa8c8b640',
 		'env_exports' : {
 			'PKGCONFIG' : 'pkg-config',
 		},
 		'conf_system' : 'meson',
 		'build_system' : 'ninja',
 		'source_subfolder' : 'VapourSynth/build', # 'VapourSynth', # 'build',
+		'run_post_patch' : [
+			#'ls -al',
+			#'ls -al ..',
+			'sed -i.BAK \'s;<VapourSynth.h>;"VapourSynth.h";g\' "../lsmashsource.h"',
+			'sed -i.BAK "s;vapoursynth_dep =;includes = include_directories(\'../include\')\\n\\nvapoursynth_dep =;g" "../meson.build"',
+			'sed -i.BAK \'s;dependencies : deps,;dependencies : deps,\\ninclude_directories : includes,;g\' "../meson.build"',
+			#'sed -iBAK \'s;;;g\' "../"',
+		],
 		'configure_options' :
 			'--prefix={target_prefix} '
 			'--libdir={target_prefix}/lib '
