dc9d902561cb56b9da2a671c391ad48eec07617d
hydra3333 <hydra3333@gmail.com>
2020-03-29 15:40:20 +1030

    Add files via upload

--- packages/dependencies/vapoursynth_libs.py
+++ packages/dependencies/vapoursynth_libs.py
@@ -5,6 +5,9 @@
 	'needs_make_install' : False,
 	'depends_on' : [ 'python3_libs' ],
 	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool VAPOURSYNTH_VERSION=R48 PYTHON_VERSION=3.7.5', # 2019.12.13 added PYTHON_VERSION=3.7.5
+	'run_post_install' : (
+		'cp -fv "{target_prefix}/include/vapoursynth/VapourSynth.h" "{target_prefix}/include/VapourSynth.h" ',
+	),
 	'packages' : {
 		'arch' : [ '7za' ],
 	},
