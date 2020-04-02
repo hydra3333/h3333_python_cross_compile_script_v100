00354f977a6b93d03b45ed0c4ecf5d15fb7a118c
hydra3333 <hydra3333@gmail.com>
2020-04-02 12:39:46 +1030

    vapoursynth R49 fails to build, backout to R48

--- packages/dependencies/python3_libs.py
+++ packages/dependencies/python3_libs.py
@@ -3,13 +3,19 @@
 	'url' : 'https://github.com/DeadSix27/python_mingw_libs.git',
 	'needs_configure' : False,
 	'needs_make_install' : False,
-	'run_post_patch' : [
-		'sed -i.bak "s;3.7.5;3.8.2;g" "Makefile"',
-		'sed -i.bak "s;\'3.7.5\';\'3.7.5\',\'3.8.2\';g" "install_python_libs.py"',
-		'sed -i.bak "s; 3.7.5 ; 3.8.2 ;g" "install_python_libs.py"',
-	],
-	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool PYTHON_VERSION=3.8.2',
-	'_info' : { 'version' : '3.8.2', 'fancy_name' : 'Python (library-only)' },
+	#
+	# python 3.7.5
+	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool PYTHON_VERSION=3.7.5',
+	'_info' : { 'version' : '3.7.5', 'fancy_name' : 'Python (library-only)' },
+	#
+	# python 3.8.2
+	#'run_post_patch' : [
+	#	'sed -i.bak "s;3.7.5;3.8.2;g" "Makefile"',
+	#	'sed -i.bak "s;\'3.7.5\';\'3.7.5\',\'3.8.2\';g" "install_python_libs.py"',
+	#	'sed -i.bak "s; 3.7.5 ; 3.8.2 ;g" "install_python_libs.py"',
+	#],
+	#'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool PYTHON_VERSION=3.8.2',
+	#
 }
 # 2019.12.13 old:
 #	'python3_libs': {
