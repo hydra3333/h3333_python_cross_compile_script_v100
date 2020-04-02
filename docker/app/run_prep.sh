03a3a820f05fe108c0cedf19c2a2d9f24c5b7933
hydra3333 <hydra3333@gmail.com>
2020-04-02 12:05:08 +1030

    Add files via upload

--- docker/app/run_prep.sh
+++ docker/app/run_prep.sh
@@ -23,14 +23,25 @@ ls -al /VM
 ls -al /VM/exe_x64_py
 
 
-curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.setup.sh -O -L
-curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.001.sh -O -L
-curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.001_ff.sh -O -L
+#curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.setup.sh -O -L
+#curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.001.sh -O -L
+#curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.001_ff.sh -O -L
 
-sed -i.bak 's;sudo ;;g' ./*.sh
-sed -i.bak 's;~/Desktop;/;g' ./*.sh
-sed -i.bak 's;/home/u/Desktop;/;g' ./*.sh
-sed -i.bak 's;./exe_x64_py;/VM/exe_x64_py;g' ./*.sh
+sed -i.bak 's;sudo ;;g' ./h3333_v100.setup.sh
+sed -i.bak 's;~/Desktop;/;g' ./h3333_v100.setup.sh
+sed -i.bak 's;/home/u/Desktop;/;g' ./h3333_v100.setup.sh
+sed -i.bak 's;./exe_x64_py;/VM/exe_x64_py;g' ./h3333_v100.setup.sh
+
+
+sed -i.bak 's;sudo ;;g' ./h3333_v100.001.sh
+sed -i.bak 's;~/Desktop;/;g' ./h3333_v100.001.sh
+sed -i.bak 's;/home/u/Desktop;/;g' ./h3333_v100.001.sh
+sed -i.bak 's;./exe_x64_py;/VM/exe_x64_py;g' ./h3333_v100.001.sh
+
+sed -i.bak 's;sudo ;;g' ./h3333_v100.001_ff.sh
+sed -i.bak 's;~/Desktop;/;g' ./h3333_v100.001_ff.sh
+sed -i.bak 's;/home/u/Desktop;/;g' ./h3333_v100.001_ff.sh
+sed -i.bak 's;./exe_x64_py;/VM/exe_x64_py;g' ./h3333_v100.001_ff.sh
 
 chmod +777 ./*.sh
 ./h3333_v100.setup.sh
