9fb0761f6c07e2e2da9ec0dcdbe2b32f68fb720a
hydra3333 <hydra3333@gmail.com>
2020-04-02 12:01:37 +1030

    Add files via upload

--- docker/app/run_dl_sh.sh
+++ docker/app/run_dl_sh.sh
@@ -25,24 +25,13 @@ ls -al /VM/exe_x64_py
 curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/docker/app/run_dl_sh.sh -O -L
 curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/docker/app/run_prep.sh -O -L
 curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/docker/app/README.md -O -L
-
 curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.setup.sh -O -L
 curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.001.sh -O -L
 curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.001_ff.sh -O -L
 
-sed -ibak 's;sudo ;;g' ./h3333_v100.001.sh
-sed -ibak 's;~/Desktop;/;g' ./h3333_v100.001.sh
-sed -ibak 's;/home/u/Desktop;/;g' ./h3333_v100.001.sh
-sed -ibak 's;./exe_x64_py;/VM/exe_x64_py;g' ./h3333_v100.001.sh
-
-sed -ibak 's;sudo ;;g' ./h3333_v100.001_ff.sh
-sed -ibak 's;~/Desktop;/;g' ./h3333_v100.001_ff.sh
-sed -ibak 's;/home/u/Desktop;/;g' ./h3333_v100.001_ff.sh
-sed -ibak 's;./exe_x64_py;/VM/exe_x64_py;g' ./h3333_v100.001_ff.sh
-
-sed -ibak 's;sudo ;;g' ./h3333_v100.setup.sh
-sed -ibak 's;~/Desktop;/;g' ./h3333_v100.setup.sh
-sed -ibak 's;/home/u/Desktop;/;g' ./h3333_v100.setup.sh
-sed -ibak 's;./exe_x64_py;/VM/exe_x64_py;g' ./h3333_v100.setup.sh
+sed -ibak 's;sudo ;;g' ./*.sh
+sed -ibak 's;~/Desktop;/;g' ./*.sh
+sed -ibak 's;/home/u/Desktop;/;g' ./*.sh
+sed -ibak 's;./exe_x64_py;/VM/exe_x64_py;g' ./*.sh
 
 chmod +777 ./*.sh
--- docker/app/run_prep.sh
+++ docker/app/run_prep.sh
@@ -22,22 +22,15 @@ ls -al /VM
 
 ls -al /VM/exe_x64_py
 
+
 curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.setup.sh -O -L
-sed -ibak 's;sudo ;;g' ./h3333_v100.setup.sh
-sed -ibak 's;~/Desktop;/;g' ./h3333_v100.setup.sh
-sed -ibak 's;/home/u/Desktop;/;g' ./h3333_v100.setup.sh
-
 curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.001.sh -O -L
-sed -ibak 's;sudo ;;g' ./h3333_v100.001.sh
-sed -ibak 's;~/Desktop;/;g' ./h3333_v100.001.sh
-sed -ibak 's;/home/u/Desktop;/;g' ./h3333_v100.001.sh
-sed -ibak 's;./exe_x64_py;/VM/exe_x64_py;g' ./h3333_v100.001.sh
-
 curl --ipv4 https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/h3333_v100.001_ff.sh -O -L
-sed -ibak 's;sudo ;;g' ./h3333_v100.001_ff.sh
-sed -ibak 's;~/Desktop;/;g' ./h3333_v100.001_ff.sh
-sed -ibak 's;/home/u/Desktop;/;g' ./h3333_v100.001_ff.sh
-sed -ibak 's;./exe_x64_py;/VM/exe_x64_py;g' ./h3333_v100.001_ff.sh
+
+sed -ibak 's;sudo ;;g' ./*.sh
+sed -ibak 's;~/Desktop;/;g' ./*.sh
+sed -ibak 's;/home/u/Desktop;/;g' ./*.sh
+sed -ibak 's;./exe_x64_py;/VM/exe_x64_py;g' ./*.sh
 
 chmod +777 ./*.sh
 ./h3333_v100.setup.sh
