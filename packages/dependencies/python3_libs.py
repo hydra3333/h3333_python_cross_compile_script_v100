e1f8834d24bd5f9fb673cb77f825fc158007fe11
hydra3333 <hydra3333@gmail.com>
2020-04-04 14:05:46 +1030

    Add files via upload

--- packages/dependencies/python3_libs.py
+++ packages/dependencies/python3_libs.py
@@ -22,4 +22,5 @@
 		'diff -U 5 install_python_libs.py.orig install_python_libs.py && echo "NO difference" || echo "YES differences!"',
 	],
 	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool PYTHON_VERSION=3.8.2',
+	'_info' : { 'version' : '3.8.2', 'fancy_name' : 'Python (library-only)' },
 }
