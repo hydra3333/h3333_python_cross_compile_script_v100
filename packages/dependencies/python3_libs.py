533641649281ca3dc2d16d52f1998714b3e58c3b
hydra3333 <hydra3333@gmail.com>
2020-04-04 14:01:18 +1030

    update vapoursynth, deadsix27
    deadsix27 has updated his/her scripts

--- packages/dependencies/python3_libs.py
+++ packages/dependencies/python3_libs.py
@@ -14,13 +14,11 @@
 	'needs_make_install' : False,
 	# python 3.8.2
 	'run_post_patch' : [
-		'cp -fv Makefile Makefile.orig',
-		'sed -i.bak "s;3.7.5;3.8.2;g" "Makefile"',
-		'diff -U 5 Makefile.orig Makefile && echo "NO difference" || echo "YES differences!"',
+		#'cp -fv Makefile Makefile.orig',
+		#'sed -i.bak "s;;;g" "Makefile"',
+		#'diff -U 5 Makefile.orig Makefile && echo "NO difference" || echo "YES differences!"',
 		'cp -fv install_python_libs.py install_python_libs.py.orig',
 		'sed -i.bak "s;_DEBUG = False;_DEBUG = True;g" "install_python_libs.py"',
-		'sed -i.bak "s;\'3.7.5\';\'3.7.5\',\'3.8.2\';g" "install_python_libs.py"',
-		'sed -i.bak "s; 3.7.5 ; 3.8.2 ;g" "install_python_libs.py"',
 		'diff -U 5 install_python_libs.py.orig install_python_libs.py && echo "NO difference" || echo "YES differences!"',
 	],
 	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool PYTHON_VERSION=3.8.2',
