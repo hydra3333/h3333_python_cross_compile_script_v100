ee08ce4a11c9b430219862a6b52307a38e8ee3f8
hydra3333 <hydra3333@gmail.com>
2020-04-06 19:23:40 +0930

    Add files via upload

--- packages/dependencies/avisynthplus_headers.py
+++ packages/dependencies/avisynthplus_headers.py
@@ -4,7 +4,8 @@
 	#'branch' : '',											
 	'url' : 'https://github.com/AviSynth/AviSynthPlus',
 	'recursive_git' : True,
-	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DHEADERS_ONLY:bool=on ',
+	'source_subfolder' : 'avisynth-build',
 	'conf_system' : 'cmake',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DHEADERS_ONLY:bool=on ',
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'AviSynthPlus headers' },
 }
