dc3914b512c5e0b5a6e61bf0a2680dac3e3565e9
hydra3333 <hydra3333@gmail.com>
2020-04-06 18:22:36 +0930

    Add files via upload

--- packages/dependencies/libffmpeg.py
+++ packages/dependencies/libffmpeg.py
@@ -2,7 +2,7 @@
 	'repo_type' : 'git',
 	'url' : 'git://git.ffmpeg.org/ffmpeg.git',
 	'depth_git': 0,
-	'branch' : '7b0f7a7f3b6bf09c076c23d0701bf42a2c997ca2',
+	'branch' : '4d9b9c5e4637ac15205467f16fcac92a28e18f18',
 	'rename_folder' : 'libffmpeg_git',
 	'configure_options' : '--sysroot={target_sub_prefix} !VAR(ffmpeg_config)VAR! !VAR(ffmpeg_nonfree)VAR! --prefix={target_prefix} --disable-sdl2 --disable-shared --enable-static --disable-doc --disable-programs',
 	'depends_on' : [ 'ffmpeg_depends', 'ffmpeg_depends_nonfree', ],
