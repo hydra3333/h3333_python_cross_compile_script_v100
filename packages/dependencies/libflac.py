c8a23e1a8cd8703d375ce10e04f63f850cab530b
hydra3333 <hydra3333@gmail.com>
2020-04-06 15:41:22 +0930

    libflac not found by sndfile when building vamp
    try patching

--- packages/dependencies/libflac.py
+++ packages/dependencies/libflac.py
@@ -3,9 +3,12 @@
 	'url' : 'https://github.com/xiph/flac.git',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_64_BIT_WORDS=ON -DBUILD_PROGRAMS=OFF -DINSTALL_MANPAGES=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DVERSION=1.3.3 -DCMAKE_BUILD_TYPE=Release',
+	#'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_64_BIT_WORDS=ON -DBUILD_PROGRAMS=OFF -DINSTALL_MANPAGES=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DVERSION=1.3.3 -DCMAKE_BUILD_TYPE=Release',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_64_BIT_WORDS=ON -DBUILD_PROGRAMS=OFF -DINSTALL_PKGCONFIG_MODULES=ON -DINSTALL_MANPAGES=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DVERSION=1.3.3 -DCMAKE_BUILD_TYPE=Release', # 2020.04.06 try to fix vamp error. this -> sndfile -> vamp
 	'patches': [
 		('flac/0001-mingw-fix-2.patch', '-p1', '..'),
+		('flac/libFLAC_CMakeLists.txt.patch', '-p1', '..'),
+		('flac/libFLAC++_CMakeLists.txt.patch', '-p1', '..'),
 	],
 	'regex_replace': {
 		'post_patch': [
@@ -32,3 +35,6 @@
 	],
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'flac (library)' },
 }
+#
+# src/libFLAC/CMakeLists.txt   DESTINATION "${CMAKE_INSTALL_DATADIR}/pkgconfig"     DESTINATION "${CMAKE_INSTALL_LIBDIR}/lib/pkgconfig")
+# src/libFLAC++/CMakeLists.txt DESTINATION "${CMAKE_INSTALL_DATAROOTDIR}/pkgconfig" DESTINATION "${CMAKE_INSTALL_LIBDIR}/lib/pkgconfig")
