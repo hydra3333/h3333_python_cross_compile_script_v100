d5ba06a3bd74f790fbac8141ff2baa0b20b6928d
hydra3333 <hydra3333@gmail.com>
2020-04-07 16:15:15 +0930

    fix libass dependencies
    'freetype' depends on depends on 'freetype_lib', 'harfbuzz_lib-with-freetype' so it gets the order right !
    
    This stops other things not re-building later !

--- packages/dependencies/libass.py
+++ packages/dependencies/libass.py
@@ -5,7 +5,8 @@
     'run_post_install': [
 		'sed -i.bak \'s/-lass -lm/-lass -lfribidi -lfreetype -lexpat -lm/\' "{pkg_config_path}/libass.pc"', #-lfontconfig # 2018.12.13
 	],
-	'depends_on' : [ 'expat', 'fontconfig',  'harfbuzz', 'freetype', 'iconv', 'libfribidi'],
+	#'depends_on' : [ 'expat', 'fontconfig',  'harfbuzz', 'freetype', 'iconv', 'libfribidi'],
+	'depends_on' : [ 'expat', 'fontconfig',  'freetype', 'iconv', 'libfribidi'], # 'freetype' depends on depends on 'freetype_lib', 'harfbuzz_lib-with-freetype' so it gets the order right !
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libass' },
 }
 # 2019.12.13 old:
