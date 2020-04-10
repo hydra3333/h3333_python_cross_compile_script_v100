d9ded708aef292023ada0013ea8885fbf00f4906
hydra3333 <hydra3333@gmail.com>
2020-04-10 20:44:38 +0930

    more freetype

--- packages/dependencies/harfbuzz_lib.py
+++ packages/dependencies/harfbuzz_lib.py
@@ -1,4 +1,4 @@
-{
+{ # 2020.04.10 NOT USED
 	'repo_type' : 'archive',
 	'download_locations' : [
 		{ 'url' : 'https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.6.4.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '9413b8d96132d699687ef914ebb8c50440efc87b3f775d25856d7ec347c03c12' }, ], },
