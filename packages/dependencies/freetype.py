993b065454286907550f0be8e87c6122cc4b4d80
hydra3333 <hydra3333@gmail.com>
2020-04-10 20:32:21 +0930

    update freetype

--- packages/dependencies/freetype.py
+++ packages/dependencies/freetype.py
@@ -1,5 +1,13 @@
 {
 	'is_dep_inheriter' : True,
+	'run_pre_depends_on' : [
+		'rm -vf  {pkg_config_path}/freetype2.pc',
+		'rm -vf  {target_prefix}/lib/libfreetype.a',
+		'rm -vfR {target_prefix}/include/freetype2',
+		'rm -vf  {pkg_config_path}/harfbuzz.pc',
+		'rm -vf  {target_prefix}/lib/libharfbuzz.a',
+		'rm -vfR {target_prefix}/include/harfbuzz',
+	],
 	'depends_on' : [ 'zlib', 'bzip2', 'libpng', 'freetype_lib', 'harfbuzz_lib-with-freetype', ], # 'freetype_lib-with-harfbuzz' ],
 }
 # 2019.12.13 old:
