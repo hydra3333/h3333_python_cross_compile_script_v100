6d1f11e5ed15d05da8b144da6c9a16252a81a07b
hydra3333 <hydra3333@gmail.com>
2020-04-11 12:18:46 +0930

    more freetype

--- packages/dependencies/freetype.py
+++ packages/dependencies/freetype.py
@@ -13,10 +13,10 @@
 		'echo "rm -vf  {target_prefix}/lib/libfreetype.a"',
 		'echo "rm -vfR {target_prefix}/include/freetype2"',
 		'echo "rm -vf  {pkg_config_path}/harfbuzz.pc"',
-		#'echo "rm -vf  {target_prefix}/lib/libharfbuzz.a"',
-		#'echo "rm -vfR {target_prefix}/include/harfbuzz"',
+		'echo "rm -vf  {target_prefix}/lib/libharfbuzz.a"',
+		'echo "rm -vfR {target_prefix}/include/harfbuzz"',
 		'echo "rm -vfR x86_64/freetype2_git"',
-		#'echo "rm -vfR x86_64/harfbuzz-with-freetype"',
+		'echo "rm -vfR x86_64/harfbuzz-with-freetype"',
 		'echo "WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING "',
 		'echo "WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING "',
 		'echo "WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING WARNING "',
@@ -25,8 +25,11 @@
 	],
 	'depends_on' : [ 'zlib', 'bzip2', 'libpng', 'freetype_lib', 'harfbuzz_lib-with-freetype', ], # 'freetype_lib-with-harfbuzz' ],
 }
-# 2019.12.13 old:
-#	'freetype' : {
-#		'is_dep_inheriter' : True,
-#		'depends_on' : [ 'bzip2', 'freetype_lib', 'harfbuzz_lib-with-freetype', ], # 'freetype_lib-with-harfbuzz' ],
-#	},
+#rm -vf  {pkg_config_path}/freetype2.pc
+#rm -vf  {target_prefix}/lib/libfreetype.a
+#rm -vfR {target_prefix}/include/freetype2
+#rm -vf  {pkg_config_path}/harfbuzz.pc
+#rm -vf  {target_prefix}/lib/libharfbuzz.a
+#rm -vfR {target_prefix}/include/harfbuzz
+#rm -vfR x86_64/freetype2_git
+#rm -vfR x86_64/harfbuzz-with-freetype
