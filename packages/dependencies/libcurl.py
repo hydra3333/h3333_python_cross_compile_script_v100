4d19b50f3828e66048f667fa5e760d2dfc2d0e31
hydra3333 <hydra3333@gmail.com>
2020-04-11 15:51:44 +0930

    Add files via upload

--- packages/dependencies/libcurl.py
+++ packages/dependencies/libcurl.py
@@ -2,7 +2,7 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/curl/curl',
 	'depth_git' : 0,
-	'branch' : '2cfac302fbeec68f1727cba3d1705e16f02220ad', # fails: 'd4afa0fc2076ee0184cf8120d83ac997e8c57692',
+	'branch' : 'b8d1366852fd0034374c5de1e4968c7a224f77cc', # works: '2cfac302fbeec68f1727cba3d1705e16f02220ad', # fails: 'b8d1366852fd0034374c5de1e4968c7a224f77cc',
 	'rename_folder' : 'curl_git',
 	'configure_options': '--enable-static --disable-shared --target={bit_name2}-{bit_name_win}-gcc --host={target_host} --build=x86_64-linux-gnu --with-libssh2 --with-gnutls --with-ca-fallback --without-winssl --prefix={target_prefix} --exec-prefix={target_prefix}', # 2019.12.13
 	'depends_on': (
