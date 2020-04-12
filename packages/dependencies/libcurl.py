917282cd27da26dede7793734a30443baa92b069
hydra3333 <hydra3333@gmail.com>
2020-04-13 00:29:53 +0930

    pthreads in libcurl

--- packages/dependencies/libcurl.py
+++ packages/dependencies/libcurl.py
@@ -4,7 +4,8 @@
 	'depth_git' : 0,
 	'branch' : '3735107d62ad28d6e25bd94109ab9f2454c22116', # works: '3735107d62ad28d6e25bd94109ab9f2454c22116', # fails: '1fc0617dccc3fa138235f219e2eaa7b405d1162e' under gcc10,
 	'rename_folder' : 'curl_git',
-	'configure_options': '--enable-static --disable-shared --target={bit_name2}-{bit_name_win}-gcc --host={target_host} --build=x86_64-linux-gnu --with-libssh2 --with-gnutls --with-ca-fallback --without-winssl --prefix={target_prefix} --exec-prefix={target_prefix}', # 2019.12.13
+	'configure_options': '--prefix={target_prefix} --exec-prefix={target_prefix} --enable-static --disable-shared --target={bit_name2}-{bit_name_win}-gcc --host={target_host} --build=x86_64-linux-gnu '
+		'--with-libssh2 --with-gnutls --with-ca-fallback --without-winssl --enable-pthreads --enable-symbol-hiding ',
 	'depends_on': (
 		'zlib', 'gnutls', 'libssh2',
 	),
