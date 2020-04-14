2a9e8295c94ef3343c400bc1598168e8e75c3d7d
hydra3333 <hydra3333@gmail.com>
2020-04-14 11:40:00 +0930

    Add files via upload

--- mingw_toolchain_script/mingw_toolchain_script_v100_001.py
+++ mingw_toolchain_script/mingw_toolchain_script_v100_001.py
@@ -100,7 +100,7 @@ SOURCES['gcc'] = { # https://www.gnu.org/software/gcc/ # https://ftp.gnu.org/gnu
 	'type': 'archive',
 	#'version'   : '9.3.0', # stable versions
 	#'url' : 'https://gcc.gnu.org/pub/gcc/releases/gcc-{version}/gcc-{version}.tar.xz', # stable versions
-	'version'   : '10-20200405', # TEST version, who not :)
+	'version'   : '10-20200412', #'10-20200405', # TEST version, why not :)
 	'url' : 'ftp://ftp.fu-berlin.de/unix/languages/gcc/snapshots/{version}/gcc-{version}.tar.xz', # TEST builds server
 	#'patches': [
 		#( 'https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/mingw_toolchain_script/patches/0001-gcc_7_1_0_weak_refs_x86_64.patch', 'p1' ),
