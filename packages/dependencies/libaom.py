c8498ed3fa362774bcff33116e936c0bbb830d80
hydra3333 <hydra3333@gmail.com>
2020-05-10 16:10:07 +0930

    updated libaom cmake options

--- packages/dependencies/libaom.py
+++ packages/dependencies/libaom.py
@@ -2,71 +2,54 @@
 	'repo_type' : 'git',
 	'url' : 'https://aomedia.googlesource.com/aom',
 	'depth_git': 0,
-	'branch': '3f4113d35c20c958b256427d41d3f839601d1f90', # 2020.03.19 try git master # acc2adf9195ff4c5d061132d860d2fe38b28aa55 works
+	#'branch': '3f4113d35c20c958b256427d41d3f839601d1f90', # 2020.03.19 try git master # acc2adf9195ff4c5d061132d860d2fe38b28aa55 works
 	'rename_folder' : 'libaom_git',
 	'conf_system' : 'cmake',
 	'source_subfolder' : 'build',
+	#
+	# BUILD system configuration options can be found at the top of the CMakeLists.txt file found in the root of the AV1 repository, 
+	# AV1 CODEC CONFIGURATION options can currently be found in the file build/cmake/aom_config_defaults.cmake.
+	#
+    # 2019.12.13 note: we make this a 64 bit build only with -DAOM_TARGET_CPU=x86_64 ... some may not prefer this.
 	'configure_options' : 
 		'.. {cmake_prefix_options} '
 		'-DCMAKE_INSTALL_PREFIX={target_prefix} '
 		'-DAOM_TARGET_CPU={bit_name2} ' # '-DAOM_TARGET_CPU=x86_64 ' 
+		'-DARCH_X86_64=1 '
+		'-DCONFIG_GCC=1 '
+		'-DCONFIG_RUNTIME_CPU_DETECT=1 '
 		'-DBUILD_SHARED_LIBS=0 '
 		'-DCONFIG_STATIC=1 '
 		'-DCONFIG_SHARED=0 '
-        '-DCONFIG_LOWBITDEPTH=0 '
 		'-DFORCE_HIGHBITDEPTH_DECODING=1 '
-		'-DCONFIG_HIGHBITDEPTH=1 '
-		'-DHAVE_PTHREAD=1 '
-		'-DENABLE_CCACHE=1 '
-		#'-DCONFIG_LPF_MASK=1 ' # 2020.05.10 per https://bugs.chromium.org/p/aomedia/issues/detail?id=2684#c6
+		'-DCONFIG_AV1_HIGHBITDEPTH=1 ' #?
+		'-DHAVE_PTHREAD_H=1 '
+		#'-DENABLE_CCACHE=1 '
+		#'-DCONFIG_LPF_MASK=1 ' # 2020.05.10 removed per https://bugs.chromium.org/p/aomedia/issues/detail?id=2684#c6
+		'-DCONFIG_LIBYUV=1 ' #?
 		'-DCONFIG_MULTITHREAD=1 '
 		'-DCONFIG_PIC=1 '
-		'-DCONFIG_COEFFICIENT_RANGE_CHECKING=0 ' # 2019.12.13
-        '-DCONFIG_RUNTIME_CPU_DETECT=1 '
+		'-DCONFIG_COEFFICIENT_RANGE_CHECKING=1 ' #?
+		'-DCONFIG_DENOISE=1 ' #?
 		'-DCONFIG_WEBM_IO=1 '
 		'-DCONFIG_SPATIAL_RESAMPLING=1 '
-		'-DENABLE_NASM=on ' # YASM is preferred (default) # 2019.12.13 no, NASM is. change -DENABLE_NASM=off 
-		'-DCONFIG_AV1=1 '
+		'-DENABLE_NASM=ON ' # YASM is preferred (default) # 2019.12.13 no, NASM is.
 		'-DCONFIG_AV1_DECODER=1 '
 		'-DCONFIG_AV1_ENCODER=1 '
+		#'-DCONFIG_ANALYZER=1 ' #?
+		#'-DCONFIG_ACCOUNTING=1 ' #?
+		#'-DCONFIG_INSPECTION=1 ' #?
 		'-DENABLE_TOOLS=0 '
 		'-DENABLE_EXAMPLES=0 '
 		'-DENABLE_DOCS=0 '
-        '-DCONFIG_INSTALL_DOCS=1 '
-		'-DCONFIG_INSTALL_BINS=1 '
-        '-DCONFIG_INSTALL_LIBS=1 '
-		'-DCONFIG_INSTALL_SRCS=0 '
-		'-DCONFIG_UNIT_TESTS=0 '
 		'-DENABLE_TESTS=0 '
 		'-DENABLE_TESTDATA=0 '
 		'-DAOM_EXTRA_C_FLAGS="{original_cflags}" '
-        '-DAOM_EXTRA_CXX_FLAGS="{original_cflags}" '
+		'-DAOM_EXTRA_CXX_FLAGS="{original_cflags}" '
 		'-DLIBXML_STATIC=1 '
 		'-DGLIB_STATIC_COMPILATION=1 '
 	,
-	'depends_on' : [ 'glib2', 'libxml2' ],
+	'depends_on' : [ 'glib2', 'libxml2' ], # 2019.12.13
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libaom-av1' },
 }
-# 2019.12.13 old:
-#	'libaom' : {
-#		'repo_type' : 'git',
-#		'url' : 'https://aomedia.googlesource.com/aom', # https://aomedia-review.googlesource.com/q/status:merged
-#		'conf_system' : 'cmake',
-#		'source_subfolder' : 'build',
-#		'configure_options': '.. {cmake_prefix_options} '
-#			'-DAOM_TARGET_CPU=x86_64 -DAOM_EXTRA_C_FLAGS="{original_cflags}" -DAOM_EXTRA_CXX_FLAGS="{original_cflags}" ' # 2019.11.10 add -fstack-protector-all -D_FORTIFY_SOURCE=2
-#			'-DCMAKE_INSTALL_PREFIX={target_prefix} '
-#			'-DCONFIG_LOWBITDEPTH=0 -DFORCE_HIGHBITDEPTH_DECODING=1 -DCONFIG_HIGHBITDEPTH=1 ' # 2019.10.22 per https://aomedia.googlesource.com/aom/+/refs/heads/master/build/cmake/aom_configure.cmake#28
-#			'-DCONFIG_AV1=1 -DHAVE_PTHREAD=1 -DBUILD_SHARED_LIBS=0 -DENABLE_DOCS=1 -DCONFIG_INSTALL_DOCS=1 '
-#			'-DCONFIG_INSTALL_BINS=0 -DCONFIG_INSTALL_LIBS=1 '
-#			'-DCONFIG_INSTALL_SRCS=1 -DCONFIG_UNIT_TESTS=0 -DENABLE_TESTS=0 -DENABLE_TESTDATA=0 -DENABLE_EXAMPLES=0 '
-#			'-DCONFIG_AV1_DECODER=1 -DCONFIG_AV1_ENCODER=1 -DENABLE_CCACHE=1 -DCONFIG_LPF_MASK=1 -DENABLE_TOOLS=0 -DENABLE_EXAMPLES=0 '
-#			'-DCONFIG_MULTITHREAD=1 -DCONFIG_PIC=1 -DCONFIG_COEFFICIENT_RANGE_CHECKING=0 '
-#			'-DCONFIG_RUNTIME_CPU_DETECT=1 -DCONFIG_WEBM_IO=1 '
-#			'-DCONFIG_SPATIAL_RESAMPLING=1 -DENABLE_NASM=on'
-#			'-DLIBXML_STATIC=1 -DGLIB_STATIC_COMPILATION=1 ' # 2018.11.23 add nasm, and change some settings
-#		,
-#		'depends_on' : [ 'libxml2' ],
-#		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libaom' },
-#	},
