cd173139c4b5e42a2b4bc13281abd00d1bf54985
hydra3333 <hydra3333@gmail.com>
2020-05-11 17:22:11 +0930

    Add files via upload

--- packages/dependencies/libarchive.py
+++ packages/dependencies/libarchive.py
@@ -32,7 +32,7 @@
 		'-DENABLE_TEST=OFF '
 		'-DENABLE_COVERAGE=OFF '
 		'-DENABLE_INSTALL=ON '
-		'-DLIBXML2_LIBRARIES="-lxml2 -lz -llzma -liconv -lws2_32"'
+		'-DLIBXML2_LIBRARIES="-lnettle -lxml2 -llzma -lbcrypt -lbz2 -lz -liconv -lcharset -llzo2 -lws2_32" ' # -lxml2 -lbcrypt -lz -llzma -liconv -lws2_32"'
 	,
 	'depends_on' : [
 		'iconv', 'bzip2', 'expat', 'zlib', 'xz', 'lzo', 'bzip2', 'libnettle', 'libxml2', 'expat', 'pcre', 'pcre2', 'libgcrypt',
