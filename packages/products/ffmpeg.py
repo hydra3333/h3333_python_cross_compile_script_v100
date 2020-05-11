3c34a579f6f459b40923a7c91235c044c1f18e45
hydra3333 <hydra3333@gmail.com>
2020-05-11 22:45:24 +0930

    Add files via upload

--- packages/products/ffmpeg.py
+++ packages/products/ffmpeg.py
@@ -1,8 +1,8 @@
 {
 	'repo_type' : 'git',
 	'url' : 'git://git.ffmpeg.org/ffmpeg.git',
-	#'depth_git': 0,
-	#'branch' : '4d9b9c5e4637ac15205467f16fcac92a28e18f18',
+	'depth_git': 0,
+	'branch' : '5727b1f13f36c4db30d5d0de51640f740edf01e8',
 	'rename_folder' : 'ffmpeg',
 	'configure_options' : '!VAR(ffmpeg_config)VAR! !VAR(ffmpeg_extra_config)VAR! !VAR(ffmpeg_nonfree)VAR! --prefix={output_prefix}/ffmpeg_git.installed --enable-sdl --disable-shared --enable-static',
 	'depends_on' : [ 'ffmpeg_depends', 'ffmpeg_depends_extra', 'ffmpeg_depends_nonfree', 'sdl2'],
