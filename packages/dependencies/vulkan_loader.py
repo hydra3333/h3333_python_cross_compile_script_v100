39e7754904081105e1fad7c022714d5a16f58a57
hydra3333 <hydra3333@gmail.com>
2020-05-11 15:57:05 +0930

    add sed x2 to align more with deadsix27

--- packages/dependencies/vulkan_loader.py
+++ packages/dependencies/vulkan_loader.py
@@ -17,64 +17,10 @@
 	'patches' : [
 		('vulkan/vulkan-0001-cross-compile-static-linking-hacks-MABS-shinchiro-2020.05.11.patch','-p1','..'), # 2020.05.11 per MABS # https://raw.githubusercontent.com/shinchiro/mpv-winbuild-cmake/master/packages/vulkan-0001-cross-compile-static-linking-hacks.patch
 	],
+	'run_post_patch' : [ 
+		'sed -i.bak \'s/ pthread m)/ pthread m cfgmgr32)/g\' ./loader/CMakeLists.txt', # 2020.05.11 to align more with deadsix27
+		'sed -i.bak \'s/ -lshlwapi -lcfgmgr32"/ -lcfgmgr32 -lpthread -lm -lshlwapi"/g\' ./loader/CMakeLists.txt', # 2020.05.11 to align more with deadsix27
+	],
 	'depends_on' : [ 'vulkan_headers', 'vulkan-d3dheaders', ], # 2020.05.11 hmm, deadsiz27 had commented out 'vulkan-d3dheaders', but lets leave it in and see what happens
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'Vulkan Loader per MABS' },
 }
-#
-#{
-#	'repo_type' : 'git',
-#	'url' : 'https://github.com/KhronosGroup/Vulkan-Loader.git',
-#	#'depth_git' : 0,
-#	#'branch' : 'tags/v1.2.135',
-#	'recursive_git' : True, 
-#	'configure_options' : 
-#		'.. {cmake_prefix_options} -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX={target_prefix} '
-#		'-DVULKAN_HEADERS_INSTALL_DIR={target_prefix} '
-#		'-DBUILD_TESTS=OFF ' 
-#		'-DENABLE_STATIC_LOADER=ON ' # 2020.04.07 By default, the loader is built as a dynamic library. This allows it to be built as a static library, instead.
-#		#'-DBUILD_STATIC_LOADER=OFF ' # 2020.04.07 comment out since "Note that this will only work on MacOS and is not supported"
-#	,
-#	#'env_exports' : { # 2019.12.13 add -D_POSIX_C_SOURCE # 2020.05.11 comment out per deadsix27
-#	#	'CFLAGS'   : ' -O3 -D_POSIX_C_SOURCE ', # 2020.04.07 attempted to add -D_POSIX_C_SOURCE
-#	#	'CXXFLAGS' : ' -O3 -D_POSIX_C_SOURCE ', # 2020.04.07 attempted to add -D_POSIX_C_SOURCE
-#	#	'CPPFLAGS' : ' -O3 -D_POSIX_C_SOURCE ', # 2020.04.07 attempted to add -D_POSIX_C_SOURCE
-#	#	'LDFLAGS'  : ' -O3 -D_POSIX_C_SOURCE ', # 2020.04.07 attempted to add -D_POSIX_C_SOURCE
-#	#},
-#	'conf_system' : 'cmake',
-#	'source_subfolder' : '_build',
-#	'patches' : [
-#		('vulkan/0001-mingw-workarounds-2020.04.08.patch','-p1','..'),
-#	],
-#	'regex_replace': {
-#		'post_install': [
-#			{
-#				0: r'(?:[^\r\n]+)?libdir=(?:[^\r\n]+)?',
-#				'in_file': '{pkg_config_path}/vulkan.pc',
-#				'out_file': '{pkg_config_path}/vulkan.pc'
-#			},
-#			{
-#				0: r'exec_prefix=([^\r\n]+)',
-#				1: r'prefix={{target_prefix}}\nexec_prefix=\1\nlibdir=${{exec_prefix}}/lib\n',
-#				'in_file': '{pkg_config_path}/vulkan.pc',
-#				'out_file': '{pkg_config_path}/vulkan.pc'
-#			},
-#			{
-#				0: r'-lvulkan$',
-#				1: r'-lvulkan-1.dll',
-#				'in_file': '{pkg_config_path}/vulkan.pc',
-#				'out_file': '{pkg_config_path}/vulkan.pc'
-#			},
-#		]
-#	},
-#	'run_post_install' : [
-#	#	##'cp -fv "{target_prefix}/lib/libvulkan-1.dll.a" "{target_prefix}/lib/libvulkan-1.a"',
-#		#'cp -fv "{target_prefix}/lib/pkgconfig/vulkan.pc" "{target_prefix}/lib/pkgconfig/vulkan.pc.orig"',
-#		#'sed -i.bak \'s/-lvulkan/-lvulkan-1.dll/g\' "{target_prefix}/lib/pkgconfig/vulkan.pc"',
-#		#'sed -i.bak \'s/-lvulkan-1.dll-1.dll/-lvulkan-1.dll/g\' "{target_prefix}/lib/pkgconfig/vulkan.pc"',
-#		#'diff -U 5 "{target_prefix}/lib/pkgconfig/vulkan.pc.orig" "{target_prefix}/lib/pkgconfig/vulkan.pc" && echo "NO difference" || echo "YES differences!"',
-#		#'cat "{target_prefix}/lib/pkgconfig/vulkan.pc"',
-#	],
-#	'depends_on' : [ 'vulkan_headers', 'vulkan-d3dheaders', ], # 2020.05.11 hmm, deadsiz27 had commented out 'vulkan-d3dheaders', but lets leave it in and see what happens
-#	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'Vulkan Loader' },
-#}
-#
