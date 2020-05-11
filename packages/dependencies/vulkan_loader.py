6cb8abb8abff374d17c4082624adcc704cda5a1c
hydra3333 <hydra3333@gmail.com>
2020-05-11 16:12:48 +0930

    a comment

--- packages/dependencies/vulkan_loader.py
+++ packages/dependencies/vulkan_loader.py
@@ -1,5 +1,5 @@
 { # 2020.05.11 let's see if the MABS methid/patch works
-	# see mpv libmpv libplacebo ffmpeg_depends
+	# see ffmpeg_depends, variables.py, liblacebo, libmpv, mpv
 	'repo_type' : 'git',
 	'url' : 'https://github.com/KhronosGroup/Vulkan-Loader.git',
 	'recursive_git' : True, 
