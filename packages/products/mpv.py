820064a6078490e3ab64355545b9e5d9b803c836
hydra3333 <hydra3333@gmail.com>
2020-05-11 17:47:44 +0930

    Add files via upload

--- packages/products/mpv.py
+++ packages/products/mpv.py
@@ -14,11 +14,11 @@
 		'DEST_OS' : '{bit_name_win}', #'DEST_OS' : 'win32',
 		'TARGET'  : '{target_host}',
 		'PKG_CONFIG' : 'pkg-config',
-		#'LDFLAGS' : '-Wl,-Bdynamic -lvulkan -fstack-protector-strong ' # see my 'custom_ldflag' instead
+		#'LDFLAGS': '-Wl,-Bdynamic -lvulkan -fstack-protector-strong' # see my 'custom_ldflag' instead
 	},
 	'custom_cflag'  : ' -O3 ',
 	#'custom_ldflag' : ' -Wl,-Bdynamic -lvulkan -fstack-protector-strong -lz -ld3d11 -lintl -liconv ', # 2020.03.19 added -ld3d11 per from libmpv.py also added -lintl -liconv # including -lzimg always throws an error
-	'custom_ldflag' : ' -Wl,-Bdynamic -fstack-protector-strong -lz -ld3d11 -lintl -liconv ', # 2020.04.09
+	'custom_ldflag' : ' -Wl,-Bdynamic -fstack-protector-strong -lz -lvulkan -ld3d11 -lintl -liconv ', # 2020.04.09
 	'configure_options' :
 		'--force '
 		'--enable-libmpv-shared '
