fb7d8c317e3477308c699059ca2a38ce83352352
hydra3333 <hydra3333@gmail.com>
2020-05-12 18:48:47 +0930

    Add files via upload

--- packages/dependencies/json-c.py
+++ packages/dependencies/json-c.py
@@ -2,8 +2,9 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/json-c/json-c.git',
 	'depth_git': 0,
-	'branch': 'tags/json-c-0.14-20200419', # 2020.05.11 per deadsix27 https://github.com/DeadSix27/python_cross_compile_script/commit/133965456b7fdbc82cbfe97e4612f5d4a319a34f
+	'branch': 'bae2f10c436eaf0d95746cbc5f1c1f0ecb866a8e', # 2020.05.12 tags failed 'tags/json-c-0.14-20200419', # 2020.05.11 per deadsix27 https://github.com/DeadSix27/python_cross_compile_script/commit/133965456b7fdbc82cbfe97e4612f5d4a319a34f
 	'conf_system' : 'cmake',
 	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release -DENABLE_RDRAND=ON -DENABLE_THREADING=ON -DDISABLE_WERROR=ON',
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'json-c' },
 }
+
