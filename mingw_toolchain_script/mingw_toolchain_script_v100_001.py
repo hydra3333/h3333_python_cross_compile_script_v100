e54043ae740ae0569491779f8e1133734571aff4
hydra3333 <hydra3333@gmail.com>
2020-05-13 01:00:53 +0930

    use latest mingw

--- mingw_toolchain_script/mingw_toolchain_script_v100_001.py
+++ mingw_toolchain_script/mingw_toolchain_script_v100_001.py
@@ -46,7 +46,7 @@ SOURCES['mingw-w64'] = { # https://sourceforge.net/p/mingw-w64/mingw-w64/ci/mast
 	'git_shallow': False,
 	'url': 'https://git.code.sf.net/p/mingw-w64/mingw-w64',  # mirror: https://github.com/mirror/mingw-w64.git but that seems suprisingly out of date sometimes.
  	#'checkout' : 'eb46f443034dad175c1e61db9062ff4c15eac0b5', # see calling  .py  -'mingw_commit': 'tags/v7.0.0', 
- 	'checkout' : 'tags/v7.0.0', # see calling  .py  -'mingw_commit': 'tags/v7.0.0', 
+ 	#'checkout' : 'tags/v7.0.0', # see calling  .py  -'mingw_commit': 'tags/v7.0.0', 
 	'run_after_patches': [
 		('autoreconf -fiv', ),
 		('mingw-w64-crt', 'autoreconf -fiv'),
