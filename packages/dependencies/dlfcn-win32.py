3f9def966ddd54b7292b6ba8b249a7972a783737
hydra3333 <hydra3333@gmail.com>
2020-05-13 02:23:12 +0930

    Add files via upload

--- packages/dependencies/dlfcn-win32.py
+++ packages/dependencies/dlfcn-win32.py
@@ -6,13 +6,3 @@
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DBUILD_TESTS=0',
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'dlfcn win32' },
 }
-# 2019.12.13 old:
-#	},
-#	'dlfcn-win32' : { # 2018.12.05
-#		'repo_type' : 'git',
-#		'url' : 'https://github.com/dlfcn-win32/dlfcn-win32.git',
-#		'conf_system' : 'cmake',
-#		'source_subfolder' : '_build',
-#		'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DBUILD_TESTS=0',
-#		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'dlfcn_win32' },
-#	},
