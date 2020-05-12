a2b7e1332696db87b8ec81a6a0ce48504c2c1357
hydra3333 <hydra3333@gmail.com>
2020-05-12 15:10:38 +0930

    Add files via upload

--- packages/dependencies/_disabled/pcre.py
+++ packages/dependencies/_disabled/pcre.py
@@ -1,9 +1,12 @@
-{
+{ # pcre added 2019.12.13  ... superseded by pcre2 ? Nope, not for glib2
 	'repo_type' : 'archive',
-	'url' : 'https://ftp.pcre.org/pub/pcre/pcre-8.41.tar.gz',
-	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --enable-unicode-properties --enable-jit --enable-pcre16 --enable-pcre32 --enable-pcregrep-libz --enable-pcregrep-libbz2',
-	'depends_on' : [
-		'bzip2',
+	'download_locations' : [
+		{ "url" : "https://fossies.org/linux/misc/pcre-8.44.tar.bz2", "hashes" : [ { "type" : "sha256", "sum" : "19108658b23b3ec5058edc9f66ac545ea19f9537234be1ec62b714c84399366d" }, ], },
+		{ "url" : "https://ftp.pcre.org/pub/pcre/pcre-8.44.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "19108658b23b3ec5058edc9f66ac545ea19f9537234be1ec62b714c84399366d" }, ], },
 	],
-	'_info' : { 'version' : '8.41', 'fancy_name' : 'pcre' },
+	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --enable-unicode-properties --enable-utf --enable-pcre8 --enable-pcre16 --enable-pcre32 --enable-pcregrep-libz --enable-pcregrep-libbz2 --enable-newline-is-anycrlf --disable-pcre2test-libedit --disable-pcretest-libreadline --enable-jit ', 
+	'depends_on' : [
+		'bzip2', 'zlib',
+	],
+	'_info' : { 'version' : '8.43', 'fancy_name' : 'pcre' },
 }
