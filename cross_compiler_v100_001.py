4176031ec21119ca04a3e7ca54c97d85d5a89857
hydra3333 <hydra3333@gmail.com>
2020-05-13 22:42:29 +0930

    attempt fix local vars

--- cross_compiler_v100_001.py
+++ cross_compiler_v100_001.py
@@ -731,16 +731,16 @@ class CrossCompileScript:
 		self.cpuCount = self.config["toolchain"]["cpu_count"]
 
 		self.original_stack_protector = self.config["toolchain"]["original_stack_protector"]  # 2019.12.13
-		self.original_stack_protector_trim = strip(self.config["toolchain"]["original_stack_protector"]) # 2020.05.13
+		self.original_stack_protector_trim = self.config["toolchain"]["original_stack_protector"].strip() # 2020.05.13
 		
 		self.original_fortify_source  = self.config["toolchain"]["original_fortify_source"] # 2019.12.13
-		self.original_fortify_source_trim  = strip(self.config["toolchain"]["original_fortify_source"]) # 2020.05.13
+		self.original_fortify_source_trim  = self.config["toolchain"]["original_fortify_source"].strip() # 2020.05.13
 		
 		self.originalCflag = self.config["toolchain"]["original_cflags"] # 2020.05.13 singular
-		self.originalCflag_trim = strip(self.config["toolchain"]["original_cflags"]) # 2020.05.13 singular
+		self.originalCflag_trim = self.config["toolchain"]["original_cflags"]strip() # 2020.05.13 singular
 		
 		self.originalCflags = "  " + self.config["toolchain"]["original_cflags"] + "  " + self.config["toolchain"]["original_stack_protector"] + "  " + self.config["toolchain"]["original_fortify_source"] + "  " # 2019.12.13 added stack protector and fortify source
-		self.originalCflags_trim = strip(self.config["toolchain"]["original_cflags"] + "  " + self.config["toolchain"]["original_stack_protector"] + "  " + self.config["toolchain"]["original_fortify_source"]) # 2020.05.13
+		self.originalCflags_trim = (self.config["toolchain"]["original_cflags"] + "  " + self.config["toolchain"]["original_stack_protector"] + "  " + self.config["toolchain"]["original_fortify_source"]).strip() # 2020.05.13
 
 		self.originbalLdLibPath = os.environ["LD_LIBRARY_PATH"] if "LD_LIBRARY_PATH" in os.environ else ""
 
