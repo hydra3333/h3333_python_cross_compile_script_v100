9cfd8ef8982abbc0661d99437f2c3dce90b44c0b
hydra3333 <hydra3333@gmail.com>
2020-05-14 20:19:40 +0930

    test if adding vulkan breaks anything

--- packages/variables.py
+++ packages/variables.py
@@ -80,7 +80,8 @@
 		'--enable-amf '
 		'--enable-opencl ' # 2019.12.13, added it, not sure why it wasn't in any of the configs ? depends on opencl_non_icd or opencl_icd
 		'--enable-opengl '
-		#'--enable-vulkan ' #' --enable-filter=scale_vulkan --enable-filter=avgblur_vulkan --enable-filter=chromaber_vulkan --enable-filter=overlay_vulkan ' # 2020.04.09
+		'--enable-vulkan ' # 2020.05.14 added back
+		'--enable-filter=scale_vulkan --enable-filter=avgblur_vulkan --enable-filter=chromaber_vulkan --enable-filter=overlay_vulkan ' # 2020.05.14 added back
 	,
 
 	'ffmpeg_nonfree': '--enable-nonfree --enable-libfdk-aac --enable-decklink', # --enable-cuda-sdk # nonfree stuff
