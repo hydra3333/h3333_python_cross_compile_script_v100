65814b8008649721ecaad7e21160e8ebbadcd293
hydra3333 <hydra3333@gmail.com>
2020-05-15 20:03:07 +0930

    boost

--- packages/dependencies/boost.py
+++ packages/dependencies/boost.py
@@ -1,8 +1,9 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		{ 'url' : 'https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_72_0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : '59c9b274bc451cf91a9ba1dd2c7fdcaf5d60b1b3aa83f2c9fa143417cc660722' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/boost_1_72_0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : '59c9b274bc451cf91a9ba1dd2c7fdcaf5d60b1b3aa83f2c9fa143417cc660722' }, ], },
+		{ 'url' : 'https://dl.bintray.com/boostorg/release/1.72.0/source/boost_1_73_0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4eb3b8d442b426dc35346235c8733b5ae35ba431690e38c6a8263dce9fcbb402' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/boost_1_73_0.tar.bz2', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4eb3b8d442b426dc35346235c8733b5ae35ba431690e38c6a8263dce9fcbb402' }, ], },
+		# in WIn10 use powershell to find the sha256 of a file https://www.youtube.com/watch?v=YM2CE6zKvoo&t=57
 	],
 	'needs_make' :False,
 	'needs_make_install' :False,
@@ -15,6 +16,6 @@
 		'if [ ! -f "already_ran_make_0" ] ; then ./b2 toolset=gcc-mingw link=static threading=multi target-os=windows address-model=64 architecture=x86 --prefix={target_prefix} variant=release --with-system --with-filesystem --with-regex --with-date_time --with-thread --user-config=user-config.jam install ; fi',
 		'if [ ! -f "already_ran_make_0" ] ; then touch already_ran_make_0 ; fi',
 	],
-	'update_check' : { 'url' : 'https://sourceforge.net/projects/boost/files/boost/', 'type' : 'sourceforge', 'regex' : r'(?P<version_num>[\d.]+)\.beta\.(?P<rc_num>[0-9])', },
-	'_info' : { 'version' : '1.72.0', 'fancy_name' : 'Boost' },
+	'update_check' : { 'url' : 'https://sourceforge.net/projects/boost/files/boost/', 'type' : 'sourceforge', 'regex' : r'(?P<version_num>[\d.]+)', },
+	'_info' : { 'version' : '1.73.0', 'fancy_name' : 'Boost' },
 }
