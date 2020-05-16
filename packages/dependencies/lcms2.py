82d27472a812e290dd7ae4e843f6c5182aa5b5ef
hydra3333 <hydra3333@gmail.com>
2020-05-16 11:33:03 +0930

    Add files via upload

--- packages/dependencies/lcms2.py
+++ packages/dependencies/lcms2.py
@@ -2,7 +2,7 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/mm2/Little-CMS.git',
 	'depth_git' : 0,
-	#'branch' : '0030b066b2eec47da5f987319bd4e8d5e92449fe', # 2020.05.15
+	'branch' : '0030b066b2eec47da5f987319bd4e8d5e92449fe', # 2020.05.15
 	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static',
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'Little-CMS2' },
