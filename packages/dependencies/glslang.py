61f27a8b47680685ea35d770537c217fba8a2c86
hydra3333 <hydra3333@gmail.com>
2020-05-31 21:13:36 +0930

    glslang patch when using gcc 10.1

--- packages/dependencies/glslang.py
+++ packages/dependencies/glslang.py
@@ -4,6 +4,9 @@
 	'url' : 'https://github.com/KhronosGroup/glslang.git',
 	'depth_git': 0,
 	#'branch': '3ed344dd784ecbbc5855e613786f3a1238823e56', # 2020.04.20 COMMENTED OUT
+	'patches' : [ 
+		('glslang/glslang-0001-fix-gcc-10.1-error-from-shinchiro.patch', '-p1'), # 2020.05.31 only when using gcc 10.1
+	],
 	'needs_make' : False,
 	'needs_make_install' : False,
 	'needs_configure' : False,
