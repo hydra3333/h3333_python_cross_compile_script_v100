21a5856286f3d6eb414851023912ccaef00a69be
hydra3333 <hydra3333@gmail.com>
2020-06-02 16:11:08 +0930

    libfile broke upstream, affix to a working commit

--- packages/dependencies/libfile.py
+++ packages/dependencies/libfile.py
@@ -2,13 +2,12 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/file/file.git',
 	'depth_git' : 0,
-	#'branch' : '24c9c086cd7c55b7b0a003a145b32466468e2608', # 2020.03.19 try latest git. 24c9c086cd7c55b7b0a003a145b32466468e2608 works
+	'branch' : 'd33b9a8d633c76878168bb85f5c993af36e52e58', # 2020.06.02
 	'rename_folder' : 'libfile.git',
 	'patches' : [
 		( 'libfile/file-win32.patch', '-p1' ),
 	],
 	'configure_options' : '{autoconf_prefix_options} -disable-shared --enable-static --enable-fsect-man5 --disable-silent-rules',
-	'depends_on' : [ 'mingw-libgnurx', 'libfile_local' ], # 2020.03.19 added 'libfile_local'
 	'env_exports' : { 'TARGET_CFLAGS' : '{original_cflags}' },
 	'run_post_regexreplace' : [
 		'sed -i.bak "s/#ifdef FIONREAD/#ifdef __linux__ /" src/seccomp.c',
@@ -16,6 +15,7 @@
 		'autoreconf -fiv' 
 	],
 	'flipped_path' : True,
+	'depends_on' : [ 'mingw-libgnurx', 'libfile_local' ], # 2020.03.19 added 'libfile_local'
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git', 'fancy_name' : 'file' },
 }
