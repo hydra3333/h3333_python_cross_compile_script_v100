f7e6a8fc3e687a919508869b8e8c4cda1facfbac
hydra3333 <hydra3333@gmail.com>
2020-06-06 00:35:14 +0930

    Add files via upload

---
+++ packages/dependencies/ffmpeg_depends_tiny.py
@@ -0,0 +1,26 @@
+{
+	'is_dep_inheriter' : True,
+	'depends_on' : [
+		'libxml2',
+		'libx264',
+		'libx265_multibit',
+		'libvpx',
+		'libaom',
+		'xvidcore',
+		'libopus',
+		'liblame'
+		'libogg',
+		'libvorbis',
+		'libtheora',
+		'libsoxr',
+		'rubberband',
+		'libwebp',
+		'nv-codec-headers',
+		'opencl_icd',
+		'vulkan_loader'
+		'fdk_aac'
+		'twolame',
+		'wavpack',
+		'fftw3',
+	],
+}
