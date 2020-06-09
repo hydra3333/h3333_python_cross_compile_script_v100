3de6ba5f8ca238697c5066299b9852d93d7543cb
hydra3333 <hydra3333@gmail.com>
2020-06-09 18:43:44 +0930

    try an updated libx264 dependency

--- packages/dependencies/libx264.py
+++ packages/dependencies/libx264.py
@@ -4,22 +4,31 @@
 	'rename_folder' : 'libx264_git',
 	'configure_options' : '--host={target_host} --enable-static --cross-prefix={cross_prefix_bare} '
 							'--prefix={target_prefix} '
-							'--enable-strip --disable-cli --bit-depth=all --disable-lavf ' # 2020.06.09 re-added --disable-lavf to libx264 only
-							'--extra-cflags="-DLIBXML_STATIC" --extra-cflags="-DGLIB_STATIC_COMPILATION" ',
-	#'depends_on' : [
-	#	'libffmpeg_extra', 'liblsw',  # 2019.12.13 HolyWu's lsw does not need avresample as it uses libswresample # 2018.11.23 superseded: liblsw required --enable-avresample which was deprecated
+							'--enable-strip --enable-lto --enable-pic '
+							'--bit-depth=all '
+							'--chroma-format=all ' # 2020.06.09
+							'--extra-cflags="-DLIBXML_STATIC" --extra-cflags="-DGLIB_STATIC_COMPILATION" '
+							'--disable-cli --disable-lavf ' # 2020.06.09 re-added --disable-lavf to libx264 building only
+							,
+	'depends_on' : [
+		'opencl_icd.py', # 2020.06.09
+		# 'libffmpeg_extra', 'liblsw',  # 2019.12.13 HolyWu's lsw does not need avresample as it uses libswresample # 2018.11.23 superseded: liblsw required --enable-avresample which was deprecated
+	],
+	'env_exports' : { # 2020.06.09 hope this happens AFTER dependencies built
+		'PKGCONFIG' : 'pkg-config',
+		'SWSCALE_LIBS' : '!CMD(pkg-config --libs libswscale)CMD!',
+		'LAVF_LIBS'    : '!CMD(pkg-config --libs libavformat libavcodec libavutil libswscale)CMD!',  # 2020.06.09
+		'LAVF_CFLAGS'  : '!CMD(pkg-config --cflags libavformat libavcodec libavutil libswscale)CMD!',  # 2020.06.09
+	},
+	#'run_post_patch' : [ # 2020.06.09 see what should be returned for env_exports
+	#	'pkg-config --libs libswscale',
+	#	'pkg-config --libs libavformat libavcodec libavutil libswscale',
+	#	'pkg-config --cflags libavformat libavcodec libavutil libswscale',  # 2020.06.09
 	#],
-    #'env_exports' : { # 2020.06.09 hope this happens AFTER dependencies built
-	#	##'PKG_CONFIG'   : 'PKGCONFIG' : 'pkg-config', # 2020.06.09
-	#	##'PKG_CONFIG'   : 'PKGCONFIG' : '{cross_prefix_full}pkg-config' # 2020.06.09
-	#	'LAVF_LIBS'    : '!CMD({cross_prefix_full}pkg-config --libs libavformat libavcodec libavutil libswscale)CMD!',  # 2020.06.09
-	#	'LAVF_CFLAGS'  : '!CMD({cross_prefix_full}pkg-config --cflags libavformat libavcodec libavutil libswscale)CMD!',  # 2020.06.09
-	#	'SWSCALE_LIBS' : '!CMD({cross_prefix_full}pkg-config --libs libswscale)CMD!',
-	#},
 	#'run_post_configure' : [ # 2020.06.09 see if the EXPORT stuff worked
+	#	'export | grep SWSCALE_LIBS',
 	#	'export | grep LAVF_LIBS',
 	#	'export | grep LAVF_CFLAGS',
-	#	'export | grep SWSCALE_LIBS',
 	#],
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'x264 (library) multibit' },
