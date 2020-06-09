02230fbd811c16b58497a019f81e49c9fa4ee2e5
hydra3333 <hydra3333@gmail.com>
2020-06-09 12:50:26 +0930

    added some dependencies to rtmpdump, to cater for gnutls-3.6.14
    added some dependencies to rtmpdump, to cater for gnutls-3.6.14

--- packages/dependencies/rtmpdump.py
+++ packages/dependencies/rtmpdump.py
@@ -2,8 +2,10 @@
 	'repo_type' : 'git',
 	'url' : 'https://git.ffmpeg.org/rtmpdump.git',
 	'needs_configure' : False,
-	'install_options' : 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lpthread -lgnutls -lhogweed -lnettle -lgmp -lcrypt32 -lwinmm -lz -liconv -lintl -liconv -lssp" OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}', # 2019.12.13
-	'build_options'   : 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lpthread -lgnutls -lhogweed -lnettle -lgmp -lcrypt32 -lwinmm -lz -liconv -lintl -liconv -lssp" OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}',  # 2019.12.13
+	'install_options' : 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lpthread -lgnutls -lhogweed -lnettle -lgmp '
+							'-lcrypt32 -lncrypt -lbcrypt -lwinmm -lz -liconv -lintl -liconv -lssp" OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}', # 2020.06.09 added -lncrypt -lbcrypt  from gnutls .14
+	'build_options'   : 'SYS=mingw CRYPTO=GNUTLS LIB_GNUTLS="-L{target_prefix}/lib -lpthread -lgnutls -lhogweed -lnettle -lgmp '
+							'-lcrypt32 -lncrypt -lbcrypt -lwinmm -lz -liconv -lintl -liconv -lssp" OPT="{original_cflags}" CROSS_COMPILE={cross_prefix_bare} SHARED=no prefix={target_prefix}', # 2020.06.09 added -lncrypt -lbcrypt  from gnutls .14
 	'run_post_install' :( # 2019.12.13
         'sed -i.bak \'s/-lrtmp -lz/-lrtmp -lwinmm -lz -lintl -liconv -lssp/\' "{pkg_config_path}/librtmp.pc"', # 2019.12.13
 	),
