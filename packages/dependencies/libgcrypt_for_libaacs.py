45b3c5a53eac06ef9ad4c117406c86689b0da016
hydra3333 <hydra3333@gmail.com>
2020-06-17 20:08:47 +0930

    prep for build libaacs.dll

---
+++ packages/dependencies/libgcrypt_for_libaacs.py
@@ -0,0 +1,26 @@
+{ # 2019.12.13 # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgcrypt.git;a=summary # This DAMN thing fails to build with "versioninfo.rc.in:21: syntax error" if not built directly from a GIT clone
+  # see https://forum.videolan.org/viewtopic.php?f=32&t=127218&p=432818&hilit=compile+libaacs#p432818
+
+	#'repo_type' : 'archive',
+	#'url' : 'https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.8.3.tar.bz2',
+	'repo_type' : 'git',
+	'recursive_git' : True,
+	'depth_git' : 0, # 2019.12.13 otherwise too shallow for the specfified commit to fetch
+	'url' : 'git://git.gnupg.org/libgcrypt.git',
+	#'branch' : '7c2943309d14407b51c8166c4dcecb56a3628567', # 2020.03.19 try git master # 7c2943309d14407b51c8166c4dcecb56a3628567 works with no errors
+	'rename_folder' : 'libgcrypt_for_aacs_git',
+	#'configure_options': '--host={target_host} --prefix={output_prefix}/libaacs_dll_git.installed --with-gpg-error-prefix={output_prefix}/libaacs_dll_git.installed --disable-doc '
+	#			'--enable-ciphers=aes --enable-pubkey-ciphers=ecc --enable-digests=sha1 --enable-digests=sha256  --enable-digests=sha512  --enable-random=w32 '
+	#			'--enable-kdfs=pkdf2 --disable-padlock-support --enable-threads=windows --disable-asm ',
+	'configure_options': '--host={target_host} --prefix={output_prefix}/libaacs_dll_git.installed --with-gpg-error-prefix={output_prefix}/libaacs_dll_git.installed --disable-doc --enable-threads=windows --disable-asm ',
+	'custom_cflag' : ' {original_fortify_source_trim} ', # 2019.12.13 this does not like -O3 -fstack-protector-all ... use one or more of {original_cflag_trim} {original_stack_protector_trim} {original_fortify_source_trim}
+	'run_post_regexreplace' : (
+		##'./autogen.sh --find-version',
+		#'autoreconf -fiv',
+	),
+	'depends_on' : (
+		'libgpg_error_for_libaacs', 
+	),
+	'update_check' : { 'type' : 'git', },
+	'_info' : { 'version' : 'git (7c2943309d14407b51c8166c4dcecb56a3628567)', 'fancy_name' : 'libgcrypt for libaacs' },
+}
