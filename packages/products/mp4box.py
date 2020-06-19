7fd217abb53ffa7f324eb027f12c7857f9715261
hydra3333 <hydra3333@gmail.com>
2020-06-19 22:28:58 +0930

    fix gpac capitalisation issues

--- packages/products/mp4box.py
+++ packages/products/mp4box.py
@@ -4,6 +4,9 @@
 	'rename_folder' : 'mp4box_git',
 	'do_not_bootstrap' : True,
 	'run_post_regexreplace' : [
+		'sed -i.bak \'s/Windows.h/windows.h/g\' "include/gpac/thread.h"', # 2020.06.19 
+		'sed -i.bak \'s/Windows.h/windows.h/g\' "src/filters/dec_nvdec_sdk.c"', # 2020.06.19 
+		'sed -i.bak \'s/Windows.h/windows.h/g\' "src/filters/dec_nvdec_sdk.h"', # 2020.06.19 
 		'sed -i.bak \'s/has_dvb4linux="yes"/has_dvb4linux="no"/g\' configure',
 		'sed -i.bak \'s/targetos=`uname -s`/targetos=MINGW64/g\' configure',
 		'sed -i.bak \'s/extralibs="-lm"/extralibs=""/g\' configure',
@@ -12,7 +15,7 @@
 	],
 	'configure_options' : '--host={target_host} --target-os={bit_name3} --prefix={output_prefix}/mp4box_git.installed --static-modules --cross-prefix={cross_prefix_bare} --static-mp4box --enable-static-bin --disable-oss-audio --disable-x11 --disable-docs --disable-shared --enable-static --extra-cflags="-DLIBXML_STATIC" --extra-cflags="-DGLIB_STATIC_COMPILATION" --enable-sdl-static ', # 2019.12.13 # 2020.05.13 remove --sdl-cfg={cross_prefix_full}sdl2-config 
 	'depends_on' : [
-		 'libffmpeg', 'sdl2', # 2020.05.13 re-enable # 2020.05.13 remove SDL2 'sdl2', 
+		 'libffmpeg_extra', 'sdl2', # 2020.05.13 re-enable # 2020.05.13 remove SDL2 'sdl2', 
 	],
 	'_info' : { 'version' : None, 'fancy_name' : 'mp4box' },
 }
