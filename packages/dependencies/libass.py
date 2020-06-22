926ed04e04fae1d1c207e064c5928fc2038b2f01
hydra3333 <hydra3333@gmail.com>
2020-06-23 01:54:58 +0930

    Add files via upload

--- packages/dependencies/libass.py
+++ packages/dependencies/libass.py
@@ -2,6 +2,12 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/libass/libass.git',
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --enable-silent-rules',
+	'env_exports' : { # 2020.06.19
+		'CFLAGS'   : ' -DFRIBIDI_LIB_STATIC {original_cflags}',
+		'CXXFLAGS' : ' -DFRIBIDI_LIB_STATIC {original_cflags}',
+		'CPPFLAGS' : ' -DFRIBIDI_LIB_STATIC {original_cflags}', # 2020.06.20 per https://github.com/fribidi/fribidi/issues/146#issuecomment-646991416
+		'LDFLAGS'  : ' -DFRIBIDI_LIB_STATIC {original_cflags}',
+	},
     'run_post_install': [
 		'sed -i.bak \'s/-lass -lm/-lass -lfribidi -lfreetype -lexpat -lm/\' "{pkg_config_path}/libass.pc"', #-lfontconfig # 2018.12.13
 	],
