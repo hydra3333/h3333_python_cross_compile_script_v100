9fa3f3756db1c24d26ccccef0d8daba95b0a383f
hydra3333 <hydra3333@gmail.com>
2020-06-23 16:32:29 +0930

    gcc version confirmation

--- mingw_toolchain_script/mingw_toolchain_script_v100_001.py
+++ mingw_toolchain_script/mingw_toolchain_script_v100_001.py
@@ -103,7 +103,7 @@ SOURCES['gcc'] = { # https://www.gnu.org/software/gcc/ # https://ftp.gnu.org/gnu
 	'type': 'archive',
 	#'version'   : '10.1.0', # stable versions
 	#'url' : 'https://gcc.gnu.org/pub/gcc/releases/gcc-{version}/gcc-{version}.tar.xz', # stable versions
-	'version': '11-20200517', #'11-20200524', #'11-20200510', #'10-20200502',
+	'version': '11-20200517', # works # '11-20200621', fails with segmentation fault
 	'url' : 'ftp://ftp.fu-berlin.de/unix/languages/gcc/snapshots/{version}/gcc-{version}.tar.xz', # TEST builds server
 	#'patches': [
 		#( 'https://raw.githubusercontent.com/hydra3333/h3333_python_cross_compile_script_v100/master/mingw_toolchain_script/patches/0001-gcc_7_1_0_weak_refs_x86_64.patch', 'p1' ),
