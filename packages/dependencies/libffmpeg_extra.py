b330f06b8848b9742b62341c673aa1a20ddadea8
hydra3333 <hydra3333@gmail.com>
2020-07-22 18:29:15 +0930

    Add files via upload

--- packages/dependencies/libffmpeg_extra.py
+++ packages/dependencies/libffmpeg_extra.py
@@ -4,6 +4,12 @@
 	#'depth_git': 0,
 	#'branch' : '5727b1f13f36c4db30d5d0de51640f740edf01e8',
 	'rename_folder' : 'libffmpeg_extra_git',
+		'env_exports' : { # 2020.06.19
+		'CFLAGS'   : ' -DFRIBIDI_LIB_STATIC {original_cflags}',
+		'CXXFLAGS' : ' -DFRIBIDI_LIB_STATIC {original_cflags}',
+		'CPPFLAGS' : ' -DFRIBIDI_LIB_STATIC {original_cflags}', # 2020.06.20 per https://github.com/fribidi/fribidi/issues/146#issuecomment-646991416
+		'LDFLAGS'  : ' -DFRIBIDI_LIB_STATIC {original_cflags}',
+	},
 	#'configure_options' : '--sysroot={target_sub_prefix} !VAR(ffmpeg_config)VAR! !VAR(ffmpeg_extra_config)VAR! !VAR(ffmpeg_nonfree)VAR! --prefix={target_prefix} --disable-sdl2 --disable-shared --enable-static --disable-doc --disable-programs',
 	'configure_options' : '--sysroot={target_sub_prefix} !VAR(ffmpeg_config)VAR! !VAR(ffmpeg_extra_config)VAR! !VAR(ffmpeg_nonfree)VAR! --prefix={target_prefix} --disable-shared --enable-static --disable-doc --disable-programs',
 	'patches' : [
