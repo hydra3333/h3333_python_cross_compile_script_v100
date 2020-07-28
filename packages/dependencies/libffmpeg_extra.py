7f11ddbc37aa18b8055c9fcfa5cd64fb17224ffb
hydra3333 <hydra3333@gmail.com>
2020-07-28 14:14:54 +0930

    srt in ffmpeg fixed upstream

--- packages/dependencies/libffmpeg_extra.py
+++ packages/dependencies/libffmpeg_extra.py
@@ -12,9 +12,6 @@
 	},
 	#'configure_options' : '--sysroot={target_sub_prefix} !VAR(ffmpeg_config)VAR! !VAR(ffmpeg_extra_config)VAR! !VAR(ffmpeg_nonfree)VAR! --prefix={target_prefix} --disable-sdl2 --disable-shared --enable-static --disable-doc --disable-programs',
 	'configure_options' : '--sysroot={target_sub_prefix} !VAR(ffmpeg_config)VAR! !VAR(ffmpeg_extra_config)VAR! !VAR(ffmpeg_nonfree)VAR! --prefix={target_prefix} --disable-shared --enable-static --disable-doc --disable-programs',
-	'patches' : [
-        ('ffmpeg/0001-Replace-deprecated-libsrt-calls-from-MABS.patch', '-p1'), # 2020.07.01 fix deprecated stuff
-	 ],
 	'depends_on' : [ 'ffmpeg_depends', 'ffmpeg_depends_extra', 'ffmpeg_depends_nonfree', ],
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'FFmpeg (library,nonfree,extra)' },
