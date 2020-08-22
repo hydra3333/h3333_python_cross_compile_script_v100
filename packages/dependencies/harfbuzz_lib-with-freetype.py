578d7f0fa04bb686ad04ba44225af9901a078832
hydra3333 <hydra3333@gmail.com>
2020-08-22 15:02:17 +0930

    misc dependency updates

--- packages/dependencies/harfbuzz_lib-with-freetype.py
+++ packages/dependencies/harfbuzz_lib-with-freetype.py
@@ -1,12 +1,10 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		#{ 'url' : 'https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.6.4.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '9413b8d96132d699687ef914ebb8c50440efc87b3f775d25856d7ec347c03c12' }, ], },
-		#{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.6.4.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '9413b8d96132d699687ef914ebb8c50440efc87b3f775d25856d7ec347c03c12' }, ], },
-		#{ 'url' : 'https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-2.6.7.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '49e481d06cdff97bf68d99fa26bdf785331f411614485d892ea4c78eb479b218' }, ], },
-		#{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.6.7.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '49e481d06cdff97bf68d99fa26bdf785331f411614485d892ea4c78eb479b218' }, ], },
-		{ 'url' : 'https://github.com/harfbuzz/harfbuzz/archive/2.6.8.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '059054760ff471d81171659a136dd253a0ddff7faa9249727cca3d7dbd356a6c' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
-		{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.6.8.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '059054760ff471d81171659a136dd253a0ddff7faa9249727cca3d7dbd356a6c' }, ], }, # https://fossies.org/linux/misc/
+		#{ 'url' : 'https://github.com/harfbuzz/harfbuzz/archive/2.6.8.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '059054760ff471d81171659a136dd253a0ddff7faa9249727cca3d7dbd356a6c' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
+		#{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.6.8.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '059054760ff471d81171659a136dd253a0ddff7faa9249727cca3d7dbd356a6c' }, ], }, # https://fossies.org/linux/misc/
+		{ 'url' : 'https://github.com/harfbuzz/harfbuzz/archive/2.7.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e95ee43b6bd0d3d1307e2aacf0f9c0050e5baceb21988b367b833028114aa569' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
+		{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.7.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e95ee43b6bd0d3d1307e2aacf0f9c0050e5baceb21988b367b833028114aa569' }, ], }, # https://fossies.org/linux/misc/
 	],
 	# 'run_post_install' : [
 		# 'sed -i.bak \'s/Libs: -L${{libdir}} -lharfbuzz.*/Libs: -L${{libdir}} -lharfbuzz -lfreetype/\' "{pkg_config_path}/harfbuzz.pc"',
