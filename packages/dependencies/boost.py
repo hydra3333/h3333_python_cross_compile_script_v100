9942d73de594e00d845de5ca08709670e323a945
hydra3333 <hydra3333@gmail.com>
2020-08-23 12:30:51 +0930

    boost, always do the actions

--- packages/dependencies/boost.py
+++ packages/dependencies/boost.py
@@ -10,13 +10,20 @@
 	'needs_make' :False,
 	'needs_make_install' :False,
 	'needs_configure' :False,
-	'run_post_regexreplace' : [
-		'if [ ! -f "already_configured_0" ] ; then ./bootstrap.sh mingw --prefix={target_prefix} ; fi',
-		'if [ ! -f "already_configured_0" ] ; then sed -i.bak \'s/case \*       : option = -pthread ; libs = rt ;/case *      : option = -pthread ;/\' tools/build/src/tools/gcc.jam ; fi',
-		'if [ ! -f "already_configured_0" ] ; then touch already_configured_0 ; fi',
-		'if [ ! -f "already_ran_make_0" ] ; then echo "using gcc : mingw : {cross_prefix_bare}g++ : <rc>{cross_prefix_bare}windres <archiver>{cross_prefix_bare}ar <ranlib>{cross_prefix_bare}ranlib ;" > user-config.jam ; fi',
-		'if [ ! -f "already_ran_make_0" ] ; then ./b2 toolset=gcc-mingw link=static threading=multi target-os=windows address-model=64 architecture=x86 --prefix={target_prefix} variant=release --with-system --with-filesystem --with-regex --with-date_time --with-thread --user-config=user-config.jam install ; fi',
-		'if [ ! -f "already_ran_make_0" ] ; then touch already_ran_make_0 ; fi',
+	#'run_post_regexreplace' : [ # 2020.08.23 comment out
+	#	'if [ ! -f "already_configured_0" ] ; then ./bootstrap.sh mingw --prefix={target_prefix} ; fi',
+	#	'if [ ! -f "already_configured_0" ] ; then sed -i.bak \'s/case \*       : option = -pthread ; libs = rt ;/case *      : option = -pthread ;/\' tools/build/src/tools/gcc.jam ; fi',
+	#	'if [ ! -f "already_configured_0" ] ; then touch already_configured_0 ; fi',
+	#	
+	#	'if [ ! -f "already_ran_make_0" ] ; then echo "using gcc : mingw : {cross_prefix_bare}g++ : <rc>{cross_prefix_bare}windres <archiver>{cross_prefix_bare}ar <ranlib>{cross_prefix_bare}ranlib ;" > user-config.jam ; fi',
+	#	'if [ ! -f "already_ran_make_0" ] ; then ./b2 toolset=gcc-mingw link=static threading=multi target-os=windows address-model=64 architecture=x86 --prefix={target_prefix} variant=release --with-system --with-filesystem --with-regex --with-date_time --with-thread --user-config=user-config.jam install ; fi',
+	#	'if [ ! -f "already_ran_make_0" ] ; then touch already_ran_make_0 ; fi',
+	#],
+	'run_post_regexreplace' : [ # 2020.08.23 always do the actions
+		'./bootstrap.sh mingw --prefix={target_prefix}',
+		'sed -i.bak \'s/case \*       : option = -pthread ; libs = rt ;/case *      : option = -pthread ;/\' tools/build/src/tools/gcc.jam',
+		'echo "using gcc : mingw : {cross_prefix_bare}g++ : <rc>{cross_prefix_bare}windres <archiver>{cross_prefix_bare}ar <ranlib>{cross_prefix_bare}ranlib ;" > user-config.jam',
+		'./b2 toolset=gcc-mingw link=static threading=multi target-os=windows address-model=64 architecture=x86 --prefix={target_prefix} variant=release --with-system --with-filesystem --with-regex --with-date_time --with-thread --user-config=user-config.jam install',
 	],
 	'update_check' : { 'url' : 'https://sourceforge.net/projects/boost/files/boost/', 'type' : 'sourceforge', 'regex' : r'(?P<version_num>[\d.]+)', },
 	'_info' : { 'version' : '1.74.0', 'fancy_name' : 'Boost' },
