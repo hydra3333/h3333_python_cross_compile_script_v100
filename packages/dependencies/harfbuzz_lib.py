e92e6939f42a0974b9bba4f79aa4288466e1d15d
hydra3333 <hydra3333@gmail.com>
2020-09-06 20:30:55 +0930

    harfbuzz to 2.7.2

--- packages/dependencies/harfbuzz_lib.py
+++ packages/dependencies/harfbuzz_lib.py
@@ -3,8 +3,8 @@
 	'download_locations' : [
 		#{ 'url' : 'https://github.com/harfbuzz/harfbuzz/archive/2.7.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e95ee43b6bd0d3d1307e2aacf0f9c0050e5baceb21988b367b833028114aa569' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
 		#{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.7.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e95ee43b6bd0d3d1307e2aacf0f9c0050e5baceb21988b367b833028114aa569' }, ], }, # https://fossies.org/linux/misc/
-		{ 'url' : 'https://github.com/harfbuzz/harfbuzz/archive/2.7.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'b8c048d7c2964a12f2c80deb6634dfc836b603dd12bf0d0a3df1627698e220ce' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
-		{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.7.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'b8c048d7c2964a12f2c80deb6634dfc836b603dd12bf0d0a3df1627698e220ce' }, ], }, # https://fossies.org/linux/misc/
+		{ 'url' : 'https://github.com/harfbuzz/harfbuzz/archive/2.7.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'b8c048d7c2964a12f2c80deb6634dfc836b603dd12bf0d0a3df1627698e220ce' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
+		{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.7.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'b8c048d7c2964a12f2c80deb6634dfc836b603dd12bf0d0a3df1627698e220ce' }, ], }, # https://fossies.org/linux/misc/
 	],
 	'folder_name' : 'harfbuzz-lib',
 	'rename_folder' : 'harfbuzz-lib',
@@ -24,6 +24,6 @@
 	],
 	'update_check' : { 'url' : 'https://github.com/harfbuzz/harfbuzz/releases', 'type' : 'githubreleases', 'name_or_tag' : 'name' },
 	#'update_check' : { 'url' : 'https://www.freedesktop.org/software/harfbuzz/release/?C=M;O=D', 'type' : 'httpindex', 'regex' : r'harfbuzz-(?P<version_num>[\d.]+)\.tar\.xz' },
-	'_info' : { 'version' : '2.7.1', 'fancy_name' : 'harfbuzz' },
+	'_info' : { 'version' : '2.7.2', 'fancy_name' : 'harfbuzz' },
 }
 
