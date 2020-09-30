7ef078b76f5df3416888656f195a8143a77318e7
hydra3333 <hydra3333@gmail.com>
2020-09-30 18:48:49 +0930

    nv-codec-headers to use github mirror

--- packages/dependencies/nv-codec-headers.py
+++ packages/dependencies/nv-codec-headers.py
@@ -1,7 +1,7 @@
 {
 	'repo_type' : 'git',
-	'url' : 'https://git.videolan.org/git/ffmpeg/nv-codec-headers.git',
-	#'url' : 'https://github.com/FFmpeg/nv-codec-headers.git', # 2020.04.20 videolan not responding :(
+	#'url' : 'https://git.videolan.org/git/ffmpeg/nv-codec-headers.git',
+	'url' : 'https://github.com/FFmpeg/nv-codec-headers.git', # 2020.09.30 videolan not responding :(
 	'needs_configure' : False,
 	'build_options' : 'PREFIX={target_prefix}',
 	'install_options' : 'PREFIX={target_prefix}',
