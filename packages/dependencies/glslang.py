828e0d65c1c46af791b9debe37ac4dba2598b3b3
hydra3333 <hydra3333@gmail.com>
2020-10-10 18:52:29 +1030

    make glslang build more like mabs build
    yet to loo into MABS "log dependencies /usr/bin/python ./update_glslang_sources.py"
    so this is unlikely to work as yet

--- packages/dependencies/glslang.py
+++ packages/dependencies/glslang.py
@@ -3,15 +3,30 @@
 	'rename_folder' : 'glslang',
 	'url' : 'https://github.com/KhronosGroup/glslang.git',
 	'depth_git': 0,
+	'recursive_git' : True,
 	#'branch': '3ed344dd784ecbbc5855e613786f3a1238823e56', # 2020.04.20 COMMENTED OUT
+	'conf_system' : 'cmake',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DUNIX=OFF ',
 	'patches' : [ 
 		('glslang/glslang-0001-fix-gcc-10.1-error-from-shinchiro.patch', '-p1'), # 2020.05.31 only when using gcc 10.1
 	],
-	'needs_make' : False,
-	'needs_make_install' : False,
-	'needs_configure' : False,
-	'recursive_git' : True,
 	'update_check' : { 'type' : 'git', },
-	#'_info' : { 'version' : 'git (3ed344dd784ecbbc5855e613786f3a1238823e56)', 'fancy_name' : 'glslang' },
 	'_info' : { 'version' : 'git', 'fancy_name' : 'glslang' },
 }
+#{
+#	'repo_type' : 'git',
+#	'rename_folder' : 'glslang',
+#	'url' : 'https://github.com/KhronosGroup/glslang.git',
+#	'depth_git': 0,
+#	#'branch': '3ed344dd784ecbbc5855e613786f3a1238823e56', # 2020.04.20 COMMENTED OUT
+#	'patches' : [ 
+#		('glslang/glslang-0001-fix-gcc-10.1-error-from-shinchiro.patch', '-p1'), # 2020.05.31 only when using gcc 10.1
+#	],
+#	'needs_make' : False,
+#	'needs_make_install' : False,
+#	'needs_configure' : False,
+#	'recursive_git' : True,
+#	'update_check' : { 'type' : 'git', },
+#	#'_info' : { 'version' : 'git (3ed344dd784ecbbc5855e613786f3a1238823e56)', 'fancy_name' : 'glslang' },
+#	'_info' : { 'version' : 'git', 'fancy_name' : 'glslang' },
+#}
