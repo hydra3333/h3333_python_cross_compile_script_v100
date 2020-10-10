51df1ce255f44e624c37610043e71b1806e5447f
hydra3333 <hydra3333@gmail.com>
2020-10-10 20:54:31 +1030

    ensure glslang is type Release not debug

--- packages/dependencies/glslang.py
+++ packages/dependencies/glslang.py
@@ -5,16 +5,16 @@
 	'depth_git': 0,
 	'recursive_git' : True,
 	'conf_system' : 'cmake', # from MABS 2020.10.10
-	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DUNIX=OFF -DSKIP_GLSLANG_INSTALL=OFF -DENABLE_GLSLANG_INSTALL=ON -DENABLE_GLSLANG_BINARIES=OFF -DENABLE_CTEST=OFF -DENABLE_GLSLANG_WEBMIN=OFF -DUSE_CCACHE=OFF', #  # from MABS 2020.10.10 -DENABLE_SPVREMAPPER=OFF
+	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DUNIX=OFF -DSKIP_GLSLANG_INSTALL=OFF -DENABLE_GLSLANG_INSTALL=ON -DENABLE_GLSLANG_BINARIES=OFF -DENABLE_CTEST=OFF -DENABLE_GLSLANG_WEBMIN=OFF -DUSE_CCACHE=OFF -DCMAKE_BUILD_TYPE=Release', #  # from MABS 2020.10.10 -DENABLE_SPVREMAPPER=OFF
 	'patches' : [ 
 		('glslang/glslang-0001-fix-gcc-10.1-error-from-shinchiro.patch', '-p1'), # 2020.05.31 only when using gcc 10.1
 	],
 	'run_post_patch' : [ 
 		'rm -vf "./compile_commands.json"'
 	],
-	'run_post_install' : [ 
-		'cp -vf "{target_prefix}/lib/libglslangd.a" "{target_prefix}/lib/libglslang.a"'
-	],
+	#'run_post_install' : [ 
+	#	'cp -vf "{target_prefix}/lib/libglslangd.a" "{target_prefix}/lib/libglslang.a"' # 2020.10.10 only needed if CMAKE_BUILD_TYPE not defined or is "Debug"
+	#],
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git', 'fancy_name' : 'glslang' },
 }
