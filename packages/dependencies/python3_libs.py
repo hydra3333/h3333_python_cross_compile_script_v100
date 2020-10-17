b256db4182e168e5f665d19d9511b6efab11a212
hydra3333 <hydra3333@gmail.com>
2020-10-17 18:25:00 +1030

    vapoursynth R52 only uses python3 3.8.6 not 3.9.0
    vapoursynth R52 only uses python3 3.8.6 not 3.9.0

--- packages/dependencies/python3_libs.py
+++ packages/dependencies/python3_libs.py
@@ -21,8 +21,8 @@
 		'sed -i.bak "s;_DEBUG = False;_DEBUG = True;g" "install_python_libs.py"',
 		'diff -U 5 install_python_libs.py.orig install_python_libs.py && echo "NO difference" || echo "YES differences!"',
 	],
-	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool PYTHON_VERSION=3.9.0',
-	#'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool PYTHON_VERSION=3.8.6',
+	#'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool PYTHON_VERSION=3.9.0', # for vapoursynth R53 ?
+	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool PYTHON_VERSION=3.8.6', # for vapoursynth R52
 	# eg 
 	# make -j 4 PREFIX=/home/u/Desktop/_working/workdir/toolchain/x86_64-w64-mingw32/x86_64-w64-mingw32 GENDEF=/home/u/Desktop/_working/workdir/toolchain/x86_64-w64-mingw32/bin/gendef DLLTOOL=/home/u/Desktop/_working/workdir/toolchain/x86_64-w64-mingw32/bin/x86_64-w64-mingw32-dlltool PYTHON_VERSION=3.8.5
 	'update_check' : { 'type' : 'git', },
