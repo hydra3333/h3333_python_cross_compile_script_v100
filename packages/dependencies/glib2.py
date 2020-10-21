d60f5363eeadf9021a07b42a0b441ff0b0349494
hydra3333 <hydra3333@gmail.com>
2020-10-21 18:51:16 +1030

    Add files via upload

--- packages/dependencies/glib2.py
+++ packages/dependencies/glib2.py
@@ -1,10 +1,15 @@
 { # 2020.10.21 be more like RDP for later versions of glib2
-	'repo_type' : 'archive',
-	'download_locations' : [
-		#{ 'url' : 'https://download.gnome.org/sources/glib/2.64/glib-2.64.3.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'fe9cbc97925d14c804935f067a3ad77ef55c0bbe9befe68962318f5a767ceb22' }, ], }, # 2020.10.21 try this version
-		{ 'url' : 'https://download.gnome.org/sources/glib/2.66/glib-2.66.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'ec390bed4e8dd0f89e918f385e8d4cfd7470b1ef7c1ce93ec5c4fc6e3c6a17c4' }, ], }, # 2020.10.21 try this version
-		{ 'url' : 'https://fossies.org/linux/misc/glib-2.66.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'ec390bed4e8dd0f89e918f385e8d4cfd7470b1ef7c1ce93ec5c4fc6e3c6a17c4' }, ], }, # 2020.10.21 try this version
-	],
+	#'repo_type' : 'archive',
+	#'download_locations' : [
+	#	#{ 'url' : 'https://download.gnome.org/sources/glib/2.64/glib-2.64.3.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'fe9cbc97925d14c804935f067a3ad77ef55c0bbe9befe68962318f5a767ceb22' }, ], }, # 2020.10.21 try this version
+	#	{ 'url' : 'https://download.gnome.org/sources/glib/2.66/glib-2.66.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'ec390bed4e8dd0f89e918f385e8d4cfd7470b1ef7c1ce93ec5c4fc6e3c6a17c4' }, ], }, # 2020.10.21 try this version
+	#	{ 'url' : 'https://fossies.org/linux/misc/glib-2.66.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'ec390bed4e8dd0f89e918f385e8d4cfd7470b1ef7c1ce93ec5c4fc6e3c6a17c4' }, ], }, # 2020.10.21 try this version
+	#],
+	'repo_type' : 'git',
+	'depth_git' : 0,
+	'url' : 'https://github.com/GNOME/glib.git', # this is a reasonably up to date mirror
+	'branch' : 'tags/2.66.2',
+	'folder_name' : 'glib2',
 	'conf_system' : 'meson',
 	'build_system' : 'ninja',
 	'source_subfolder' : 'build',
@@ -37,19 +42,18 @@
 		#('glib2/glib2-debug-patch-2020.10.21.patch', '-Np1', '..'), # note Np1, MY TEMPORARY DEBUG PATCH ON TOP OF PRIOR PATCHES for meson.build
 	],
 	'run_post_patch' : [
-		'ls -al "../meson.build*"',
-		'ls -al "../meson.build*"',
+		'ls -al "../"',
 		'cp -fv "../meson.build" "../meson.build.orig_post_patch"',
-		'sed -iBAK "s;installed_tests_metadir =;message(\'glib_prefix=\' + glib_prefix)\ninstalled_tests_metadir =;g" "../meson.build"',
-		'sed -iBAK "s;installed_tests_metadir =;message(\'glib_bindir=\' + glib_bindir)\ninstalled_tests_metadir =;g" "../meson.build"',
-		'sed -iBAK "s;installed_tests_metadir =;message(\'glib_libdir=\' + glib_libdir)\ninstalled_tests_metadir =;g" "../meson.build"',
-		'sed -iBAK "s;installed_tests_metadir =;message(\'glib_libexecdir=\' + glib_libexecdir)\ninstalled_tests_metadir =;g" "../meson.build"',
-		'sed -iBAK "s;installed_tests_metadir =;message(\'glib_datadir=\' + glib_datadir)\ninstalled_tests_metadir =;g" "../meson.build"',
-		'sed -iBAK "s;installed_tests_metadir =;message(\'glib_pkgdatadir=\' + glib_pkgdatadir)\ninstalled_tests_metadir =;g" "../meson.build"',
-		'sed -iBAK "s;installed_tests_metadir =;message(\'glib_includedir=\' + glib_includedir)\ninstalled_tests_metadir =;g" "../meson.build"',
-		'sed -iBAK "s;installed_tests_metadir =;message(\'glib_pkgconfigreldir=\' + glib_pkgconfigreldir)\ninstalled_tests_metadir =;g" "../meson.build"',
-		'sed -iBAK "s;installed_tests_metadir =;message(\'glib_charsetaliasdir=\' + glib_charsetaliasdir)\ninstalled_tests_metadir =;g" "../meson.build"',
-		'sed -iBAK "s;installed_tests_metadir =;\ninstalled_tests_metadir =;g" "../meson.build"',
+		'sed -i.bak"s;installed_tests_metadir =;message(\'glib_prefix=\' + glib_prefix)\\ninstalled_tests_metadir =;g" "../meson.build"',
+		'sed -i.bak"s;installed_tests_metadir =;message(\'glib_bindir=\' + glib_bindir)\\ninstalled_tests_metadir =;g" "../meson.build"',
+		'sed -i.bak"s;installed_tests_metadir =;message(\'glib_libdir=\' + glib_libdir)\\ninstalled_tests_metadir =;g" "../meson.build"',
+		'sed -i.bak"s;installed_tests_metadir =;message(\'glib_libexecdir=\' + glib_libexecdir)\\ninstalled_tests_metadir =;g" "../meson.build"',
+		'sed -i.bak"s;installed_tests_metadir =;message(\'glib_datadir=\' + glib_datadir)\\ninstalled_tests_metadir =;g" "../meson.build"',
+		'sed -i.bak"s;installed_tests_metadir =;message(\'glib_pkgdatadir=\' + glib_pkgdatadir)\\ninstalled_tests_metadir =;g" "../meson.build"',
+		'sed -i.bak"s;installed_tests_metadir =;message(\'glib_includedir=\' + glib_includedir)\\ninstalled_tests_metadir =;g" "../meson.build"',
+		'sed -i.bak"s;installed_tests_metadir =;message(\'glib_pkgconfigreldir=\' + glib_pkgconfigreldir)\\ninstalled_tests_metadir =;g" "../meson.build"',
+		'sed -i.bak"s;installed_tests_metadir =;message(\'glib_charsetaliasdir=\' + glib_charsetaliasdir)\\ninstalled_tests_metadir =;g" "../meson.build"',
+		'sed -i.bak"s;installed_tests_metadir =;\\ninstalled_tests_metadir =;g" "../meson.build"',
 		'diff -U 5 "../meson.build.orig_post_patch" "../meson.build" && echo "NO difference" || echo "YES differences!"',
 		#
 		'/home/u/Desktop/_working/workdir/toolchain/x86_64-w64-mingw32/bin/pkg-config --modversion libelf',
