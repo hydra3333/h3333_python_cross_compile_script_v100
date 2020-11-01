8629e87445090de7645bf2ae81742354c06195a8
hydra3333 <hydra3333@gmail.com>
2020-11-01 13:45:20 +1030

    Add files via upload

--- packages/dependencies/liblsw.py
+++ packages/dependencies/liblsw.py
@@ -2,8 +2,8 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/HolyWu/L-SMASH-Works.git', # 2019.11.19 swap to HolyWu's fork as it seems more updated
 	'depth_git' : 0,
-	#'branch' : 'ab66a24f255dc89c03eb955c1a996a12d9c7595a', # 2020.08.21 this commit works per https://github.com/HolyWu/L-SMASH-Works/issues/11
-	'branch' : 'tags/20200728',
+	#'branch' : 'ab66a24f255dc89c03eb955c1a996a12d9c7595a', # 2020.08.21 this commit works per https://github.com/HolyWu/L-SMASH-Works/issues/11 (space between 
+	#'branch' : 'TAGS/20200728',
 	'env_exports' : {
 		'PKGCONFIG' : 'pkg-config',
 	},
@@ -13,10 +13,14 @@
 	'run_post_regexreplace' : [
 		#'ls -al',
 		#'ls -al ..',
+		'cp -fv "../lsmashsource.h" "../lsmashsource.h.old"',
 		'sed -i.BAK \'s;<VapourSynth.h>;"VapourSynth.h";g\' "../lsmashsource.h"',
-		#'sed -i.BAK \'s;<VSHelper.h>;"VSHelper.h";g\' "../video_output.c"', # 2020.11.01
+		'diff -U 5 "../lsmashsource.h.old" "../lsmashsource.h" && echo "NO difference" || echo "YES differences!"',
+		'cp -fv "../meson.build" "../meson.build.old"',
 		'sed -i.BAK "s;vapoursynth_dep =;includes = include_directories(\'../include\')\\n\\nvapoursynth_dep =;g" "../meson.build"',
-		'sed -i.BAK \'s;dependencies: deps,;dependencies : deps,\\ninclude_directories : includes,;g\' "../meson.build"', # 2020.09.10 removed a space prior to :
+		'sed -i.BAK \'s;dependencies: deps,;dependencies : deps,\\ninclude_directories : includes,;g\' "../meson.build"', # 2020.09.10 removed a space prior to : to fix an upstream commit
+		'diff -U 5 "../meson.build.old" "../meson.build" && echo "NO difference" || echo "YES differences!"',
+		'sleep 3',
 		#'sed -iBAK \'s;;;g\' "../"',
 	],
 	'configure_options' :
