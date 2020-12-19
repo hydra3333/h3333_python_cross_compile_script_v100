8aa5e948312e7f342dd3a6214484c3071df67b3e
hydra3333 <hydra3333@gmail.com>
2020-12-19 11:51:33 +1030

    snappy broke upstream so affix to a working commit

--- packages/dependencies/libsnappy.py
+++ packages/dependencies/libsnappy.py
@@ -2,7 +2,7 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/google/snappy.git',
 	'depth_git' : 0,
-	#'branch' : '537f4ad6240e586970fe554614542e9717df7902', # 2020.04.11 it broke after this
+	'branch' : '0793e2ae2d51640ae569b75ffb42fc444afafb32', # 2020.12.19 it broke after this
 	'conf_system' : 'cmake',
 	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DBUILD_BINARY=OFF -DSNAPPY_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release',
 	'run_post_install' : [
