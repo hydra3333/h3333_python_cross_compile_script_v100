c72bd1d31d386601c16a294f73aa2634c5256446
hydra3333 <hydra3333@gmail.com>
2020-12-19 10:48:57 +1030

    Add files via upload

--- packages/dependencies/iconv.py
+++ packages/dependencies/iconv.py
@@ -3,9 +3,12 @@
 	'download_locations' : [
 		{ 'url' : 'https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.16.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04' }, ], },
 		{ 'url' : 'https://fossies.org/linux/misc/libiconv-1.16.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e6a1b1b589654277ee790cce3734f07876ac4ccfaecbee8afa0b649cf529cc04' }, ], },
+		#{ 'url' : 'https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '' }, ], },
+		#{ 'url' : 'https://fossies.org/linux/misc/libiconv-1.17.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '' }, ], },
 	],
 	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static --disable-nls --enable-extra-encodings', # 2020.03.19 --enable-nls
 	#'depends_on' : [ 'gettext', ], # no, instead, gettext depends on iconv
 	'update_check' : { 'url' : 'https://ftp.gnu.org/pub/gnu/libiconv/?C=M;O=D', 'type' : 'httpindex', 'regex' : r'libiconv-(?P<version_num>[\d.]+)\.tar\.gz' },
 	'_info' : { 'version' : '1.16', 'fancy_name' : 'libiconv' },
+	#'_info' : { 'version' : '1.17', 'fancy_name' : 'libiconv' },
 }
--- packages/dependencies/libilbc.py
+++ packages/dependencies/libilbc.py
@@ -1,6 +1,7 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/dekkers/libilbc.git',
+	'branch' : 'main',  # 2020.12.19 they've changed the trunk from master to main (a US political race thing against the word, apparently)
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release',
