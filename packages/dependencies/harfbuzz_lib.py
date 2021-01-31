fc4046a15b67cec9980fdae1beb9019df5e09e68
hydra3333 <hydra3333@gmail.com>
2021-01-31 13:26:22 +1030

    updated dependencies

--- packages/dependencies/harfbuzz_lib.py
+++ packages/dependencies/harfbuzz_lib.py
@@ -2,8 +2,11 @@
 	'repo_type' : 'archive',
 	'download_locations' : [
 		#{ 'url' : 'https://github.com/harfbuzz/harfbuzz/archive/2.7.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'b8c048d7c2964a12f2c80deb6634dfc836b603dd12bf0d0a3df1627698e220ce' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
-		{ 'url' : 'https://github.com/harfbuzz/harfbuzz/releases/download/2.7.2/harfbuzz-2.7.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'b8c048d7c2964a12f2c80deb6634dfc836b603dd12bf0d0a3df1627698e220ce' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
-		{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.7.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'b8c048d7c2964a12f2c80deb6634dfc836b603dd12bf0d0a3df1627698e220ce' }, ], }, # https://fossies.org/linux/misc/
+		#{ 'url' : 'https://github.com/harfbuzz/harfbuzz/releases/download/2.7.2/harfbuzz-2.7.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'b8c048d7c2964a12f2c80deb6634dfc836b603dd12bf0d0a3df1627698e220ce' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
+		#{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.7.2.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'b8c048d7c2964a12f2c80deb6634dfc836b603dd12bf0d0a3df1627698e220ce' }, ], }, # https://fossies.org/linux/misc/
+		{ 'url' : 'https://github.com/harfbuzz/harfbuzz/releases/download/2.7.4/harfbuzz-2.7.4.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '6ad11d653347bd25d8317589df4e431a2de372c0cf9be3543368e07ec23bb8e7' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
+		{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.7.4.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '6ad11d653347bd25d8317589df4e431a2de372c0cf9be3543368e07ec23bb8e7' }, ], }, # https://fossies.org/linux/misc/
+
 	],
 	'folder_name' : 'harfbuzz-lib',
 	'rename_folder' : 'harfbuzz-lib',
@@ -23,7 +26,7 @@
 	],
 	'update_check' : { 'url' : 'https://github.com/harfbuzz/harfbuzz/releases', 'type' : 'githubreleases', 'name_or_tag' : 'name' },
 	#'update_check' : { 'url' : 'https://www.freedesktop.org/software/harfbuzz/release/?C=M;O=D', 'type' : 'httpindex', 'regex' : r'harfbuzz-(?P<version_num>[\d.]+)\.tar\.xz' },
-	#'_info' : { 'version' : '2.7.1', 'fancy_name' : 'harfbuzz' },
-	'_info' : { 'version' : '2.7.2', 'fancy_name' : 'harfbuzz' },
+	#'_info' : { 'version' : '2.7.2', 'fancy_name' : 'harfbuzz' },
+	'_info' : { 'version' : '2.7.4', 'fancy_name' : 'harfbuzz' },
 }
 
--- packages/dependencies/libilbc.py
+++ packages/dependencies/libilbc.py
@@ -4,9 +4,8 @@
 	#'branch' : 'main',  # 2020.12.19 they've changed the trunk from master to main (a US political race thing against the word, apparently)
 	'repo_type' : 'archive',
 	'download_locations' : [
-		#{ 'url' : 'https://github.com/TimothyGu/libilbc/releases/download/v3.0.1/libilbc-3.0.1.tar.gz',	'hashes' : [ { 'type' : 'sha256', 'sum' : '033e5b5d311b5c5e688e23f9877a8cbf4f82a69f8e82cdc758e3c1085f7e91ae' },	], }, # sha256sum filename
-		#{ 'url' : 'https://github.com/TimothyGu/libilbc/releases/download/v3.0.2/libilbc-3.0.2.tar.gz',	'hashes' : [ { 'type' : 'sha256', 'sum' : 'e82cbc41c8c84c0828af869a9c6bbb62e06dece0d17d069c8b9db95082f0a4ce' },	], }, # sha256sum filename
-		{ 'url' : 'https://github.com/TimothyGu/libilbc/releases/download/v3.0.3/libilbc-3.0.3.tar.gz',	'hashes' : [ { 'type' : 'sha256', 'sum' : '47c57deb26a36da11cbf7d51e76894c7b909ab007664f736319267bde57c73d0' },	], }, # sha256sum filename
+		#{ 'url' : 'https://github.com/TimothyGu/libilbc/releases/download/v3.0.3/libilbc-3.0.3.tar.gz',	'hashes' : [ { 'type' : 'sha256', 'sum' : '47c57deb26a36da11cbf7d51e76894c7b909ab007664f736319267bde57c73d0' },	], }, # sha256sum filename
+		{ 'url' : 'https://github.com/TimothyGu/libilbc/releases/download/v3.0.4/libilbc-3.0.4.tar.gz',	'hashes' : [ { 'type' : 'sha256', 'sum' : '6820081a5fc58f86c119890f62cac53f957adb40d580761947a0871cea5e728f' },	], }, # sha256sum filename
 	],	
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
@@ -14,5 +13,6 @@
 	'update_check' : { 'type' : 'git', },
 	'update_check' : { 'url' : 'https://github.com/TimothyGu/libilbc/releases', 'type' : 'githubreleases', 'name_or_tag' : 'name' },
 	#'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libilbc' },
-	'_info' : { 'version' : '3.0.3', 'fancy_name' : 'libilbc' },
+	#'_info' : { 'version' : '3.0.3', 'fancy_name' : 'libilbc' },
+	'_info' : { 'version' : '3.0.4', 'fancy_name' : 'libilbc' },
 }
