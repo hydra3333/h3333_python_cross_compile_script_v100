8b10b6c514263220ead9a7808efe970923dec3d9
hydra3333 <hydra3333@gmail.com>
2021-02-03 22:17:05 +1030

    Add files via upload

--- packages/dependencies/xz.py
+++ packages/dependencies/xz.py
@@ -4,6 +4,7 @@
 	#'url' : 'http://git.tukaani.org/xz.git',
 	'custom_cflag' : '-D_FORTIFY_SOURCE=2', # 2019.12.13 it fails to build with anythinf other than this, eg it crashes with -O3 and -fstack-protector-all
 	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static --disable-xz --disable-xzdec --disable-lzmadec --disable-lzmainfo --disable-doc', # 2019.12.13 --disable-shared --enable-static
+	'depends_on' : [ 'xz', ],
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'xz' },
 }
