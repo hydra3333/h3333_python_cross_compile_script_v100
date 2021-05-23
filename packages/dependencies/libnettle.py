b29a9023ecddbe33499390c2310ee2fbcf1904a6
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-05-23 19:50:08 +0930

    libnettle to 3.7.2

--- packages/dependencies/libnettle.py
+++ packages/dependencies/libnettle.py
@@ -3,8 +3,10 @@
 	'download_locations' : [
 		#{ 'url' : 'https://ftp.gnu.org/gnu/nettle/nettle-3.6.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'd24c0d0f2abffbc8f4f34dcf114b0f131ec3774895f3555922fe2f40f3d5e3f1' }, ], },
 		#{ 'url' : 'https://fossies.org/linux/privat/nettle-3.6.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'd24c0d0f2abffbc8f4f34dcf114b0f131ec3774895f3555922fe2f40f3d5e3f1' }, ], },
-		{ 'url' : 'https://ftp.gnu.org/gnu/nettle/nettle-3.7.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'f001f64eb444bf13dd91bceccbc20acbc60c4311d6e2b20878452eb9a9cec75a' }, ], },
-		{ 'url' : 'https://fossies.org/linux/privat/nettle-3.7.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'f001f64eb444bf13dd91bceccbc20acbc60c4311d6e2b20878452eb9a9cec75a' }, ], },
+		#{ 'url' : 'https://ftp.gnu.org/gnu/nettle/nettle-3.7.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'f001f64eb444bf13dd91bceccbc20acbc60c4311d6e2b20878452eb9a9cec75a' }, ], },
+		#{ 'url' : 'https://fossies.org/linux/privat/nettle-3.7.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'f001f64eb444bf13dd91bceccbc20acbc60c4311d6e2b20878452eb9a9cec75a' }, ], },
+		{ 'url' : 'https://ftp.gnu.org/gnu/nettle/nettle-3.7.2.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '8d2a604ef1cde4cd5fb77e422531ea25ad064679ff0adf956e78b3352e0ef162' }, ], },
+		{ 'url' : 'https://fossies.org/linux/privat/nettle-3.7.2.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '8d2a604ef1cde4cd5fb77e422531ea25ad064679ff0adf956e78b3352e0ef162' }, ], },
 	],
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --disable-openssl --disable-mini-gmp --with-included-libtasn1', # 2019.12.13
 	'depends_on' : [
