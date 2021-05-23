60dcff52142b8902227bd581c36c2e36c63f519e
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-05-23 20:08:11 +0930

    sqlite3 to 3350500

--- packages/dependencies/sqlite3.py
+++ packages/dependencies/sqlite3.py
@@ -3,8 +3,10 @@
 	'download_locations' : [ # https://fossies.org/linux/misc/
 		#{ 'url' : 'https://www.sqlite.org/2020/sqlite-autoconf-3340100.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '2a3bca581117b3b88e5361d0ef3803ba6d8da604b1c1a47d902ef785c1b53e89' }, ], },
 		#{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3340100.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '2a3bca581117b3b88e5361d0ef3803ba6d8da604b1c1a47d902ef785c1b53e89' }, ], },
-		{ 'url' : 'https://www.sqlite.org/2021/sqlite-autoconf-3350400.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '7771525dff0185bfe9638ccce23faa0e1451757ddbda5a6c853bb80b923a512d' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3350400.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '7771525dff0185bfe9638ccce23faa0e1451757ddbda5a6c853bb80b923a512d' }, ], },
+		#{ 'url' : 'https://www.sqlite.org/2021/sqlite-autoconf-3350400.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '7771525dff0185bfe9638ccce23faa0e1451757ddbda5a6c853bb80b923a512d' }, ], },
+		#{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3350400.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '7771525dff0185bfe9638ccce23faa0e1451757ddbda5a6c853bb80b923a512d' }, ], },
+		{ 'url' : 'https://www.sqlite.org/2021/sqlite-autoconf-3350500.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'f52b72a5c319c3e516ed7a92e123139a6e87af08a2dc43d7757724f6132e6db0' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3350500.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'f52b72a5c319c3e516ed7a92e123139a6e87af08a2dc43d7757724f6132e6db0' }, ], },
 	],
 	'cflag_addition' : '-fexceptions -DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_USE_MALLOC_H=1 -DSQLITE_USE_MSIZE=1 -DSQLITE_DISABLE_DIRSYNC=1 -DSQLITE_ENABLE_RTREE=1 -fno-strict-aliasing',
 	'strip_cflags': ['-ffast-math', ],
