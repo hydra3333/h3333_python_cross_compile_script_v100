22983d6b3d83f6fcc01c6cf84612bd61cbfc187b
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-06-19 16:55:16 +0930

    try build mkvtoolnix

--- h3333_v100.001.sh
+++ h3333_v100.001.sh
@@ -240,15 +240,15 @@ if [ $exit_status -ne 0 ]; then
 fi
 #read -p "done libaacs_dll press any key to continue"
 
-#rm -fv ./mkvtoolnix.log
-#./cross_compiler_v100_001.py --force --debug -p mkvtoolnix 2>&1 | tee -a ./mkvtoolnix.log
-#exit_status=$?
-#echo "exit_status='$exit_status'"
-#if [ $exit_status -ne 0 ]; then
-#    echo "Error $exit_status detected"
-#	exit $exit_status
-#fi
-#read -p "done mkvtoolnix press any key to continue"
+rm -fv ./mkvtoolnix.log
+./cross_compiler_v100_001.py --force --debug -p mkvtoolnix 2>&1 | tee -a ./mkvtoolnix.log
+exit_status=$?
+echo "exit_status='$exit_status'"
+if [ $exit_status -ne 0 ]; then
+    echo "Error $exit_status detected"
+	exit $exit_status
+fi
+read -p "done mkvtoolnix press any key to continue"
 
 
 #rm -fv ./youtube-dl.log
