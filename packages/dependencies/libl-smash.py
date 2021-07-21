1f46e1964296031694701ecef69a5a1cc513dbaf
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-07-21 18:46:55 +0930

    revert holywu's lsw stuff for x264 since it no longer works nor is supported

--- packages/dependencies/libl-smash.py
+++ packages/dependencies/libl-smash.py
@@ -1,6 +1,6 @@
 { # used by liblsw and in turn product x264
 		'repo_type' : 'git',
-		'url' : 'https://github.com/hydra3333/l-smash', # 2019.12.13 mine is patched #'https://github.com/l-smash/l-smash.git',
+		'url' : 'https://github.com/hydra3333/l-smash', # 2019.12.13 ?? is mine more patched than than 'https://github.com/l-smash/l-smash.git',
 		'env_exports' : {
 			'PKGCONFIG' : 'pkg-config',
 		},
@@ -13,3 +13,21 @@
 		'update_check' : { 'type' : 'git', },
 		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libl-smash' },
 }
+#
+#{ # used by liblsw and in turn product x264
+#		'repo_type' : 'git',
+#		'url' : 'https://github.com/hydra3333/l-smash', # 2019.12.13 mine is patched #'https://github.com/l-smash/l-smash.git',
+#		'env_exports' : {
+#			'PKGCONFIG' : 'pkg-config',
+#		},
+#		'configure_options':
+#			'--prefix={target_prefix} '
+#			'--cross-prefix={cross_prefix_bare} '
+#			'--extra-libs="-lssp" ' 
+#		,
+#		'build_options': 'install-lib',
+#		'update_check' : { 'type' : 'git', },
+#		'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libl-smash' },
+#}
+#
+
