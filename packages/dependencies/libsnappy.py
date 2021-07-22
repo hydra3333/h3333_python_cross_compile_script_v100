e80bd284ae9b8d53642fd21c5076b52530a1acf7
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-07-22 12:11:45 +0930

    Add files via upload

--- packages/dependencies/libsnappy.py
+++ packages/dependencies/libsnappy.py
@@ -4,7 +4,7 @@
 	'depth_git' : 0,
 	#'branch' : '0793e2ae2d51640ae569b75ffb42fc444afafb32', # 2020.12.19 it broke after this
 	'conf_system' : 'cmake',
-	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DBUILD_BINARY=OFF -DSNAPPY_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release',
+	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DBUILD_BINARY=OFF -DSNAPPY_BUILD_BENCHMARKS=OFF -DSNAPPY_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release',
 	'run_post_install' : [
 		'rm -vf {target_prefix}/lib/libsnappy.dll.a',
 	],
