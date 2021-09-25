aca6049822e313bdd42a527d687937252ee629f9
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-09-25 19:37:56 +0930

    Add files via upload

--- packages/dependencies/ffmpeg_depends.py
+++ packages/dependencies/ffmpeg_depends.py
@@ -4,8 +4,8 @@
 		'bzip2',
 		'zlib',
 		'lzma',
-		'fontconfig', # fontconfig builds 'freetype'
-		#'freetype', # it depends on 'freetype_lib', 'harfbuzz_lib-with-freetype' so it builds these in the right order
+		'freetype', # it depends on 'freetype_lib', 'harfbuzz_lib-with-freetype' so it builds these in the right order
+		'fontconfig', # fontconfig builds 'freetype' (again ?)
 		'libbluray',
 		'libcdio',
         'python3_libs',
