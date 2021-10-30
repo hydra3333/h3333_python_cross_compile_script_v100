4af756de5f444b916693ed627c905d9f0dd35ce2
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-10-31 09:56:20 +1030

    Add files via upload

--- packages/dependencies/libx264.py
+++ packages/dependencies/libx264.py
@@ -7,6 +7,7 @@
 							'--enable-strip --enable-lto --enable-pic '
 							'--bit-depth=all '
 							'--chroma-format=all ' # 2020.06.09
+											   
 							'--disable-win32thread ' # make posix threeads like the other things being built
 							'--extra-cflags="-DLIBXML_STATIC" --extra-cflags="-DGLIB_STATIC_COMPILATION" '
 							'--disable-cli --disable-lavf ' # 2020.06.09 re-added --disable-lavf to libx264 building only
@@ -15,25 +16,28 @@
 		#'opencl_icd', # 2020.11.24
 		'opencl_non_icd', # 2020.11.24
 		# 2020.06.09 only the x264 package, not this dependency depends on libffmpeg_extra being built first, so comment this out or it fails to build
-		# 'libffmpeg_extra', 'liblsw',  # 2019.12.13 HolyWu's lsw does not need avresample as it uses libswresample # 2018.11.23 superseded: liblsw required --enable-avresample which was deprecated
+		# 'libffmpeg_extra', 'liblsw',	# 2019.12.13 HolyWu's lsw does not need avresample as it uses libswresample # 2018.11.23 superseded: liblsw required --enable-avresample which was deprecated
 	],
 	'env_exports' : { 
 		'PKGCONFIG' : 'pkg-config',
 	# 2020.06.09 only the x264 package, not this dependency depends on libffmpeg_extra being built first, so comment this out or it fails to build
 	#	'SWSCALE_LIBS' : '!CMD(pkg-config --libs libswscale)CMD!',
-	#	'LAVF_LIBS'    : '!CMD(pkg-config --libs libavformat libavcodec libavutil libswscale)CMD!',  # 2020.06.09
+	#	'LAVF_LIBS'	   : '!CMD(pkg-config --libs libavformat libavcodec libavutil libswscale)CMD!',	 # 2020.06.09
 	#	'LAVF_CFLAGS'  : '!CMD(pkg-config --cflags libavformat libavcodec libavutil libswscale)CMD!',  # 2020.06.09
 	},
 	#'run_post_patch' : [ # 2020.06.09 see what should be returned for env_exports
 	#	'pkg-config --libs libswscale',
 	#	'pkg-config --libs libavformat libavcodec libavutil libswscale',
-	#	'pkg-config --cflags libavformat libavcodec libavutil libswscale',  # 2020.06.09
+	#	'pkg-config --cflags libavformat libavcodec libavutil libswscale',	# 2020.06.09
 	#],
 	#'run_post_configure' : [ # 2020.06.09 see if the EXPORT stuff worked
 	#	'export | grep SWSCALE_LIBS',
 	#	'export | grep LAVF_LIBS',
 	#	'export | grep LAVF_CFLAGS',
 	#],
+	'depends_on' : [
+	#	 'libgpac', # libgpac depends on ffmpeg_extra
+	],
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'x264 (library) multibit' },
 }
--- packages/dependencies/libx264_3333.py
+++ packages/dependencies/libx264_3333.py
@@ -15,25 +15,28 @@
 	#	#'opencl_icd', # 2020.11.24
 	#	'opencl_non_icd', # 2021.02.03 removed for 3333
 	#	# 2020.06.09 only the x264 package, not this dependency depends on libffmpeg_extra being built first, so comment this out or it fails to build
-	#	# 'libffmpeg_extra', 'liblsw',  # 2019.12.13 HolyWu's lsw does not need avresample as it uses libswresample # 2018.11.23 superseded: liblsw required --enable-avresample which was deprecated
+	#	# 'libffmpeg_extra', 'liblsw',	# 2019.12.13 HolyWu's lsw does not need avresample as it uses libswresample # 2018.11.23 superseded: liblsw required --enable-avresample which was deprecated
 	#],
 	'env_exports' : { 
 		'PKGCONFIG' : 'pkg-config',
 	# 2020.06.09 only the x264 package, not this dependency depends on libffmpeg_extra being built first, so comment this out or it fails to build
 	#	'SWSCALE_LIBS' : '!CMD(pkg-config --libs libswscale)CMD!',
-	#	'LAVF_LIBS'    : '!CMD(pkg-config --libs libavformat libavcodec libavutil libswscale)CMD!',  # 2020.06.09
+	#	'LAVF_LIBS'	   : '!CMD(pkg-config --libs libavformat libavcodec libavutil libswscale)CMD!',	 # 2020.06.09
 	#	'LAVF_CFLAGS'  : '!CMD(pkg-config --cflags libavformat libavcodec libavutil libswscale)CMD!',  # 2020.06.09
 	},
 	#'run_post_patch' : [ # 2020.06.09 see what should be returned for env_exports
 	#	'pkg-config --libs libswscale',
 	#	'pkg-config --libs libavformat libavcodec libavutil libswscale',
-	#	'pkg-config --cflags libavformat libavcodec libavutil libswscale',  # 2020.06.09
+	#	'pkg-config --cflags libavformat libavcodec libavutil libswscale',	# 2020.06.09
 	#],
 	#'run_post_configure' : [ # 2020.06.09 see if the EXPORT stuff worked
 	#	'export | grep SWSCALE_LIBS',
 	#	'export | grep LAVF_LIBS',
 	#	'export | grep LAVF_CFLAGS',
 	#],
+	'depends_on' : [
+	#	 'libgpac', # libgpac depends on ffmpeg_extra
+	],
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'x264_3333 (library) multibit' },
 }
