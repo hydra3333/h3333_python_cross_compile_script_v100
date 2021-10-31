70487e56b594418938a1c86f199597b1b054eb1a
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-10-31 12:13:41 +1030

    Add files via upload

--- packages/dependencies/libcurl.py
+++ packages/dependencies/libcurl.py
@@ -4,6 +4,9 @@
 	'depth_git' : 0,
 	#'branch' : '3735107d62ad28d6e25bd94109ab9f2454c22116', # https://github.com/curl/curl/issues/5219#ref-pullrequest-599156369 # works: '3735107d62ad28d6e25bd94109ab9f2454c22116', # fails: '1fc0617dccc3fa138235f219e2eaa7b405d1162e' under gcc10,
 	'rename_folder' : 'curl_git',
+	'run_post_regexreplace' : [
+		'autoreconf -fiv',
+	],
 	'configure_options': '--prefix={target_prefix} --exec-prefix={target_prefix} --enable-static --disable-shared --target={bit_name2}-{bit_name_win}-gcc --host={target_host} --build=x86_64-linux-gnu '
 		'--with-libssh2 --with-gnutls --with-ca-fallback --without-winssl --enable-pthreads --enable-symbol-hiding ',
 	'depends_on': (
