cf2becf3280ab46d3b0676f7e467c9297eb41b3e
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-10-31 12:29:10 +1030

    Add files via upload

--- packages/dependencies/libmediainfo.py
+++ packages/dependencies/libmediainfo.py
@@ -6,8 +6,8 @@
 	'configure_options' : '--host={target_host} --prefix={target_prefix} --enable-shared --enable-static --with-libcurl --with-libmms --with-libmediainfo-name=MediaInfo.dll', # --enable-static --disable-shared --enable-shared=no
 	'custom_cflag' : '-D__USE_MINGW_ANSI_STDIO=1 {original_cflags}', # 2019.12.13
 	'run_post_regexreplace' : [
-		'sed -i.bak \'s/Windows.h/windows.h/\' ../../../Source/MediaInfo/Reader/Reader_File.h',
-		'sed -i.bak \'s/Windows.h/windows.h/\' ../../../Source/MediaInfo/Reader/Reader_File.cpp',
+		'sed -i.bak \'s/Windows.h/windows.h/g\' ../../../Source/MediaInfo/Reader/Reader_File.h',
+		'sed -i.bak \'s/Windows.h/windows.h/g\' ../../../Source/MediaInfo/Reader/Reader_File.cpp',
         './autogen.sh NOCONFIGURE=1',
         'autoreconf -fiv',
 	],
