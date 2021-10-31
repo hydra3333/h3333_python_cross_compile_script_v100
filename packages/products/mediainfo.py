7f2648c6601a4a160b70ef4027d9e7e16a20fd2f
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-10-31 12:07:39 +1030

    Add files via upload

--- packages/products/mediainfo.py
+++ packages/products/mediainfo.py
@@ -12,7 +12,7 @@
 	'configure_options': '--host={target_host} --prefix={output_prefix}/mediainfo_git.installed --disable-shared --disable-static-libs',
     'custom_cflag' : '-D__USE_MINGW_ANSI_STDIO=1 {original_cflags}', # 2019.12.13
 	'depends_on': [
-		'zenlib', 'libmediainfo', # 2019.12.13
+		'zenlib', 'libmediainfo', 'libcurl', # 2019.12.13
 	],
 	'run_post_configure' : [
 		'sed -i.bak \'s/ -DSIZE_T_IS_LONG//g\' Makefile',
