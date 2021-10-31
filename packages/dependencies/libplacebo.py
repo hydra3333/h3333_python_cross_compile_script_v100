956275421142b2711f4b5b9bbfbfe589858d4cd9
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-10-31 16:46:28 +1030

    Add files via upload

--- packages/dependencies/libplacebo.py
+++ packages/dependencies/libplacebo.py
@@ -6,9 +6,9 @@
 	'conf_system' : 'meson',
 	'build_system' : 'ninja',
 	'source_subfolder' : 'build',
-	'run_post_regexreplace' : [
-		'cp -nv "/usr/bin/pkg-config" "{cross_prefix_full}pkg-config"', # gotta fix this properly at some point.
-	],
+	#'run_post_regexreplace' : [
+	#	'cp -fuv "/usr/bin/pkg-config" "{cross_prefix_full}pkg-config"', # gotta fix this properly at some point.
+	#],
 	'configure_options' :
 		'--prefix={target_prefix} '
 		'--libdir={target_prefix}/lib '
@@ -16,19 +16,17 @@
 		'--buildtype=plain '
 		'--backend=ninja '
 		'--buildtype=release '
-		#'-Dvulkan=enabled ' # 2020.10.12 comment out vulkan since it an no longer be statically linked
 		'-Dvulkan=enabled ' # 2021.10.30 re-try vulkan
-		#'-Dvulkan-registry={target_prefix}/share/vulkan/registry/vk.xml ' # 2020.10.12 comment out vulkan since it an no longer be statically linked
 		'-Dvulkan-registry={target_prefix}/share/vulkan/registry/vk.xml '  # 2021.10.30 re-try vulkan
 		'-Dglslang=enabled ' # 2021.10.30 add back ? since the dependency is built
 		'-Dshaderc=enabled ' # 2021.10.30 add back ? since the dependency is built
 		'-Dlcms=enabled '
 		'-Dtests=false '
 		'-Dbench=false '
-        '-Ddemos=false ' # 2021.04.09 try this from MABS
+		'-Ddemos=false ' # 2021.04.09 try this from MABS
 		'--cross-file={meson_env_file} ./ ..'
 	,
-	#'depends_on' : [ 'lcms2', 'spirv-tools', 'glslang', 'shaderc', ], # 2021.10.30 add spirv-tools # 'vulkan_loader',  2020.10.12 comment out vulkan since it an no longer be statically linked
+	#'depends_on' : [ 'lcms2', 'spirv-tools', 'glslang', 'shaderc', ], # 2021.10.30 add spirv-tools # 'vulkan_loader',	2020.10.12 comment out vulkan since it an no longer be statically linked
 	'depends_on' : [ 'lcms2', 'spirv-tools', 'glslang', 'shaderc', 'vulkan_loader' ], # 2021.10.30 re-try 'vulkan_loader'
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'libplacebo' },
