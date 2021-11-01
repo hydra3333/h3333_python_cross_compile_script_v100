7d4ab77f0966c81485f07dab1482257fe80a29eb
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-11-01 20:22:13 +1030

    Add files via upload

--- packages/dependencies/libcurl.py
+++ packages/dependencies/libcurl.py
@@ -1,14 +1,14 @@
 {
-	#'repo_type' : 'git',
-	#'url' : 'https://github.com/curl/curl',
-	#'depth_git' : 0,
+	'repo_type' : 'git',
+	'url' : 'https://github.com/curl/curl',
+	'depth_git' : 0,
 	##'branch' : '8e701cc978430d638517d025fade2b0dc1ae9b73',
-	#'rename_folder' : 'curl_git',
-    'repo_type' : 'archive',
-	'download_locations' : [
-		{ 'url' : 'https://github.com/curl/curl/releases/download/curl-7_79_1/curl-7.79.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '0606f74b1182ab732a17c11613cbbaf7084f2e6cca432642d0e3ad7c224c3689' }, ], }, # https://github.com/curl/curl/releases
-		{ 'url' : 'https://fossies.org/linux/www/curl-7.79.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '0606f74b1182ab732a17c11613cbbaf7084f2e6cca432642d0e3ad7c224c3689' }, ], }, # https://fossies.org/linux/misc/
-    ],
+	'rename_folder' : 'curl_git',
+    #'repo_type' : 'archive',
+	#'download_locations' : [
+	#	{ 'url' : 'https://github.com/curl/curl/releases/download/curl-7_79_1/curl-7.79.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '0606f74b1182ab732a17c11613cbbaf7084f2e6cca432642d0e3ad7c224c3689' }, ], }, # https://github.com/curl/curl/releases
+	#	{ 'url' : 'https://fossies.org/linux/www/curl-7.79.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '0606f74b1182ab732a17c11613cbbaf7084f2e6cca432642d0e3ad7c224c3689' }, ], }, # https://fossies.org/linux/misc/
+    #],
 	'run_post_regexreplace' : [
 		'autoreconf -fiv',
 	],
