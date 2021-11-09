5cd8bfbd0198a356a17be223c601df82fd080bde
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-11-09 20:36:59 +1030

    fix libgpg_error git site unreachable

--- packages/dependencies/libgpg_error.py
+++ packages/dependencies/libgpg_error.py
@@ -1,9 +1,13 @@
 { # 2019.12.13 # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgpg-error.git;a=summary
-		#'repo_type' : 'archive',
+		'repo_type' : 'archive',
 		#'url' : 'https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.32.tar.bz2',
-		'repo_type' : 'git',
-		'recursive_git' : True,
-		'url' : 'git://git.gnupg.org/libgpg-error.git', # https://git.gnupg.org/ # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgpg-error.git;a=summary
+        'url' : 'https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.43.tar.bz2',
+		#
+        #'repo_type' : 'git',
+		#'recursive_git' : True,
+		#'url' : 'git://git.gnupg.org/libgpg-error.git', # https://git.gnupg.org/ # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgpg-error.git;a=summary
+		#'url' : 'https://dev.gnupg.org/source/libgpg-error.git', # https://git.gnupg.org/ # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgpg-error.git;a=summary
+        #
 		'configure_options': '--host={target_host} --prefix={target_prefix} --disable-shared --enable-static --disable-rpath --disable-doc --disable-tests --with-libiconv-prefix={target_prefix}', # --with-libintl=no --with-libpth=no',
 		'custom_cflag' : ' ', # 2019.12.13 it fails to build with anything other than this, eg it crashes with -O3 and -fstack-protector-all -D_FORTIFY_SOURCE=2 
 		'run_post_regexreplace' : (
