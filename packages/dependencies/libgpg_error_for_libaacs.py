a2f40b6b6158cbdd67782fb9539b06bce0d1fe39
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-11-17 20:45:35 +1030

    Add files via upload

--- packages/dependencies/libgpg_error_for_libaacs.py
+++ packages/dependencies/libgpg_error_for_libaacs.py
@@ -1,12 +1,12 @@
 { # 2019.12.13 # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgpg-error.git;a=summary
   # see https://forum.videolan.org/viewtopic.php?f=32&t=127218&p=432818&hilit=compile+libaacs#p432818
-	#'repo_type' : 'archive',
-	#'url' : 'https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.43.tar.bz2',
+	'repo_type' : 'archive',
+	'url' : 'https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.43.tar.bz2',
 	#
-	'repo_type' : 'git',
-	'recursive_git' : True,
-	'url' : 'git://git.gnupg.org/libgpg-error.git', # https://git.gnupg.org/ # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgpg-error.git;a=summary
-	#'url' : 'https://dev.gnupg.org/source/libgpg-error.git', # https://git.gnupg.org/ # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgpg-error.git;a=summary
+	#'repo_type' : 'git',
+	#'recursive_git' : True,
+	#'url' : 'git://git.gnupg.org/libgpg-error.git', # https://git.gnupg.org/ # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgpg-error.git;a=summary
+	##'url' : 'https://dev.gnupg.org/source/libgpg-error.git', # https://git.gnupg.org/ # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgpg-error.git;a=summary
 	#
 	'rename_folder' : 'libgpg-error_for_aacs_git',
 	'configure_options': '--host={target_host} --prefix={output_prefix}/libaacs_dll_git.installed --disable-rpath --disable-doc --disable-tests ', #--with-libiconv-prefix={target_prefix}', # --with-libintl=no --with-libpth=no',
