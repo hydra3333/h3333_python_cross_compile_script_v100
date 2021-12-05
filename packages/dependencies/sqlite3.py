68f111703e8659d1114abd6a853e1eac57151a4f
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-12-05 13:50:01 +1030

    sqlite3 to 3.37.0

--- packages/dependencies/sqlite3.py
+++ packages/dependencies/sqlite3.py
@@ -1,8 +1,10 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [ # https://fossies.org/linux/misc/
-		{ 'url' : 'https://www.sqlite.org/2021/sqlite-autoconf-3360000.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'bd90c3eb96bee996206b83be7065c9ce19aef38c3f4fb53073ada0d0b69bbce3' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3360000.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'bd90c3eb96bee996206b83be7065c9ce19aef38c3f4fb53073ada0d0b69bbce3' }, ], },
+		#{ 'url' : 'https://www.sqlite.org/2021/sqlite-autoconf-3360000.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'bd90c3eb96bee996206b83be7065c9ce19aef38c3f4fb53073ada0d0b69bbce3' }, ], },
+		#{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3360000.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'bd90c3eb96bee996206b83be7065c9ce19aef38c3f4fb53073ada0d0b69bbce3' }, ], },
+		{ 'url' : 'https://www.sqlite.org/2021/sqlite-autoconf-3370000.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '731a4651d4d4b36fc7d21db586b2de4dd00af31fd54fb5a9a4b7f492057479f7' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3370000.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '731a4651d4d4b36fc7d21db586b2de4dd00af31fd54fb5a9a4b7f492057479f7' }, ], },
 	],
 	'cflag_addition' : '-fexceptions -DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_USE_MALLOC_H=1 -DSQLITE_USE_MSIZE=1 -DSQLITE_DISABLE_DIRSYNC=1 -DSQLITE_ENABLE_RTREE=1 -fno-strict-aliasing',
 	'strip_cflags': ['-ffast-math', ],
@@ -11,5 +13,5 @@
 		'zlib',
 	),
 	'update_check' : { 'url' : 'https://www.sqlite.org/index.html', 'type' : 'httpregex', 'regex' : r'<a href="releaselog/.*\.html">Version (?P<version_num>[\d.]+)<\/a>' },
-	'_info' : { 'version' : '3.36.0', 'fancy_name' : 'libsqlite3' },
+	'_info' : { 'version' : '3.37.0', 'fancy_name' : 'libsqlite3' },
 }
