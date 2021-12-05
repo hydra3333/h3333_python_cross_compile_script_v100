1ea62b43dd612833ea8845b1f6481d2b5f48f5e3
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2021-12-05 13:47:55 +1030

    up sdl2 ver

--- packages/dependencies/sdl2.py
+++ packages/dependencies/sdl2.py
@@ -54,5 +54,5 @@
 	#	'cp -fv "{target_prefix}/bin/sdl2-config" "{cross_prefix_full}sdl2-config"', # this is the only mingw dir in the PATH so use it for now [though FFmpeg doesn't use it?]
 	#),
 	'update_check' : { 'url' : 'https://www.libsdl.org/release/', 'type' : 'httpindex', 'regex' : r'SDL2-(?P<version_num>[\d.]+)\.tar\.gz' },
-	'_info' : { 'version' : '2.0.16', 'fancy_name' : 'SDL2' },
+	'_info' : { 'version' : '2.0.18', 'fancy_name' : 'SDL2' },
 }
