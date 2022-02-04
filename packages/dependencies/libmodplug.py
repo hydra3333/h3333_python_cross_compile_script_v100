5bf01307361ee112122339db40aa924177a99d37
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-02-04 17:05:36 +1030

    make libmodplug a bit like rdp's

--- packages/dependencies/libmodplug.py
+++ packages/dependencies/libmodplug.py
@@ -7,14 +7,22 @@
 	#	{ "url" : "https://sourceforge.net/projects/modplug-xmms/files/libmodplug/0.8.9.0/libmodplug-0.8.9.0.tar.gz", "hashes" : [ { "type" : "sha256", "sum" : "457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de" }, ], },
 	#],
 	'conf_system' : 'cmake',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix}  -DMODPLUG_STATIC=1 -DBUILD_SHARED_LIBS=0',  # 2019.12.13
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DMODPLUG_STATIC=1 -DBUILD_SHARED_LIBS=0',  # 2019.12.13
 	'source_subfolder': '_build',
-	'patches': [
-		('modplug/0001-modplug-mingw-workaround.patch', '-p1', '..'), # to avoid setting -DLIBMODPLUG_STATIC
-	],
+	#
+	# 2022.02.04 change to be similar to RDP's
+	#'patches': [
+	#	('modplug/0001-modplug-mingw-workaround.patch', '-p1', '..'), # to avoid setting -DLIBMODPLUG_STATIC
+	#],
     #'run_post_install': ( # 2019.12.13 perhaps not needed in latest git ?
 	#	'sed -i.bak \'s/-lmodplug.*/-lmodplug -lstdc++/\' "{pkg_config_path}/libmodplug.pc"', # huh ?? c++?  # 2019.12.13
 	#),
+    'run_post_install': ( # 2022.02.04 perhaps not needed in latest git ? Do it similar to RDP anyway
+		'sed -i.bak \'s/__declspec(dllexport)//\' "{target_prefix}/include/libmodplug/modplug.h"', #strip DLL import/export directives
+		'sed -i.bak \'s/__declspec(dllimport)//\' "{target_prefix}/include/libmodplug/modplug.h"', #strip DLL import/export directives
+		'sed -i.bak \'s/__declspec(dllexport)//\' "{target_prefix}/include/libmodplug/stdafx.h"', #strip DLL import/export directives
+		'sed -i.bak \'s/__declspec(dllimport)//\' "{target_prefix}/include/libmodplug/stdafx.h"', #strip DLL import/export directives
+	),
 	'regex_replace': {
 		'post_patch': [
 			{
