c4260255e2e9351028ba69f649e3ab49f633ac9c
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-02-16 14:22:06 +1030

    affix xz to a working commit

--- packages/dependencies/xz.py
+++ packages/dependencies/xz.py
@@ -2,7 +2,7 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/xz-mirror/xz.git',
 	'depth_git' : 0,
-	'branch' : 'f7711d228c3c32395460c82498c60a9f730d0239', # '6468f7e41a8e9c611e4ba8d34e2175c5dacdbeb4',
+	'branch' : '6468f7e41a8e9c611e4ba8d34e2175c5dacdbeb4',
 	#'url' : 'http://git.tukaani.org/xz.git',
 	'custom_cflag' : '-D_FORTIFY_SOURCE=2', # 2019.12.13 it fails to build with anythinf other than this, eg it crashes with -O3 and -fstack-protector-all
 	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static --disable-xz --disable-xzdec --disable-lzmadec --disable-lzmainfo --disable-doc', # 2019.12.13 --disable-shared --enable-static
