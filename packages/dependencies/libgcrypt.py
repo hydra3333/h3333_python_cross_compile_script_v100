94ad9d78f40a091751f244c409c8bccafa3d8675
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-02-16 15:18:51 +1030

    Add files via upload

--- packages/dependencies/libgcrypt.py
+++ packages/dependencies/libgcrypt.py
@@ -4,13 +4,13 @@
 	#'url' : 'https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.8.3.tar.bz2',
 	'repo_type' : 'git',
 	'recursive_git' : True,
-	'depth_git' : 0, # 2019.12.13 otherwise too shallow for the specfified commit to fetch
+	'depth_git' : 0,
 	'url' : 'git://git.gnupg.org/libgcrypt.git',
 	##'branch' : 'tags/libgcrypt-1.9.2',  # 1.9.3 onward fails 2021.09.18 on commit d2b3d046fc66a3166dc0c003a430ce756532ff74
-	#'branch' : '5e0187d84fc16d9ff0fbb0ccd4348657fea90d36',	 # works: '5e0187d84fc16d9ff0fbb0ccd4348657fea90d36' breaks on and after: '3bacdac611b9eb3bd5ae8d78156b1110e77e9518'
-											
+	#'branch' : '',
 	'patches' : [
-		('libgcrypt/libgcrypt.patch', '-p1'),	# 2021.09.18 undo commit d2b3d046fc66a3166dc0c003a430ce756532ff74
+		#('libgcrypt/libgcrypt.patch', '-p1'),	# 2021.09.18 undo commit d2b3d046fc66a3166dc0c003a430ce756532ff74
+		('libgcrypt-2022.02.16.patch', '-p1'),	# 2022.02.16
 	],
 	'custom_cflag' : ' {original_fortify_source_trim} ', # 2019.12.13 this does not like -O3 -fstack-protector-all ... use one or more of {original_cflag_trim} {original_stack_protector_trim} {original_fortify_source_trim}
 	'run_post_regexreplace' : (
