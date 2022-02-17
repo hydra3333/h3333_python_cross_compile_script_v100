9f30ee8c9eae0098abd5d43f98981531893fd9b6
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-02-17 13:01:01 +1030

    fix for aacs

--- packages/dependencies/libgcrypt_for_libaacs.py
+++ packages/dependencies/libgcrypt_for_libaacs.py
@@ -1,14 +1,13 @@
 { # 2019.12.13 # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgcrypt.git;a=summary # This DAMN thing fails to build with "versioninfo.rc.in:21: syntax error" if not built directly from a GIT clone
-																									 
+  # see https://forum.videolan.org/viewtopic.php?f=32&t=127218&p=432818&hilit=compile+libaacs#p432818
 	#'repo_type' : 'archive',
 	#'url' : 'https://www.gnupg.org/ftp/gcrypt/libgcrypt/libgcrypt-1.8.3.tar.bz2',
 	'repo_type' : 'git',
 	'recursive_git' : True,
-	'depth_git' : 0,
+	'depth_git' : 0, # 2019.12.13 otherwise too shallow for the specfified commit to fetch
 	'url' : 'git://git.gnupg.org/libgcrypt.git',
 	##'branch' : 'tags/libgcrypt-1.9.2',  # 1.9.3 onward fails 2021.09.18 on commit d2b3d046fc66a3166dc0c003a430ce756532ff74
-	#'branch' : '5e0187d84fc16d9ff0fbb0ccd4348657fea90d36',	 # works: '5e0187d84fc16d9ff0fbb0ccd4348657fea90d36' breaks on and after: '3bacdac611b9eb3bd5ae8d78156b1110e77e9518'
-											
+	#'branch' : '',	'rename_folder' : 'libgcrypt_for_aacs_git',
 	'patches' : [
 		#('libgcrypt/libgcrypt.patch', '-p1'),	# 2021.09.18 undo commit d2b3d046fc66a3166dc0c003a430ce756532ff74
 		('libgcrypt/libgcrypt-2022.02.16.patch', '-p1'),	# 2022.02.16
@@ -16,12 +15,12 @@
 	'custom_cflag' : ' {original_fortify_source_trim} ', # 2019.12.13 this does not like -O3 -fstack-protector-all ... use one or more of {original_cflag_trim} {original_stack_protector_trim} {original_fortify_source_trim}
 	'run_post_regexreplace' : (
 		'autoreconf -fiv', # https://dev.gnupg.org/T5696
-		#'./autogen.sh --force --build-w64 --prefix={target_prefix}',	  # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgcrypt.git;a=blob_plain;f=README.GIT;hb=HEAD https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgcrypt.git;a=blob_plain;f=README;hb=HEAD
+		#'./autogen.sh --force --build-w64 --prefix={output_prefix}/libaacs_dll_git.installed',     # https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgcrypt.git;a=blob_plain;f=README.GIT;hb=HEAD https://git.gnupg.org/cgi-bin/gitweb.cgi?p=libgcrypt.git;a=blob_plain;f=README;hb=HEAD
 	),
-	'configure_options': '--host={target_host} --prefix={target_prefix} --with-gpg-error-prefix={target_prefix} --disable-shared --enable-static --disable-doc --enable-threads=windows ', # --disable-asm 
+	'configure_options': '--host={target_host} --prefix={output_prefix}/libaacs_dll_git.installed --with-gpg-error-prefix={output_prefix}/libaacs_dll_git.installed --disable-doc --enable-threads=windows ', # --disable-asm 
 	'depends_on' : (
-		'libgpg_error', 
+		'libgpg_error_for_libaacs', 
 	),
 	'update_check' : { 'type' : 'git', },
-	'_info' : { 'version' : 'git', 'fancy_name' : 'libgcrypt for libaacs' },
+	'_info' : { 'version' : 'git', 'fancy_name' : 'libgcrypt for libaacs_dll' },
 }
