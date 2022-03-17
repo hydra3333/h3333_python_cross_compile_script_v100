9a125f06d5de30d62f8ffcdfe062058f40fa22fb
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-17 14:24:25 +1030

    Add files via upload

--- packages/dependencies/libogg.py
+++ packages/dependencies/libogg.py
@@ -3,13 +3,13 @@
 	'url' : 'https://github.com/xiph/ogg.git',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release -DINSTALL_DOCS=OFF -DHAVE_SQLITE3=ON',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release -DINSTALL_DOCS=OFF',
 	'run_post_regexreplace' : [
 		'cd .. ; pwd ; sh ./autogen.sh ; cd _build ; pwd',
 	],
-	'depends_on' : [
-		'sqlite3',
-	],
+	#'depends_on' : [
+	#	'sqlite3',
+	#],
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'ogg' },
 }
