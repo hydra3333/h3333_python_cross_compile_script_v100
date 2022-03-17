bd8fb238d0698373d0f66b61c9c9cd64998fbb4b
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-17 16:52:53 +1030

    convert over to cmake

--- packages/dependencies/libxml2.py
+++ packages/dependencies/libxml2.py
@@ -8,10 +8,57 @@
 	],
 	'folder_name' : 'libxml2-2.9.12',
 	#'rename_folder' : 'libxml2-2.9.12',
-	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static --without-python --enable-tests=no --enable-programs=no',
-	'run_post_regexreplace' : [
-		'autoreconf -fiv',
-	],
+	
+	'conf_system' : 'cmake',
+	'source_subfolder' : '_build',
+	'run_post_patch' : [
+		'pwd ; cd .. ; sh autogen.sh --no-symlink ; cd _build ; pwd',
+	],	
+	'configure_options' :
+		'--prefix={target_prefix} '
+		'--libdir={target_prefix}/lib '
+		'--default-library=static '
+		'--backend=ninja '
+		'--strip '
+		'--buildtype=release '
+		'-DBUILD_SHARED_LIBS=OFF '
+		'-DLIBXML2_WITH_DEBUG=OFF '
+		'-DLIBXML2_WITH_DOCB=OFF '
+		'-DLIBXML2_WITH_FTP=ON '
+		'-DLIBXML2_WITH_HTML=ON '
+		'-DLIBXML2_WITH_HTTP=ON '
+		'-DLIBXML2_WITH_ICONV '
+		'-DLIBXML2_WITH_ISO8859X=ON '
+		'-DLIBXML2_WITH_LZMA=ON '
+		'-DLIBXML2_WITH_LEGACY=ON '
+		'-DLIBXML2_WITH_MODULES=ON '
+		'-DLIBXML2_WITH_OUTPUT=ON '
+		'-DLIBXML2_WITH_PATTERN=ON '
+		'-DLIBXML2_WITH_PROGRAMS=OFF '
+		'-DLIBXML2_WITH_PUSH=ON '
+		'-DLIBXML2_WITH_PYTHON=ON '
+		'-DLIBXML2_WITH_READER=ON '
+		'-DLIBXML2_WITH_REGEXPS=ON '
+		'-DLIBXML2_WITH_SAX1=ON '
+		'-DLIBXML2_WITH_SCHEMATRON=ON '
+		'-DLIBXML2_WITH_SCHEMATRON=ON '
+		'-DLIBXML2_WITH_TESTS=OFF '
+		'-DLIBXML2_WITH_THREADS=ON '
+		'-DLIBXML2_WITH_THREAD_ALLOC=OFF '
+		'-DLIBXML2_WITH_TREE=ON '
+		'-DLIBXML2_WITH_VALID=ON '
+		'-DLIBXML2_WITH_WRITER=ON '
+		'-DLIBXML2_WITH_XINCLUDE=ON '
+		'-DLIBXML2_WITH_XPATH=ON '
+		'-DLIBXML2_WITH_XPTR=ON '
+		'-DLIBXML2_WITH_XPTR=ON '
+		'-Denable_tests=false '
+		'-Denable_tools=false '
+		'-Denable_examples=false '
+		'-Denable_docs=false '
+		'-Dtestdata_tests=false '
+		'--cross-file={meson_env_file} ./ ..'
+	,
 	'run_post_install' : [
 		'sed -i.bak \'s/Libs: -L${{libdir}} -lxml2/Libs: -L${{libdir}} -lxml2 -lz -llzma -liconv -lws2_32/\' "{pkg_config_path}/libxml-2.0.pc"', # libarchive complaints without this.
 	],
@@ -21,3 +68,12 @@
 	'update_check' : { 'url' : 'http://xmlsoft.org/sources/?C=M;O=D', 'type' : 'httpindex', 'regex' : r'libxml2-(?P<version_num>[\d.]+)-rc(?P<rc_num>[0-9]).tar.gz' },
 	'_info' : { 'version' : '2.9.10.1', 'fancy_name' : 'libxml2' },
 }
+# PRE-CMAKE:
+#	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static --without-python --enable-tests=no --enable-programs=no',
+#	'run_post_regexreplace' : [
+#		#'autoreconf -fiv',
+#		'./autogen.sh',
+#	],
+#	'run_post_install' : [
+#		'sed -i.bak \'s/Libs: -L${{libdir}} -lxml2/Libs: -L${{libdir}} -lxml2 -lz -llzma -liconv -lws2_32/\' "{pkg_config_path}/libxml-2.0.pc"', # libarchive complaints without this.
+#	],
