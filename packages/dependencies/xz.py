63dd08b890f5c16a5083a5a0d8709ba72d9bd17d
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-17 14:02:57 +1030

    Add files via upload

--- packages/dependencies/xz.py
+++ packages/dependencies/xz.py
@@ -4,7 +4,7 @@
 	'depth_git' : 0,
 	#'branch' : '6468f7e41a8e9c611e4ba8d34e2175c5dacdbeb4',
 	#'url' : 'http://git.tukaani.org/xz.git',
-	#'custom_cflag' : ' -D_FORTIFY_SOURCE=2 ', # 2019.12.13 it fails to build with anything other than this, eg it crashes with -O3 and -fstack-protector-all
+	'custom_cflag' : ' -D_FORTIFY_SOURCE=2 ', # 2019.12.13 it fails to build with anything other than this, eg it crashes with -O3 and -fstack-protector-all
 	#'custom_cflag' : ' -O3 -fstack-protector-all -D_FORTIFY_SOURCE=2 ', # 2019.12.13 it fails to build with anything other than this, eg it crashes with -O3 and -fstack-protector-all
 	#'run_post_regexreplace' : [ # 
 	'run_post_regexreplace' : [
