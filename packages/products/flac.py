ea0c8bb00caf42c6ae1f8fc0ad0771e293f614d2
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-18 00:18:35 +1030

    Add files via upload

--- packages/products/flac.py
+++ packages/products/flac.py
@@ -4,11 +4,18 @@
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
 	#'configure_options' : '--host={target_host} --prefix={output_prefix}/flac_git.installed --disable-shared --enable-static',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_64_BIT_WORDS=ON -DBUILD_PROGRAMS=ON -DINSTALL_PKGCONFIG_MODULES=ON -DINSTALL_MANPAGES=OFF -DBUILD_SHARED_LIBS=OFF -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DVERSION=1.3.3 -DCMAKE_BUILD_TYPE=Release', # 2020.04.06 try to fix vamp error. this -> sndfile -> vamp
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} '
+								'-DENABLE_64_BIT_WORDS=ON -DBUILD_PROGRAMS=ON -DINSTALL_PKGCONFIG_MODULES=ON -DINSTALL_MANPAGES=OFF -DBUILD_SHARED_LIBS=OFF '
+								'-DWITH_OGG=ON -DBUILD_DOCS=OFF -DWITH_STACK_PROTECTOR=ON -DBUILD_TESTING=OFF -DBUILD_EXAMPLES=OFF -DVERSION=1.3.4 '
+								'-DCMAKE_BUILD_TYPE=Release',
 	'patches': [
-		('flac/0001-mingw-fix-2.patch', '-p1', '..'),
-		('flac/libFLAC_CMakeLists.txt.patch', '-p1', '..'),
-		#('flac/libFLAC++_CMakeLists.txt.patch', '-p1', '..'),
+		#('flac/0001-mingw-fix-2.patch', '-p1', '..'),
+		#('flac/libFLAC_CMakeLists.txt.patch', '-p1', '..'),
+		('flac/0001-mingw-fix-2020.05.14.patch', '-p1', '..'), # 2020.05.11 from deadsix27
+		('flac/0001-mingw-fix-src-CMakeLists-2020.05.14.patch', '-p1', '..'), # 2020.05.11 from deadsix27
+	],
+	'run_post_patch' : [
+		'pwd ; cd .. ; sh ./autogen.sh --no-symlink ; cd _build ; pwd',
 	],
 	#'regex_replace': {
 	#	'post_patch': [
