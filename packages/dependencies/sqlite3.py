bc32bbf72cd48fdd6800053e4fe32d7b5f55e73a
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-17 19:18:00 +1030

    Add files via upload

--- packages/dependencies/sqlite3.py
+++ packages/dependencies/sqlite3.py
@@ -1,8 +1,10 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [ # https://fossies.org/linux/misc/ 
-		{ 'url' : 'https://www.sqlite.org/2022/sqlite-autoconf-3380100.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '8e3a8ceb9794d968399590d2ddf9d5c044a97dd83d38b9613364a245ec8a2fc4' }, ], },
-		# fall back to older version on fossies ... why did fossies revert ?
+		#{ 'url' : 'https://www.sqlite.org/2022/sqlite-autoconf-3380100.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '8e3a8ceb9794d968399590d2ddf9d5c044a97dd83d38b9613364a245ec8a2fc4' }, ], },
+		#{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3380000.tar.gz' , 'hashes' : [ { 'type' : 'sha256', 'sum' : '1c76e25dc63d9f3935e0f406aec520a33ee77cf54ea5147dffe1fae8369eff68' }, ], },
+		# fall back to older version on fossies ... why did fossies revert ? We will too.
+		{ 'url' : 'https://www.sqlite.org/2022/sqlite-autoconf-3380000.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '1c76e25dc63d9f3935e0f406aec520a33ee77cf54ea5147dffe1fae8369eff68' }, ], },
 		{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3380000.tar.gz' , 'hashes' : [ { 'type' : 'sha256', 'sum' : '1c76e25dc63d9f3935e0f406aec520a33ee77cf54ea5147dffe1fae8369eff68' }, ], },
 	],
 	'cflag_addition' : '-fexceptions -DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_USE_MALLOC_H=1 -DSQLITE_USE_MSIZE=1 -DSQLITE_DISABLE_DIRSYNC=1 -DSQLITE_ENABLE_RTREE=1 -fno-strict-aliasing',
