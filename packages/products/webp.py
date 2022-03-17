5e83d80f98913db51e00821c5a0c17999c58f158
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-18 07:12:16 +1030

    Add files via upload

--- packages/products/webp.py
+++ packages/products/webp.py
@@ -7,21 +7,22 @@
 	'source_subfolder': '_build',
 	'conf_system' : 'cmake',
 	'configure_options' : '.. {cmake_prefix_options} '
-		'-DCMAKE_INSTALL_PREFIX={output_prefix}/webp.installed -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release '
+		'-DCMAKE_INSTALL_PREFIX={output_prefix}/webp.installed -DBUILD_SHARED_LIBS=OFF '
 		'-DWEBP_ENABLE_SIMD=ON '
 		'-DWEBP_NEAR_LOSSLESS=ON '
 		'-DWEBP_UNICODE=ON '
 		'-DWEBP_BUILD_GIF2WEBP=OFF '
 		'-DWEBP_BUILD_IMG2WEBP=OFF '
-		'-DWEBP_BUILD_WEBPMUX=OFF '
-		'-DWEBP_BUILD_ANIM_UTILS=OFF '
 		'-DWEBP_BUILD_CWEBP=ON '
 		'-DWEBP_BUILD_DWEBP=ON '
 		'-DWEBP_BUILD_VWEBP=ON '
 		'-DWEBP_BUILD_WEBPINFO=ON '
+		'-DWEBP_BUILD_WEBPMUX=ON '
 		'-DWEBP_BUILD_EXTRAS=OFF '
+		'-DWEBP_BUILD_ANIM_UTILS=OFF '
 		'-DWEBP_BUILD_WEBP_JS=OFF '
 		'-DWEBP_ENABLE_SWAP_16BIT_CSP=ON '
+		'-DCMAKE_BUILD_TYPE=Release '
 	,
 #   'run_post_regexreplace': [ # 2019.12.13
 #		'sed -i.bak "s/\$LIBPNG_CONFIG /\$LIBPNG_CONFIG --static /g" ../configure.ac', # fix building with libpng # 2019.12.13
