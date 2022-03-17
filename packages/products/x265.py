062ed474ae8e444f5f8b57b9c61fb1c51937977e
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-18 07:19:38 +1030

    Add files via upload

--- packages/products/x265.py
+++ packages/products/x265.py
@@ -12,13 +12,16 @@
 		'-DCMAKE_AR={cross_prefix_full}ar '
 		'-DENABLE_SHARED=OFF '
 		'-DENABLE_ASSEMBLY=ON '
+		'-DENABLE_SHARED=OFF '
+		#'-DENABLE_CLI:BOOL=OFF '
 		'-DEXTRA_LIB="x265_main10.a;x265_main12.a" '
 		'-DEXTRA_LINK_FLAGS="-L{offtree_prefix}/libx265_10bit/lib;-L{offtree_prefix}/libx265_12bit/lib" '
 		'-DLINKED_10BIT=ON '
 		'-DLINKED_12BIT=ON '
-		'-DLIBXML_STATIC=ON -DGLIB_STATIC_COMPILATION=ON ' # 2019.12.13
-		'-DENABLE_HDR10_PLUS=ON ' # 2020.07.27
 		'-DCMAKE_INSTALL_PREFIX={output_prefix}/x265.installed'
+		'-DLIBXML_STATIC=ON '
+		'-DGLIB_STATIC_COMPILATION=ON ' # 2019.12.13
+		'-DENABLE_HDR10_PLUS=ON ' # 2020.07.27
 	,
 	'conf_system' : 'cmake',
 	'depends_on' : [ 'libxml2', 'libx265_multibit_10', 'libx265_multibit_12' ],
