c7b152dd61f2d04b46f41c8ec165814546529352
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-17 19:30:16 +1030

    Add files via upload

--- packages/dependencies/libpng.py
+++ packages/dependencies/libpng.py
@@ -6,9 +6,16 @@
 	],
 	'conf_system' : 'cmake',
 	# 'custom_cflag' : '-fno-asynchronous-unwind-tables',
-	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DBUILD_BINARY=OFF -DCMAKE_BUILD_TYPE=Release -DPNG_TESTS=OFF -DPNG_SHARED=OFF -DPNG_STATIC=ON', # 2019.12.13
+	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DCMAKE_BUILD_TYPE=Release '
+						'-DPNG_SHARED=OFF -DPNG_STATIC=ON -DPNG_TESTS=OFF -DPNG_BUILD_ZLIB=ON -DPNG_FRAMEWORK=OFF '
+						'-DPNG_HARDWARE_OPTIMIZATIONS=ON -DPNG_DEBUG=OFF '
+						'-DBUILD_SHARED_LIBS=OFF -DBUILD_BINARY=OFF  '
+						
 	'patches' : [
 		('libpng/libpng-1.6.37-apng.patch', '-p1'),
+	],
+	'run_post_patch' : [
+		'pwd ; sh ./autogen.sh ; pwd',
 	],
 	'depends_on' : [ 'zlib', ],
 	'update_check' : { 'url' : 'https://sourceforge.net/projects/libpng/files/libpng16/', 'type' : 'sourceforge', },
