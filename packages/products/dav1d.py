33e7a11495cf12a594dcf41ffa41cd603368a997
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-18 00:05:35 +1030

    Add files via upload

--- packages/products/dav1d.py
+++ packages/products/dav1d.py
@@ -4,12 +4,12 @@
 	'conf_system' : 'meson',
 	'build_system' : 'ninja',
 	'source_subfolder' : 'build',
-	'env_exports' : { # 2020.09.17 remove fortify_source sue to link errors
-		'CFLAGS'   : ' -O3 ',
-		'CXXFLAGS' : ' -O3 ',
-		'CPPFLAGS' : ' -O3 ',
-		'LDFLAGS'  : ' -O3 ',
-	},
+	#'env_exports' : { # 2020.09.17 remove fortify_source sue to link errors
+	#	'CFLAGS'   : ' -O3 ',
+	#	'CXXFLAGS' : ' -O3 ',
+	#	'CPPFLAGS' : ' -O3 ',
+	#	'LDFLAGS'  : ' -O3 ',
+	#},
 	'regex_replace': { #hacky but works, so who cares, for some reason libdav1d thinks we have POSIX_MEMALIGN.. maybe mingw or gcc bug, .. so we'll just force it to not define that we have it so it doesn't use it.
 		'post_patch': [
 			{
@@ -24,17 +24,23 @@
 			},
 		],
 	},
-    'run_post_regexreplace' : [
+    #'run_post_regexreplace' : [
 		# 'sed -i.bak \'s/sdl2_dependency.found()/false/\' ../tools/meson.build' # 2019.12.13 # 2020.03.19 commented out
-	],
+	#],
 	'configure_options' :
 		'--prefix={output_prefix}/dav1d.installed  '    # 2019.12.13 old '--prefix={product_prefix}/dav1d.installed  '
 		'--libdir={output_prefix}/dav1d.installed/lib ' # 2019.12.13 old '--libdir={product_prefix}/dav1d.installed/lib '
 		'--default-library=static '
+		'--strip '
 		'--backend=ninja '
+		'--buildtype=release '
 		'-Denable_tests=false '
 		'-Denable_tools=true '
-		'--buildtype=release '
+		'-Denable_examples=false '
+		'-Denable_docs=false '
+		'-Dtestdata_tests=false '
+		'-Denable_asm=true '
+		'-DBITDEPTHS=["8","16"] '
 		'--cross-file={meson_env_file} ./ ..'
 	,
   	#'run_post_regexreplace' : [ # 2019.12.13
