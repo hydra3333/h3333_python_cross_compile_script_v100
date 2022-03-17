7ee92418d9bd87166970fea502250a1b54b3f1ab
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-17 17:03:47 +1030

    Add files via upload

--- packages/dependencies/libxml2.py
+++ packages/dependencies/libxml2.py
@@ -8,57 +8,10 @@
 	],
 	'folder_name' : 'libxml2-2.9.12',
 	#'rename_folder' : 'libxml2-2.9.12',
-	
-	'conf_system' : 'cmake',
-	'source_subfolder' : '_build',
 	'run_post_patch' : [
 		'pwd ; cd .. ; sh autogen.sh --no-symlink ; cd _build ; pwd',
 	],	
-	'configure_options' :
-		'--prefix={target_prefix} '
-		'--libdir={target_prefix}/lib '
-		'--default-library=static '
-		'--backend=ninja '
-		'--strip '
-		'--buildtype=release '
-		'-DBUILD_SHARED_LIBS=OFF '
-		'-DLIBXML2_WITH_DEBUG=OFF '
-		'-DLIBXML2_WITH_DOCB=OFF '
-		'-DLIBXML2_WITH_FTP=ON '
-		'-DLIBXML2_WITH_HTML=ON '
-		'-DLIBXML2_WITH_HTTP=ON '
-		'-DLIBXML2_WITH_ICONV '
-		'-DLIBXML2_WITH_ISO8859X=ON '
-		'-DLIBXML2_WITH_LZMA=ON '
-		'-DLIBXML2_WITH_LEGACY=ON '
-		'-DLIBXML2_WITH_MODULES=ON '
-		'-DLIBXML2_WITH_OUTPUT=ON '
-		'-DLIBXML2_WITH_PATTERN=ON '
-		'-DLIBXML2_WITH_PROGRAMS=OFF '
-		'-DLIBXML2_WITH_PUSH=ON '
-		'-DLIBXML2_WITH_PYTHON=ON '
-		'-DLIBXML2_WITH_READER=ON '
-		'-DLIBXML2_WITH_REGEXPS=ON '
-		'-DLIBXML2_WITH_SAX1=ON '
-		'-DLIBXML2_WITH_SCHEMATRON=ON '
-		'-DLIBXML2_WITH_SCHEMATRON=ON '
-		'-DLIBXML2_WITH_TESTS=OFF '
-		'-DLIBXML2_WITH_THREADS=ON '
-		'-DLIBXML2_WITH_THREAD_ALLOC=OFF '
-		'-DLIBXML2_WITH_TREE=ON '
-		'-DLIBXML2_WITH_VALID=ON '
-		'-DLIBXML2_WITH_WRITER=ON '
-		'-DLIBXML2_WITH_XINCLUDE=ON '
-		'-DLIBXML2_WITH_XPATH=ON '
-		'-DLIBXML2_WITH_XPTR=ON '
-		'-DLIBXML2_WITH_XPTR=ON '
-		'-Denable_tests=false '
-		'-Denable_tools=false '
-		'-Denable_examples=false '
-		'-Denable_docs=false '
-		'-Dtestdata_tests=false '
-		'--cross-file={meson_env_file} ./ ..'
-	,
+	'configure_options' : '{autoconf_prefix_options} --disable-shared --enable-static --without-python --enable-tests=no --enable-programs=no',
 	'run_post_install' : [
 		'sed -i.bak \'s/Libs: -L${{libdir}} -lxml2/Libs: -L${{libdir}} -lxml2 -lz -llzma -liconv -lws2_32/\' "{pkg_config_path}/libxml-2.0.pc"', # libarchive complaints without this.
 	],
@@ -73,7 +26,54 @@
 #	'run_post_regexreplace' : [
 #		#'autoreconf -fiv',
 #		'./autogen.sh',
-#	],
-#	'run_post_install' : [
-#		'sed -i.bak \'s/Libs: -L${{libdir}} -lxml2/Libs: -L${{libdir}} -lxml2 -lz -llzma -liconv -lws2_32/\' "{pkg_config_path}/libxml-2.0.pc"', # libarchive complaints without this.
-#	],
+#
+#
+# CMAKE does not have the usual options, revert or ordinary configure/make
+#	'conf_system' : 'cmake',
+#	'source_subfolder' : '_build',
+#	'configure_options' :
+#		'.. {cmake_prefix_options} '
+#		'--prefix={target_prefix} '
+#		'--libdir={target_prefix}/lib '
+#		'--default-library=static '
+#		'--backend=ninja '
+#		'--strip '
+#		'--cross-file={meson_env_file} '
+#		'--buildtype=release '
+#		'-DBUILD_SHARED_LIBS=OFF '
+#		'-DLIBXML2_WITH_DEBUG=OFF '
+#		'-DLIBXML2_WITH_DOCB=OFF '
+#		'-DLIBXML2_WITH_FTP=ON '
+#		'-DLIBXML2_WITH_HTML=ON '
+#		'-DLIBXML2_WITH_HTTP=ON '
+#		'-DLIBXML2_WITH_ICONV '
+#		'-DLIBXML2_WITH_ISO8859X=ON '
+#		'-DLIBXML2_WITH_LZMA=ON '
+#		'-DLIBXML2_WITH_LEGACY=ON '
+#		'-DLIBXML2_WITH_MODULES=ON '
+#		'-DLIBXML2_WITH_OUTPUT=ON '
+#		'-DLIBXML2_WITH_PATTERN=ON '
+#		'-DLIBXML2_WITH_PROGRAMS=OFF '
+#		'-DLIBXML2_WITH_PUSH=ON '
+#		'-DLIBXML2_WITH_PYTHON=ON '
+#		'-DLIBXML2_WITH_READER=ON '
+#		'-DLIBXML2_WITH_REGEXPS=ON '
+#		'-DLIBXML2_WITH_SAX1=ON '
+#		'-DLIBXML2_WITH_SCHEMATRON=ON '
+#		'-DLIBXML2_WITH_SCHEMATRON=ON '
+#		'-DLIBXML2_WITH_TESTS=OFF '
+#		'-DLIBXML2_WITH_THREADS=ON '
+#		'-DLIBXML2_WITH_THREAD_ALLOC=OFF '
+#		'-DLIBXML2_WITH_TREE=ON '
+#		'-DLIBXML2_WITH_VALID=ON '
+#		'-DLIBXML2_WITH_WRITER=ON '
+#		'-DLIBXML2_WITH_XINCLUDE=ON '
+#		'-DLIBXML2_WITH_XPATH=ON '
+#		'-DLIBXML2_WITH_XPTR=ON '
+#		'-DLIBXML2_WITH_XPTR=ON '
+#		'-Denable_tests=false '
+#		'-Denable_tools=false '
+#		'-Denable_examples=false '
+#		'-Denable_docs=false '
+#		'-Dtestdata_tests=false '
+#
