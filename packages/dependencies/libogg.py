de7aae7f604d3f81c02f9ce27df4a248903ed396
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-17 14:19:38 +1030

    Add files via upload

--- packages/dependencies/libogg.py
+++ packages/dependencies/libogg.py
@@ -5,7 +5,7 @@
 	'source_subfolder' : '_build',
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release -DINSTALL_DOCS=OFF -DHAVE_SQLITE3=ON',
 	'run_post_regexreplace' : [
-		'cd .. ; sh ./autogen.sh ; cd _build',
+		'cd .. ; pwd ; sh ./autogen.sh ; cd _build ; pwd',
 	],
 	'depends_on' : [
 		'sqlite3',
