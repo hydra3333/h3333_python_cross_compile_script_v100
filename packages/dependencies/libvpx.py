59fb9b291974b17590229c041311d28c991907ab
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-18 00:25:46 +1030

    Add files via upload

--- packages/dependencies/libvpx.py
+++ packages/dependencies/libvpx.py
@@ -1,19 +1,19 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://chromium.googlesource.com/webm/libvpx',
-	'depth_git' : 0,
+    'depth_git' : 0,
 	'branch' : 'main',  # they've changed the trunk from master to main (a US political race thing against the word, apparently)
 	'configure_options' :
 		'--target={bit_name2}-{bit_name_win}-gcc '
 		'--prefix={target_prefix} '
 		'--disable-shared --enable-static --enable-webm-io --enable-libyuv --enable-vp9 '
 		'--enable-vp8 --enable-ssse3 --enable-runtime-cpu-detect --enable-postproc '
-		'--enable-vp9-highbitdepth --enable-vp9-postproc --enable-postproc-visualizer ' # 2020.10.01 removed --enable-coefficient-range-checking 
+		'--enable-vp9-highbitdepth --enable-vp9-postproc --enable-coefficient-range-checking --enable-postproc-visualizer '
 		'--enable-error-concealment --enable-better-hw-compatibility '
 		'--enable-multi-res-encoding --enable-vp9-temporal-denoising '
 		'--disable-tools --disable-docs --disable-examples --disable-install-docs ' # 2019.12.13 enable tools and examples
 		'--disable-unit-tests --disable-decode-perf-tests --disable-encode-perf-tests '
-        '--disable-avx512 --as=nasm ' # 2013.13.13 back to --as=nasm rather than yasm # 2019.12.13 removed --as=yasm, added --disable-avx512
+		'--disable-avx512 --as=nasm ' # 2013.13.13 back to --as=nasm rather than yasm # 2019.12.13 removed --as=yasm, added --disable-avx512
 	,
 	'env_exports' : {
 		'CROSS' : '{cross_prefix_bare}',
