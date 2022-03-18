db9a38f43238528b8ed41f5fcc3a09dadd4bbdf7
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-18 19:53:12 +1030

    Add files via upload

--- packages/products/mp4box.py
+++ packages/products/mp4box.py
@@ -1,10 +1,8 @@
 {
 	'repo_type' : 'git',
 	'depth_git' : 0,
-	#'branch' : 'ce9a843ffcd941d3d17a7bab20749b94a8e3c43c', # as at 2020.06.19, ce9a843ffcd941d3d17a7bab20749b94a8e3c43c is top of the tree 'legacy' (not the master tree)
-	#'branch' : 'tags/v0.8.1', # 2020.06.19 0.8.1 is not the master, it's a separate branch named legacy. the commit above is to the legacy tree
-	#'branch' : '7d0bb6eb526f0bd9fae53fcbdd846cfe839ed821', # 2020.06.19 as of april 8th, first use of "#include <Windows.h>" in the non-Legacy tree
 	'url' : 'https://github.com/gpac/gpac.git',
+	'branch' : 'tags/v2.0.0',
 	'rename_folder' : 'mp4box_git',
 	'do_not_bootstrap' : True,
 	'run_post_regexreplace' : [
