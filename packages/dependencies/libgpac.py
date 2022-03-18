e1db84457b352905795e96bff49aa4aa718dce21
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-18 10:52:06 +1030

    Add files via upload

--- packages/dependencies/libgpac.py
+++ packages/dependencies/libgpac.py
@@ -1,9 +1,10 @@
 {
 	'repo_type' : 'git',
-	#'depth_git' : 0,
+	'depth_git' : 0,
 	'url' : 'https://github.com/gpac/gpac.git',
-	'branch' : 'tags/v2.0.0',
-	'rename_folder' : 'GPAC_git',
+	#'branch' : 'tags/v2.0.0',
+	#'branch' : '7f40077a2af0b50d8fbdd1f95d01cb02c36e22db',
+	'rename_folder' : 'gpac_lib_git',
 	'do_not_bootstrap' : True,
 	'run_post_regexreplace' : [
 		#'sed -i.bak \'s/Windows.h/windows.h/g\' "include/gpac/thread.h"', # 2020.06.19 ah, tree "legacy" does not have these to do a sed on
@@ -16,16 +17,25 @@
 		'sed -i.bak \'s/SHFLAGS=-shared/SHFLAGS=/g\' configure',
 		'sed -i.bak \'s/extralibs="$extralibs -lws2_32 -lwinmm -limagehlp"/extralibs="$extralibs -lws2_32 -lwinmm -lz -lssp"/g\' configure',
 	],
-	#'configure_options' : '--host={target_host} --target-os={bit_name3} --prefix={output_prefix}/mp4box_git.installed --static-modules --cross-prefix={cross_prefix_bare} --static-mp4box --enable-static-bin --disable-oss-audio --disable-x11 --disable-docs --disable-shared --enable-static --extra-cflags="-DLIBXML_STATIC" --extra-cflags="-DGLIB_STATIC_COMPILATION" --enable-sdl-static ', # 2019.12.13 # 2020.05.13 remove --sdl-cfg={cross_prefix_full}sdl2-config 
 	'configure_options' : '--host={target_host} --target-os={bit_name3} --prefix={output_prefix}/mp4box_git.installed --cross-prefix={cross_prefix_bare} '
         '--enable-static --static-modules --static-build --static-bin --disable-shared '
         '--disable-docs --disable-ipv6 --enable-mem-track --enable-depth '
         '--enable-sdl-static '
         '--disable-oss-audio --disable-x11 '
-        '--extra-cflags="-DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION" '
-        '--extra-ldflags="-L{target_prefix}/lib -lbz2" '
-        #'--use-ffmpeg=local --extra-ldflags="-L{target_prefix}/lib -lavutil -lavcodec -lavfilter -lavformat -lpostproc" ' # add ffmpeg 
-    , # 2019.12.13 # 2020.05.13 remove --sdl-cfg={cross_prefix_full}sdl2-config 
+        #'--use-ffmpeg=no '
+        #'--extra-cflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION " '
+        #'--extra-ldflags=" -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc " '
+        #'--extra-cflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION" -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc " '
+        #'--extra-ldflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION" -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc " '
+        '--extra-cflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc " '
+        '--extra-ldflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc " '
+		'--extra-libs=" -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc " '
+		'--disable-all --enable-mem-track --enable-depth --enable-sdl-static '
+		'--enable-avi --enable-m2ps --enable-m2ts --enable-m2ts-mux --enable-parsers --enable-import '
+		'--enable-loader-isoff --enable-loader-bt --enable-loader-xmt '
+		'--enable-isoff --enable-isoff-write --enable-isoff-hint --enable-isoff-frag --enable-isoff-hds '
+		'--enable-streaming --enable-hevc --enable-nvdec '
+    ,
 	#'run_post_configure' : [ 
 	#	'./check_revision.sh', # 2021.04.10 per https://github.com/rdp/ffmpeg-windows-build-helpers/commit/f8f1c51573b7e3a85183e9f2a1ddcb6895d1e844
 	#],
