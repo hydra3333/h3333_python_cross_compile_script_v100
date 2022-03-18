e3a89aec24fc312fc4dbc27b4ea2033218173ef3
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-18 23:58:46 +1030

    Add files via upload

--- packages/products/mp4box.py
+++ packages/products/mp4box.py
@@ -17,11 +17,17 @@
 		'sed -i.bak \'s/SHFLAGS=-shared/SHFLAGS=/g\' configure',
 		'sed -i.bak \'s/extralibs="$extralibs -lws2_32 -lwinmm -limagehlp"/extralibs="$extralibs -lws2_32 -lwinmm -lz -lbz2 -lssp"/g\' configure',
 	],
+	'env_exports' : {
+		'CFLAGS'   : ' {original_cflags} -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc "',
+		'CXXFLAGS' : ' {original_cflags} -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc "',
+		'CPPFLAGS' : ' {original_cflags} -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc "',
+		'LDFLAGS'  : ' {original_cflags} -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc "',
+	},
 	'configure_options' : '--host={target_host} --target-os={bit_name3} --prefix={output_prefix}/mp4box_git.installed --cross-prefix={cross_prefix_bare} '
         '--enable-static --static-modules --static-build --static-bin --disable-shared '
         '--disable-docs --disable-ipv6 --enable-mem-track --enable-depth '
         '--disable-oss-audio --disable-x11 '
-        #'--use-ffmpeg=no '
+        '--use-ffmpeg=no '
         #'--extra-cflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION " '
         #'--extra-ldflags=" -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc " '
         #'--extra-cflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION" -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc " '
@@ -29,7 +35,7 @@
         '--extra-cflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc " '
         '--extra-ldflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc " '
 		'--extra-libs=" -L{target_prefix}/lib -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc " '
-		'--disable-all '
+		#'--disable-all '
 		'--enable-mem-track --enable-depth --enable-sdl-static '
 		'--enable-avi --enable-m2ps --enable-m2ts --enable-m2ts-mux --enable-parsers --enable-import '
 		'--enable-loader-isoff --enable-loader-bt --enable-loader-xmt '
