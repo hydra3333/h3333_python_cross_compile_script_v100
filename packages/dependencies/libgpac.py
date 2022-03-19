4cb1e9d3e04b23dbf8114ad4755e2afb4ed52614
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-19 21:51:03 +1030

    Add files via upload

--- packages/dependencies/libgpac.py
+++ packages/dependencies/libgpac.py
@@ -17,23 +17,24 @@
 		#'sed -i.BAK1 \'s|docc()|docczlib() {\\n \$cc -o \$TMPO \$TMPC \$@ \\n \$cc -o \$TMPO \$TMPC \$@ 0\>/dev/null 2\>\$TMPL\\n dolog $@\\n}\\ndocc()|\' configure',
 	],
 	'env_exports' : {
-		'CFLAGS'   : ' {original_cflags} -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 ',
-		'CXXFLAGS' : ' {original_cflags} -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 ',
-		'CPPFLAGS' : ' {original_cflags} -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 ',
-		'LDFLAGS'  : ' {original_cflags} -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 ',
+		'CFLAGS'   : ' {original_cflags} -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavdevice -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 ',
+		'CXXFLAGS' : ' {original_cflags} -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavdevice -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 ',
+		'CPPFLAGS' : ' {original_cflags} -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavdevice -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 ',
+		'LDFLAGS'  : ' {original_cflags} -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavdevice -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 ',
 	},
 	#'cpu_count' : '1',
 	'configure_options' : '--host={target_host} --target-os={bit_name3} --prefix={target_prefix} --cross-prefix={cross_prefix_bare} '
 		'--enable-static --static-modules --static-build --static-bin --disable-shared --enable-pic '
 		'--disable-docs --disable-ipv6 --enable-mem-track --enable-depth '
 		'--disable-oss-audio --disable-x11 '
-		'--extra-cflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
-		'--extra-ldflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
-		'--extra-libs=" -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
+		'--extra-cflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavdevice -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
+		'--extra-ldflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavdevice -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
+		'--extra-libs=" -L{target_prefix}/lib/ -lavutil -lavdevice -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
 		'--enable-mem-track --enable-depth --enable-sdl-static '
 		'--enable-avi --enable-m2ps --enable-m2ts --enable-m2ts-mux --enable-parsers --enable-import '
 		'--enable-loader-isoff --enable-loader-bt --enable-loader-xmt '
 		'--enable-isoff --enable-isoff-write --enable-isoff-hint --enable-isoff-frag --enable-isoff-hds '
+		'--enable-streaming --enable-hevc --enable-nvdec '
 		'--enable-streaming '
 		'--enable-hevc '
 		'--enable-nvdec '
