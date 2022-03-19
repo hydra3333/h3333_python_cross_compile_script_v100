5ce95d3beb9ec0da683cd53ede68c4a7566a8252
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-19 21:42:32 +1030

    Add files via upload

--- packages/dependencies/libgpac.py
+++ packages/dependencies/libgpac.py
@@ -24,29 +24,31 @@
 	},
 	#'cpu_count' : '1',
 	'configure_options' : '--host={target_host} --target-os={bit_name3} --prefix={target_prefix} --cross-prefix={cross_prefix_bare} '
-        '--enable-static --static-modules --static-build --static-bin --disable-shared '
-        '--disable-docs --disable-ipv6 --enable-mem-track --enable-depth '
-        '--disable-oss-audio --disable-x11 '
-#        '--use-ffmpeg=no '
-        #'--extra-cflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION " '
-        #'--extra-ldflags=" -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
-        #'--extra-cflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION" -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
-        #'--extra-ldflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION" -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
-        '--extra-cflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
-        '--extra-ldflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
+		'--enable-static --static-modules --static-build --static-bin --disable-shared --enable-pic '
+		'--disable-docs --disable-ipv6 --enable-mem-track --enable-depth '
+		'--disable-oss-audio --disable-x11 '
+		'--extra-cflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
+		'--extra-ldflags=" -DGPAC_STATIC_MODULES -DLIBXML_STATIC -DGLIB_STATIC_COMPILATION -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
 		'--extra-libs=" -L{target_prefix}/lib/ -lbz2 -lavutil -lavcodec -lavfilter -lavformat -lpostproc -lz -lbz2 " '
-		#'--disable-all '
 		'--enable-mem-track --enable-depth --enable-sdl-static '
 		'--enable-avi --enable-m2ps --enable-m2ts --enable-m2ts-mux --enable-parsers --enable-import '
 		'--enable-loader-isoff --enable-loader-bt --enable-loader-xmt '
 		'--enable-isoff --enable-isoff-write --enable-isoff-hint --enable-isoff-frag --enable-isoff-hds '
-		'--enable-streaming --enable-hevc --enable-nvdec '
+		'--enable-streaming '
+		'--enable-hevc '
+		'--enable-nvdec '
+		'--use-a52==system '
+		'--use-zlib==system '
+		'--use-vorbis=system '
+		'--use-theora=system '
+		'--use-ogg=system '
+		'--use-ffmpeg=system '
     ,
 	#'run_post_configure' : [ 
 	#	'./check_revision.sh', # 2021.04.10 per https://github.com/rdp/ffmpeg-windows-build-helpers/commit/f8f1c51573b7e3a85183e9f2a1ddcb6895d1e844
 	#],
 	'depends_on' : [
-		 'zlib', 'bzip2', 'libffmpeg_extra', 'sdl2', # 2020.05.13 re-enable # 2020.05.13 remove SDL2 'sdl2', 
+		 'a52dec', 'zlib', 'bzip2', 'libffmpeg_extra', 'sdl2', # 2020.05.13 re-enable # 2020.05.13 remove SDL2 'sdl2', 
 	],
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'GPAC library' },
 }
