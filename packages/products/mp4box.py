0973743509cab2c345bc22e150d339ff7d94f6ca
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-03-19 15:51:29 +1030

    Add files via upload

--- packages/products/mp4box.py
+++ packages/products/mp4box.py
@@ -2,7 +2,7 @@
 	'repo_type' : 'git',
 	'depth_git' : 0,
 	'url' : 'https://github.com/gpac/gpac.git',
-	'branch' : 'tags/v2.0.0',
+	#'branch' : 'tags/v2.0.0',
 	#'folder_name' : 'mp4box_git',
 	#'rename_folder' : 'mp4box_git',
 	'do_not_bootstrap' : True,
