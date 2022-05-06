1f44677976de14373cdb0831613bee426da7a785
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-05-07 09:11:11 +0930

    revert freetype version

--- packages/dependencies/freetype_lib-with-harfbuzz.py
+++ packages/dependencies/freetype_lib-with-harfbuzz.py
@@ -1,13 +1,13 @@
 { # http://git.savannah.gnu.org/cgit/freetype/freetype2.git/
 	'repo_type' : 'archive',
 	'download_locations' : [
-        # freetype 2.11.0 kills mp4box building, so keep freetype 2.10.4 ? mp4box fails anyway so just keep going
-        #{ 'url' : 'https://download.savannah.gnu.org/releases/freetype/freetype-2.11.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3333ae7cfda88429c97a7ae63b7d01ab398076c3b67182e960e5684050f2c5c8' }, ], },
-		#{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.11.1/freetype-2.11.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3333ae7cfda88429c97a7ae63b7d01ab398076c3b67182e960e5684050f2c5c8' }, ], },
-		#{ 'url' : 'https://fossies.org/linux/misc/freetype-2.11.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3333ae7cfda88429c97a7ae63b7d01ab398076c3b67182e960e5684050f2c5c8' }, ], },
-        { 'url' : 'https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f' }, ], },
-		{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.12.1/freetype-2.12.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/freetype-2.12.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f' }, ], },
+		# freetype 2.11.0 kills mp4box building, so keep freetype 2.10.4 ? mp4box fails anyway so just keep going
+		{ 'url' : 'https://download.savannah.gnu.org/releases/freetype/freetype-2.11.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3333ae7cfda88429c97a7ae63b7d01ab398076c3b67182e960e5684050f2c5c8' }, ], },
+		{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.11.1/freetype-2.11.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3333ae7cfda88429c97a7ae63b7d01ab398076c3b67182e960e5684050f2c5c8' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/freetype-2.11.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3333ae7cfda88429c97a7ae63b7d01ab398076c3b67182e960e5684050f2c5c8' }, ], },
+		#{ 'url' : 'https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f' }, ], },
+		#{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.12.1/freetype-2.12.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f' }, ], },
+		#{ 'url' : 'https://fossies.org/linux/misc/freetype-2.12.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f' }, ], },
 	],
 	'folder_name' : 'freetype-with-harfbuzz',
 	'rename_folder' : 'freetype-with-harfbuzz',
