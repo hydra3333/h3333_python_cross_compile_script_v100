4ef367f58b62f84a4ee7484604f3dc8c61d304d1
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-05-08 12:28:58 +0930

    update sqlite3

--- packages/dependencies/sqlite3.py
+++ packages/dependencies/sqlite3.py
@@ -1,10 +1,10 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [ # https://fossies.org/linux/misc/ 
-		#{ 'url' : 'https://www.sqlite.org/2022/sqlite-autoconf-3380000.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '1c76e25dc63d9f3935e0f406aec520a33ee77cf54ea5147dffe1fae8369eff68' }, ], },
-		#{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3380000.tar.gz' , 'hashes' : [ { 'type' : 'sha256', 'sum' : '1c76e25dc63d9f3935e0f406aec520a33ee77cf54ea5147dffe1fae8369eff68' }, ], },
-		{ 'url' : 'https://www.sqlite.org/2022/sqlite-autoconf-3380200.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e7974aa1430bad690a5e9f79a6ee5c8492ada8269dc675875ad0fb747d7cada4' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3380200.tar.gz' , 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e7974aa1430bad690a5e9f79a6ee5c8492ada8269dc675875ad0fb747d7cada4' }, ], },
+		#{ 'url' : 'https://www.sqlite.org/2022/sqlite-autoconf-3380200.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e7974aa1430bad690a5e9f79a6ee5c8492ada8269dc675875ad0fb747d7cada4' }, ], },
+		#{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3380200.tar.gz' , 'hashes' : [ { 'type' : 'sha256', 'sum' : 'e7974aa1430bad690a5e9f79a6ee5c8492ada8269dc675875ad0fb747d7cada4' }, ], },
+		{ 'url' : 'https://www.sqlite.org/2022/sqlite-autoconf-3380500.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '5af07de982ba658fd91a03170c945f99c971f6955bc79df3266544373e39869c' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/sqlite-autoconf-3380500.tar.gz' , 'hashes' : [ { 'type' : 'sha256', 'sum' : '5af07de982ba658fd91a03170c945f99c971f6955bc79df3266544373e39869c' }, ], },
 	],
 	'cflag_addition' : '-fexceptions -DSQLITE_ENABLE_COLUMN_METADATA=1 -DSQLITE_USE_MALLOC_H=1 -DSQLITE_USE_MSIZE=1 -DSQLITE_DISABLE_DIRSYNC=1 -DSQLITE_ENABLE_RTREE=1 -fno-strict-aliasing',
 	'strip_cflags': ['-ffast-math', ],
