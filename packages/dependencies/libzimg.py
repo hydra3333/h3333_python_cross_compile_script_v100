1a65fe1a2467eb7ff45f4235d1872d3f5ad47c05
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-06-25 12:05:25 +0930

    Add files via upload

--- packages/dependencies/libzimg.py
+++ packages/dependencies/libzimg.py
@@ -3,7 +3,7 @@
 	'url' : 'https://github.com/sekrit-twc/zimg.git',
 	#'depth_git' : 0,
 	#'branch' : 'c9a15ec4f86adfef6c7cede8dae79762d34f2564', # 2022.06.25 revert per MABS # 2022.06.24 per MABS, affix commit as after this one breaks, cpuinfo broken
-	'recursive_git' : True,
+	'recursive_git' : False,
 	'patches' : [
 		('zimg/0001-libm_wrapper-define-__CRT__NO_INLINE-before-math.h-from-MABS.patch', '-Np1' ), # 2020.11.07 from MABS per https://github.com/m-ab-s/media-autobuild_suite/commit/e57877f87abf1a07b79284de257e3cad6bbf7409#diff-adfad29200df8b40f03fd8670a191195ff4f98c0160a7b12ada8caa8cb824c75
 	],
