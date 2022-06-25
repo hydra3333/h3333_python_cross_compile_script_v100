35f241cfe09dc6bed903fd40d1711204a3d336e3
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-06-25 10:47:56 +0930

    Add files via upload

--- packages/dependencies/libzimg.py
+++ packages/dependencies/libzimg.py
@@ -8,6 +8,7 @@
 		('zimg/0001-libm_wrapper-define-__CRT__NO_INLINE-before-math.h-from-MABS.patch', '-Np1' ), # 2020.11.07 from MABS per https://github.com/m-ab-s/media-autobuild_suite/commit/e57877f87abf1a07b79284de257e3cad6bbf7409#diff-adfad29200df8b40f03fd8670a191195ff4f98c0160a7b12ada8caa8cb824c75
 	],
 	'run_post_regexreplace' : [
+		'git submodule update --init --recursive',
 		'git submodule update --remote --recursive', # MABS: git submodule update --init --recursive
 		#'autoreconf -fiv', # 2020.11.07 from MABS 
 		'sh ./autogen.sh',
