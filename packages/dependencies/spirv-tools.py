6392ce95f8e7a22d7f07afea7573d0eab5649939
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-06-28 19:46:12 +0930

    like MABS bulk update SPIRV,glslang,vulkan,libplacebo
    this is the initial fix, including MABS path files
    it may not work, so iterate until it does

--- packages/dependencies/spirv-tools.py
+++ packages/dependencies/spirv-tools.py
@@ -3,11 +3,39 @@
 	'rename_folder' : 'spirv-tools',
 	'url' : 'https://github.com/KhronosGroup/SPIRV-Tools.git',
 	'depth_git': 0,
-	#'branch': '323a81fc5e30e43a04e5e22af4cba98ca2a161e6', # 2020.03.19 comment out
-	'needs_make' : False,
-	'needs_make_install' : False,
-	'needs_configure' : False,
 	'recursive_git' : True,
+	#'branch': '323a81fc5e30e43a04e5e22af4cba98ca2a161e6', # 2020.03.19 comment out
+	#'needs_make' : False,
+	#'needs_make_install' : False,
+	#'needs_configure' : False,
+	'conf_system' : 'cmake',
+	'source_subfolder' : '_build',
+	'configure_options' : 
+		'.. {cmake_prefix_options} '
+		'-DCMAKE_INSTALL_PREFIX={target_prefix} '
+		'-DSKIP_SPIRV_TOOLS_INSTALL=OFF '
+		'-DSPIRV_TOOLS_BUILD_STATIC=ON '
+		'-DSPIRV_SKIP_EXECUTABLES=ON '
+		'-DSPIRV_SKIP_TESTS=ON '
+	,
+	#
+	# If SPIR-V Tools is configured as a standalone project, then 
+	# download the googletest source into the 
+	# <spirv-dir>/external/googletest 
+	# directory before configuring and building the project.
+	#
+	'run_post_regexreplace' : [
+		'pwd',
+		'if [ -d "../external/spirv-headers" ] ; then rm -fvR "../external/spirv-headers" ; fi',
+		'if [ -d "../external/googletest" ] ; then rm -fvR "../external/googletest" ; fi',
+		'if [ -d "../external/effcee" ] ; then rm -fvR "../external/effcee" ; fi',
+		'if [ -d "../external/re2" ] ; then rm -fvR "../external/re2" ; fi',
+		'git clone https://github.com/KhronosGroup/SPIRV-Headers.git ../external/spirv-headers',
+		'git clone https://github.com/google/googletest.git          ../external/googletest',
+		'git clone https://github.com/google/effcee.git              ../external/effcee',
+		'git clone https://github.com/google/re2.git                 ../external/re2',
+	],
+	'depends_on' : [ 'spirv-headers', ],
 	'update_check' : { 'type' : 'git', },
-	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'SPIRV Tools' },
+	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'SPIRV-Tools' },
 }
--- packages/dependencies/vulkan_loader.py
+++ packages/dependencies/vulkan_loader.py
@@ -19,7 +19,7 @@
 		'-DUNIX=OFF '                 # 2020.05.11 per MABS # 2020.10.12 comment out
 		'-DBUILD_TESTS=OFF '
 		'-DUSE_CCACHE=OFF ' # 2020.05.11 per MABS 
-		#'-DSTRSAFE_NO_DEPRECATE=ON ' # 2020.08.21 per MABS
+		#'-DSTRSAFE_NO_DEPRECATE=ON ' # 2020.08.21 per MABS ... 2022.06.28 try turning on
 		'-DUSE_UNSAFE_C_GEN=ON ' # 2020.10.10 per MABS https://github.com/m-ab-s/media-autobuild_suite/commit/7034e948ca14323514fca98c83adc1ec7720909e
 	,
 	'env_exports' : { # 2019.12.13 add -D_POSIX_C_SOURCE
@@ -28,12 +28,14 @@
 		'CPPFLAGS' : ' -O3 -D_POSIX_C_SOURCE -DSTRSAFE_NO_DEPRECATE ', # 2020.04.07 attempted to add -D_POSIX_C_SOURCE # 2020.08.21 per MABS -DSTRSAFE_NO_DEPRECATE
 		'LDFLAGS'  : ' -O3 -D_POSIX_C_SOURCE -DSTRSAFE_NO_DEPRECATE ', # 2020.04.07 attempted to add -D_POSIX_C_SOURCE # 2020.08.21 per MABS -DSTRSAFE_NO_DEPRECATE
 	},
-	#'patches' : [
-	#	#('vulkan/0001-mingw-workarounds-deadsix27-2021.10.30.patch','-p1','..'),
-	#],
+	'patches' : [
+		('vulkan/vulkan-0001-cross-compile-static-linking-hacks-MABS-2022.06.28.patch','-p1','..'),
+		('vulkan/vulkan-0002-pc-remove-CMAKE_CXX_IMPLICIT_LINK_LIBRARIES-MABS-2022.06.28','-p1','..'),
+	],
 	'run_post_patch' : [ 
 		'sed -i.bak \'s/ pthread m)/ pthread m cfgmgr32)/g\' ../loader/CMakeLists.txt', # 2020.05.11 to align more with deadsix27
 		'sed -i.bak \'s/ -lshlwapi -lcfgmgr32"/ -lcfgmgr32 -lpthread -lm -lshlwapi -lglslang"/g\' ../loader/CMakeLists.txt', # 2020.05.11 to align more with deadsix27 # 2020.10.11 libglslang
+		'sed -i.bak "s/\@VULKAN_LIB_SUFFIX\@//" ../loader/vulkan.pc.in',
 	],
 	'regex_replace': {
 		'post_install': [
@@ -58,15 +60,18 @@
 	},
 	'run_post_install' : [ 
 		#'cat {pkg_config_path}/vulkan.pc',
-		'sed -i.bak "s;/Lib;/lib;g" "{pkg_config_path}/vulkan.pc"',
 		'sed -i.bak "s/-lvulkan-1/-lvulkan/g" "{pkg_config_path}/vulkan.pc"',
+		'sed -i.bak "s;/Lib;/lib;g" "{pkg_config_path}/vulkan.pc"',
 		#'cat {pkg_config_path}/vulkan.pc',		'ls -al {target_prefix}/lib/libvulkan*',
-		##'cp -fv "{target_prefix}/lib/libvulkan.dll.a" "{target_prefix}/lib/libvulkan.a"', # Hmmm ... 2020.10.11 STATIC LINKING NO LONGER POSSIBLE so do this ????
-		'cp -fv "{target_prefix}/lib/libvulkan-1.dll.a" "{target_prefix}/lib/libvulkan.a"', # Hmmm ... 2020.10.11 STATIC LINKING NO LONGER POSSIBLE so do this ????
-		'cp -fv "{target_prefix}/lib/libvulkan-1.dll.a" "{target_prefix}/lib/libvulkan-1.a"', # Hmmm ... 2020.10.11 STATIC LINKING NO LONGER POSSIBLE so do this ????
-		#'ls -al {target_prefix}/lib/libvulkan*',
+		#'cp -fv "{target_prefix}/lib/libvulkan.dll.a" "{target_prefix}/lib/libvulkan.a"',
+		'ls -al {target_prefix}/lib/libvulkan*',
+		'cp -fv "{target_prefix}/lib/libvulkan-1.dll.a" "{target_prefix}/lib/libvulkan.a"',
+		'cp -fv "{target_prefix}/lib/libvulkan-1.dll.a" "{target_prefix}/lib/libvulkan-1.a"',
+		'cp -fv "{target_prefix}/lib/libvulkan-1.dll.a" "{target_prefix}/lib/libvulkan-1.dll.a"',
+		'ls -al {target_prefix}/lib/libvulkan*',
 	],
-	'depends_on' : [ 'glslang', 'spirv-headers', 'spirv-tools', 'spirv-cross', 'shaderc', 'vulkan_headers', 'vulkan-d3dheaders', ], # 2020.10.11 libglslang
+	#'depends_on' : [ 'glslang', 'spirv-headers', 'spirv-tools', 'spirv-cross', 'shaderc', 'vulkan_headers', 'vulkan-d3dheaders', ], # 2020.10.11 libglslang
+	'depends_on' : [  'vulkan_headers', 'vulkan-d3dheaders', ], # MABS/DEADSIX27 VULKAN depends only on these
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'Vulkan Loader' },
 }
