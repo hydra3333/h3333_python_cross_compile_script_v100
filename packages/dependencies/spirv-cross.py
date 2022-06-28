9c9a49d7341d36f23150faf8a924ea2e351db003
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-06-28 20:29:36 +0930

    update spirv-cross

--- packages/dependencies/spirv-cross.py
+++ packages/dependencies/spirv-cross.py
@@ -50,16 +50,17 @@
 	'patches' : [
 		('spirv-cross/0001-add-a-basic-Meson-build-system-for-use-as-a-subproje-MABS-2022.06.28.patch', '-Np1', '..'),
 	],
-	'run_post_patch' : [
-		'ls -al "{target_prefix}/lib/pkgconfig/spirv-cross.pc"',
-		'rm -fv "{target_prefix}/lib/pkgconfig/spirv-cross.pc"',
-		'ls -al "{target_prefix}/lib/pkgconfig/spirv-cross.pc"',
-	],
+	#'run_post_patch' : [
+		#'ls -al "{target_prefix}/lib/pkgconfig/spirv-cross.pc"',
+		#'rm -fv "{target_prefix}/lib/pkgconfig/spirv-cross.pc"',
+		#'ls -al "{target_prefix}/lib/pkgconfig/spirv-cross.pc"',
+	#],
 	'run_post_install' : [
 		# 2022.06.28 the next line is to create a .pc ... if SHOULD not be needed any more ??? 
-		#"echo 'prefix={target_prefix}\nexec_prefix=${{prefix}}\nlibdir=${{exec_prefix}}/lib\nincludedir=${{prefix}}/include/spirv_cross\nName: spirv-cross-c-shared\nDescription: C API for SPIRV-Cross\nVersion:\nLibs: -L${{libdir}} -lspirv-cross-c -lspirv-cross-cpp -lspirv-cross-reflect -lspirv-cross-glsl -lspirv-cross-hlsl -lspirv-cross-msl -lspirv-cross-core -lstdc++\nCflags: -I${{includedir}}' > {target_prefix}/lib/pkgconfig/spirv-cross.pc",
-		'echo "It should have created a .pc file {target_prefix}/lib/pkgconfig/spirv-cross.pc" IF NOT then create it in run_post_install',
+		"echo 'prefix={target_prefix}\nexec_prefix=${{prefix}}\nlibdir=${{exec_prefix}}/lib\nincludedir=${{prefix}}/include/spirv_cross\nName: spirv-cross-c-shared\nDescription: C API for SPIRV-Cross\nVersion:\nLibs: -L${{libdir}} -lspirv-cross-c -lspirv-cross-cpp -lspirv-cross-reflect -lspirv-cross-glsl -lspirv-cross-hlsl -lspirv-cross-msl -lspirv-cross-core -lstdc++\nCflags: -I${{includedir}}' > {target_prefix}/lib/pkgconfig/spirv-cross.pc",
+		#'echo "It should have created a .pc file {target_prefix}/lib/pkgconfig/spirv-cross.pc" IF NOT then create it in run_post_install',
 		'ls -al "{target_prefix}/lib/pkgconfig/spirv-cross.pc"', # 2022.06.28 it should have created a .pc file
+		'cat "{target_prefix}/lib/pkgconfig/spirv-cross.pc"', # 2022.06.28 it should have created a .pc file
 	],
 	'run_post_patch' : [ 
 		'sed -i.bak "s/0.13.0/0.48.0/" ../meson.build',
