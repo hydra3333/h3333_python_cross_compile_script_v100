c9c366d51b6f6990a32a15dd5e9f8f319dbe4d51
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-06-29 20:12:27 +0930

    try updated shaderc

--- packages/dependencies/shaderc.py
+++ packages/dependencies/shaderc.py
@@ -11,10 +11,11 @@
 		'-DCMAKE_INSTALL_PREFIX={target_prefix} '
 		'-DSHADERC_SKIP_INSTALL=OFF '
 		'-DSHADERC_SKIP_TESTS=ON '
-		'-DSHADERC_ENABLE_SPVC=ON '
 		'-DSHADERC_SKIP_EXAMPLES=ON '
+		'-DSHADERC_ENABLE_SHARED_CRT=OFF '
+		'-DSHADERC_ENABLE_SPVC=ON '
 		'-DSHADERC_ENABLE_WERROR_COMPILE=OFF ' # 2021.11.01 per MABS
-		# 2022.06.26 the rest are from gyan
+		# 2022.06.26 
 		'-DSHADERC_SKIP_COPYRIGHT_CHECK=ON '
         '-DENABLE_EXCEPTIONS=ON '
 		'-DENABLE_CTEST=OFF '
@@ -22,6 +23,11 @@
 		'-DSPIRV_SKIP_EXECUTABLES=ON '
 		'-DSPIRV_TOOLS_BUILD_STATIC=ON '
 		'-DBUILD_SHARED_LIBS=OFF '
+		'-DSKIP_GLSLANG_INSTALL=N '
+		'-DSKIP_SPIRV_HEADERS_INSTALL=ON '
+		'-DSKIP_SPIRV_CROSS_INSTALL=ON '
+		'-DSKIP_SPIRV_TOOLS_INSTALL=ON '
+		'-DSKIP_GOOGLETEST_INSTALL=ON '
 	,
 	'source_subfolder' : '_build',
 	'conf_system' : 'cmake',
@@ -37,10 +43,10 @@
 		"sed -i 's/add_subdirectory(examples)/#add_subdirectory(examples)/g' ../CMakeLists.txt",
 		"sed -i 's/--check/#--check/g' ../CMakeLists.txt",
 		"sed -i 's/printed_count += 1/#printed_count += 1/g' ../utils/add_copyright.py", # 2021.02.27 grr since it fails with glslang, ignore errors
-        'if [ -f "{target_prefix}/lib/libshaderc.a" ] ; then rm -fv "{target_prefix}/lib/libshaderc.a" ; fi',
-        'if [ -f "{target_prefix}/lib/libshaderc_combined.a" ] ; then rm -fv "{target_prefix}/lib/libshaderc_combined.a" ; fi',
-        'if [ -d "{target_prefix}/include/shaderc" ] ; then rm -fvR "{target_prefix}/include/shaderc" ; fi',
-        'if [ -d "{target_prefix}/include/libshaderc_util" ] ; then rm -fvR "{target_prefix}/include/libshaderc_util" ; fi',
+		'if [ -f "{target_prefix}/lib/libshaderc.a" ] ; then rm -fv "{target_prefix}/lib/libshaderc.a" ; fi',
+		'if [ -f "{target_prefix}/lib/libshaderc_combined.a" ] ; then rm -fv "{target_prefix}/lib/libshaderc_combined.a" ; fi',
+		'if [ -d "{target_prefix}/include/shaderc" ] ; then rm -fvR "{target_prefix}/include/shaderc" ; fi',
+		'if [ -d "{target_prefix}/include/libshaderc_util" ] ; then rm -fvR "{target_prefix}/include/libshaderc_util" ; fi',
 	],
 	'regex_replace': {
 		'post_patch': [
@@ -56,7 +62,11 @@
 		'cp -frv "libshaderc/libshaderc_combined.a" "{target_prefix}/lib/libshaderc_combined.a"',
 		'cp -frv "libshaderc/libshaderc.a" "{target_prefix}/lib/libshaderc.a"',
 	],
-	#'depends_on' : ['glslang', 'spirv-headers', 'spirv-cross', 'spirv-tools', ],
+	#
+	'patches' : [
+		('shaderc/0001-third_party-set-INSTALL-variables-as-cache-MABS-2022.06.28.patch', '-Np1', '..'),
+		('shaderc/0002-shaderc_util-add-install-MABS-2022.06.28.patch',' -Np1', '..'),
+	],
 	'depends_on' : [ 'spirv-headers', 'spirv-cross', 'spirv-tools', 'glslang', ],
 
 	'update_check' : { 'type' : 'git', },
