ab398c60c04e530bf6507e43f6e72f1c067e2494
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-06-30 16:43:29 +0930

    save pre-likeMABS stuff

---
+++ packages/dependencies/spirv-headers_pre_likeMABS.py
@@ -0,0 +1,13 @@
+{
+	'repo_type' : 'git',
+	'rename_folder' : 'spirv-headers',
+	'url' : 'https://github.com/KhronosGroup/SPIRV-Headers.git',
+	'depth_git': 0,
+	#'branch': '204cd131c42b90d129073719f2766293ce35c081', # 2020.03.19 comment out
+	'needs_make' : False,
+	'needs_make_install' : False,
+	'needs_configure' : False,
+	'recursive_git' : True,
+	'update_check' : { 'type' : 'git', },
+	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'SPIRV Headers' },
+}
