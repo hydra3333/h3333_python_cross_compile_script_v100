b8aecceaa48e2770f663d791e79a5a4014cb72c6
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-07-01 16:53:28 +0930

    Add files via upload

--- packages/variables.py
+++ packages/variables.py
@@ -82,7 +82,7 @@
 		'--enable-amf '
 		'--enable-opencl ' # 2019.12.13, added it, not sure why it wasn't in any of the configs ? depends on opencl_non_icd or opencl_icd
 		'--enable-opengl '
-		'--enable-vulkan --enable-libshaderc --enable-libglslang '
+		'--enable-vulkan --enable-libshaderc ' # --enable-libglslang # ERROR: libshaderc and libglslang are mutually exclusive, if in doubt, disable libglslang
 			'--enable-filter=scale_vulkan '
 			'--enable-filter=avgblur_vulkan '
 			'--enable-filter=chromaber_vulkan '
