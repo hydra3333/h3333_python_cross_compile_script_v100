a156a61d5885fe5be3787614906de11a7e99be98
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-07-10 11:35:07 +0930

    Add files via upload

--- Reset.sh
+++ Reset.sh
@@ -54,8 +54,10 @@ rsync -rvtI ./_ref/ ./_working
 sudo chmod -R a=rwx ./_working/*.sh
 sudo chmod -R a=rwx ./_working/*.py
 sudo chmod -R a=rwx ./_working
-sudo cp -fv ./_ref/*.sh ~/Desktop/
-sudo chmod -R a=rwx ./Desktop/*.sh
+#
+#sudo cp -fv ./_ref/*.sh ~/Desktop/
+#sudo chmod -R a=rwx ./Desktop/*.sh
+#
 cd ./_working
 rm -fv *.yaml
 #git fetch origin
@@ -78,5 +80,9 @@ rm -frv ./exe_x64_py/* 2>&1 | tee -a ./exe.log
 mkdir -pv ./exe_x64_py 2>&1 | tee -a ./exe.log
 
 ./Remove_ffmpeg_3333_related_files.sh
+
+echo "NOW do these:"
+echo "sudo cp -fv ~/Desktop/_ref/*.sh ~/Desktop/"
+echo "sudo chmod -R a=rwx ./Desktop/*.sh"
 
 exit
