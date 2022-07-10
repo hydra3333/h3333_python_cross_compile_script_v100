eef3076b066dad0dc3abd54ee0044abb36223f95
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-07-10 11:36:19 +0930

    Add files via upload

--- Reset.sh
+++ Reset.sh
@@ -84,5 +84,7 @@ mkdir -pv ./exe_x64_py 2>&1 | tee -a ./exe.log
 echo "NOW do these:"
 echo "sudo cp -fv ~/Desktop/_ref/*.sh ~/Desktop/"
 echo "sudo chmod -R a=rwx ./Desktop/*.sh"
+echo "sudo cp -fv ~/Desktop/_ref/*.sh ~/Desktop/_working/"
+echo "sudo chmod -R a=rwx ./Desktop/_working/*.sh"
 
 exit
