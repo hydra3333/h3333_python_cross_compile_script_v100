59fde52b31550b69e30fcc9db23b4782161102b9
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-07-25 18:55:32 +0930

    Add files via upload

--- packages/dependencies/libarchive.py
+++ packages/dependencies/libarchive.py
@@ -1,7 +1,8 @@
 {
 	'repo_type' : 'git',
 	'url' : 'https://github.com/libarchive/libarchive.git',
-    #'branch' : 'tags/v3.5.2',
+    #'depth_git': : 0,
+    #'branch' : 'tags/v3.6.1',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=0 -DCMAKE_BUILD_TYPE=Release '
