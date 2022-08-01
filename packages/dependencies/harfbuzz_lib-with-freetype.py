8d87b685a8440027d98171f2fae6df10803e621f
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2022-08-01 15:24:31 +0930

    harfbuzz to 5.1.0

--- packages/dependencies/harfbuzz_lib-with-freetype.py
+++ packages/dependencies/harfbuzz_lib-with-freetype.py
@@ -1,11 +1,6 @@
 { # https://fossies.org/linux/misc/
 	'repo_type' : 'archive',
 	'download_locations' : [
-		#{ 'url' : 'https://github.com/harfbuzz/harfbuzz/releases/download/2.9.0/harfbuzz-2.9.0.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3e1c2e1d2c65d56364fd16d1c41a06b2a35795496f78dfff635c2b7414b54c5a' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
-		#{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-2.9.0.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3e1c2e1d2c65d56364fd16d1c41a06b2a35795496f78dfff635c2b7414b54c5a' }, ], }, # https://fossies.org/linux/misc/
-		# 2022.05.06 hmm, 2.9.0 to 4.2.1 is a big jump, it may not work.
-		#{ 'url' : 'https://github.com/harfbuzz/harfbuzz/releases/download/4.2.1/harfbuzz-4.2.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'bd17916513829aeff961359a5ccebba6de2f4bf37a91faee3ac29c120e3d7ee1' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
-		#{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-4.2.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'bd17916513829aeff961359a5ccebba6de2f4bf37a91faee3ac29c120e3d7ee1' }, ], }, # https://fossies.org/linux/misc/
 		{ 'url' : 'https://github.com/harfbuzz/harfbuzz/releases/download/4.4.1/harfbuzz-4.4.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'c5bc33ac099b2e52f01d27cde21cee4281b9d5bfec7684135e268512478bc9ee' }, ], }, # https://github.com/harfbuzz/harfbuzz/releases
 		{ 'url' : 'https://fossies.org/linux/misc/harfbuzz-4.4.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'c5bc33ac099b2e52f01d27cde21cee4281b9d5bfec7684135e268512478bc9ee' }, ], }, # https://fossies.org/linux/misc/
 	],
@@ -30,6 +25,5 @@
 	],
 	'update_check' : { 'url' : 'https://github.com/harfbuzz/harfbuzz/releases', 'type' : 'githubreleases', 'name_or_tag' : 'name' },
 	#'update_check' : { 'url' : 'https://www.freedesktop.org/software/harfbuzz/release/?C=M;O=D', 'type' : 'httpindex', 'regex' : r'harfbuzz-(?P<version_num>[\d.]+)\.tar\.xz' },
-	#'_info' : { 'version' : '2.9.0', 'fancy_name' : 'harfbuzz (with freetype2)' },
 	'_info' : { 'version' : '4.4.1', 'fancy_name' : 'harfbuzz (with freetype2)' },
 }
