3b400051083ccfe4cd33c0067a409c52dd70f459
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-01-11 17:06:52 +1030

    fix mujs

--- packages/dependencies/mujs.py
+++ packages/dependencies/mujs.py
@@ -1,6 +1,9 @@
 {
 	'repo_type' : 'git',
-	'url' : 'https://github.com/ArtifexSoftware/mujs.git', # this seems to be as up to date as #'url' : 'git://git.ghostscript.com/mujs.git',
+	'url' : 'https://github.com/ArtifexSoftware/mujs.git', # this seems to be as up to date as #'url' : http://git.ghostscript.com/mujs.git
+	'recursive_git' : True,
+	'depth_git' : 0,
+	'branch' : 'bb6a85a31c46f82577bacc1cc21d3c3b9df02b82', # 2023.01.11 see https://bugs.ghostscript.com/show_bug.cgi?id=706322
 	'needs_configure' : False,
 	'build_options' : '{make_prefix_options} prefix={target_prefix} HAVE_READLINE=no',
 	'install_options' : '{make_prefix_options} prefix={target_prefix} HAVE_READLINE=no',
