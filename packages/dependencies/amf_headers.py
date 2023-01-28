0b4bb0d68125c61fd327020495a801890754d52a
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-01-28 17:50:51 +1030

    amf_headers, swap to original source
    deadsiz27's source is well out of date

--- packages/dependencies/amf_headers.py
+++ packages/dependencies/amf_headers.py
@@ -1,16 +1,21 @@
 {
 	'repo_type' : 'git',
-	'url' : 'https://github.com/DeadSix27/AMF',
-	'rename_folder' : 'amd_media_framework_headers',
+	'url' : 'https://github.com/GPUOpen-LibrariesAndSDKs/AMF',	# 2023.01.28, use this, deaqdsix27's source is well out of date
+	'rename_folder' : 'AMF_headers',
 	'depth_git': 1,
 	'needs_configure' : False,
 	'needs_make' : False,
 	'needs_make_install' : False,
 	'run_post_regexreplace' : [
-		'if [ ! -f "already_done" ] ; then if [ ! -d "{target_prefix}/include/AMF" ]; then mkdir -p "{target_prefix}/include/AMF" ; fi ; fi',
-		'if [ ! -f "already_done" ] ; then pwd ; fi',
-		'if [ ! -f "already_done" ] ; then cp -av "amf/public/include/." "{target_prefix}/include/AMF" ; fi',
-		'if [ ! -f "already_done" ] ; then touch  "already_done" ; fi',
+		'if [ ! -d "{target_prefix}/include/AMF" ]; then mkdir -p "{target_prefix}/include/AMF" ; fi',
+		'pwd',
+		'!SWITCHDIR|amf/public/include',
+		'pwd',
+		# use install, per MABS
+		'install -D -p -v -t "{target_prefix}/include/AMF/core" core/*.h',
+		'install -D -p -v -t "{target_prefix}/include/AMF/components" components/*.h',
+		'!SWITCHDIR|../../..',
+		'pwd',
 	],
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'AMF (headers)' },
