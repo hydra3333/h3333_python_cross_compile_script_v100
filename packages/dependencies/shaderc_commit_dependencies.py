a0e6554c1c30a118d551b19c522505cfbb63c2ae
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-02-04 16:52:11 +1030

    place shaderc_commit_dependencies ids in files
    		#--------------------------------------
    		#
    		# To pass a variable to parent process:
    		#	child process:
    		#		echo ${variable} >/tmp/file
    		#	parent process:
    		#		read variable </tmp/file
    		#--------------------------------------

--- packages/dependencies/shaderc_commit_dependencies.py
+++ packages/dependencies/shaderc_commit_dependencies.py
@@ -18,26 +18,38 @@
 		'wget https://raw.githubusercontent.com/google/shaderc/main/DEPS',
 		'cat ./DEPS',
 		#
-		'if [ -f "./glslang_revision.txt" ] ; then rm -fv ./glslang_revision.txt ; fi',
-		'grep "\'glslang_revision\':" ./DEPS >> ./glslang_revision.txt',
-		'sed -i "s/  \'glslang_revision\': \'/export glslang_revision=/g" ./glslang_revision.txt',
-		'sed -i "s/\',//g" ./glslang_revision.txt',
+		'if [ -f "./glslang_revision.txt" ] ; then rm -f ./glslang_revision.txt ; fi',
+		'if [ -f "./glslang_revision.commit" ] ; then rm -f ./glslang_revision.commit ; fi',
+		'grep "\'glslang_revision\':" ./DEPS >> ./glslang_revision.commit',
+		'sed -i "s/  \'glslang_revision\': \'//g" ./glslang_revision.commit',
+		'sed -i "s/\',//g" ./glslang_revision.commit',
+		'cp -fv ./glslang_revision.commit ./glslang_revision.txt',
+		'sed -i "s/^/export glslang_revision=/" ./glslang_revision.txt',
 		'chmod +x ./glslang_revision.txt',
-		#'cat ./glslang_revision.txt',
+		'chmod +x ./glslang_revision.commit',
+		'cat ./glslang_revision.commit ; cat ./glslang_revision.txt',
 		#
-		'if [ -f "./spirv_headers_revision.txt" ] ; then rm -fv ./spirv_headers_revision.txt ; fi',
-		'grep "\'spirv_headers_revision\':" ./DEPS >> ./spirv_headers_revision.txt',
-		'sed -i "s/  \'spirv_headers_revision\': \'/export spirv_headers_revision=/g" ./spirv_headers_revision.txt',
-		'sed -i "s/\',//g" ./spirv_headers_revision.txt',
+		'if [ -f "./spirv_headers_revision.txt" ] ; then rm -f ./spirv_headers_revision.txt ; fi',
+		'if [ -f "./spirv_headers_revision.commit" ] ; then rm -f ./spirv_headers_revision.commit ; fi',
+		'grep "\'spirv_headers_revision\':" ./DEPS >> ./spirv_headers_revision.commit',
+		'sed -i "s/  \'spirv_headers_revision\': \'//g" ./spirv_headers_revision.commit',
+		'sed -i "s/\',//g" ./spirv_headers_revision.commit',
+		'cp -fv ./spirv_headers_revision.commit ./spirv_headers_revision.txt',
+		'sed -i "s/^/export spirv_headers_revision=/" ./spirv_headers_revision.txt',
 		'chmod +x ./spirv_headers_revision.txt',
-		'cat ./spirv_headers_revision.txt',
-		##
-		'if [ -f "./spirv_tools_revision.txt" ] ; then rm -fv ./spirv_tools_revision.txt ; fi',
-		'grep "\'spirv_tools_revision\':" ./DEPS >> ./spirv_tools_revision.txt',
-		'sed -i "s/  \'spirv_tools_revision\': \'/export spirv_tools_revision=/g" ./spirv_tools_revision.txt',
-		'sed -i "s/\',//g" ./spirv_tools_revision.txt',
+		'chmod +x ./spirv_headers_revision.commit',
+		'cat ./spirv_headers_revision.commit ; cat ./spirv_headers_revision.txt',
+		#
+		'if [ -f "./spirv_tools_revision.txt" ] ; then rm -f ./spirv_tools_revision.txt ; fi',
+		'if [ -f "./spirv_tools_revision.commit" ] ; then rm -f ./spirv_tools_revision.commit ; fi',
+		'grep "\'spirv_tools_revision\':" ./DEPS >> ./spirv_tools_revision.commit',
+		'sed -i "s/  \'spirv_tools_revision\': \'//g" ./spirv_tools_revision.commit',
+		'sed -i "s/\',//g" ./spirv_tools_revision.commit',
+		'cp -fv ./spirv_tools_revision.commit ./spirv_tools_revision.txt',
+		'sed -i "s/^/export spirv_tools_revision=/" ./spirv_tools_revision.txt',
 		'chmod +x ./spirv_tools_revision.txt',
-		#'cat ./spirv_tools_revision.txt',
+		'chmod +x ./spirv_tools_revision.commit',
+		'cat ./spirv_tools_revision.commit ; cat ./spirv_tools_revision.txt',
 		#
 		#'cat ./glslang_revision.txt',
 		#'!CMD(cat ./glslang_revision.txt)CMD! ; export',
@@ -47,20 +59,20 @@
 		#'!CMD(cat ./spirv_tools_revision.txt)CMD! ; export',
 		#'printenv',
 		#
-		'cat ../shaderc_commit_dependencies/glslang_revision.txt',
-		'cat ../shaderc_commit_dependencies/spirv_headers_revision.txt',
-		'cat ../shaderc_commit_dependencies/spirv_tools_revision.txt',
-		#
 		'if [ -f "already_done" ] ; then rm -fv  "already_done" ; fi',
 		'touch  "already_done"',
 		'!SWITCHDIR|..',
 		'!SWITCHDIR|..',
 		'pwd',
+		#--------------------------------------
+		#--------------------------------------
+		#
+		# To pass a variable to parent process:
+		#	child process:
+		#		echo ${variable} >/tmp/file
+		#	parent process:
+		#		read variable </tmp/file
+		#--------------------------------------
+		#--------------------------------------
 	],
-	#
-	#'env_exports' : {
-	#	'glslang_revision': '!CMD(cat ./glslang_revision.txt)CMD!',
-	#	'spirv_tools_revision': '!CMD(cat ./spirv_tools_revision.txt)CMD!',
-	#	'spirv_headers_revision': '!CMD(cat ./spirv_headers_revision.txt)CMD!',
-	#}
 }
