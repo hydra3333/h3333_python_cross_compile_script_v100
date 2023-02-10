27c4d3563c367273bd3e21ba870a3d58f72da510
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-02-10 23:10:34 +1030

    Delete packages/products/_disabled directory

--- packages/products/_disabled/gdb.py
+++
@@ -1,6 +0,0 @@
-{
-	'repo_type' : 'git',
-	'url' : 'git://sourceware.org/git/binutils-gdb.git',
-	'configure_options': '--host={target_host} --enable-static --enable-lto --prefix={product_prefix}/gdb_git.installed',
-	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'GDB' },
-}
