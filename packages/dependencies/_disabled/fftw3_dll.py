70e828ce6d172681bf8221c8194a75982adcd8c5
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-02-10 23:10:10 +1030

    Delete packages/dependencies/_disabled directory

--- packages/dependencies/_disabled/fftw3_dll.py
+++
@@ -1,7 +0,0 @@
-{
-	'is_dep_inheriter' : True,
-	'depends_on' : [
-		'fftw3_dll_single', 'fftw3_dll_double', 'fftw3_dll_ldouble', 'fftw3_dll_quad', 
-	],
-	'_info' : { 'version' : '3.3.8', 'fancy_name' : 'fftw3 shared DLLs' },
-}
--- packages/dependencies/_disabled/libtheora.py
+++
@@ -1,9 +0,0 @@
-{
-	'repo_type' : 'git',
-	'url' : 'https://github.com/xiph/theora.git',
-	'patches' : [
-		('theora/theora_remove_rint_1.2.0alpha1.patch', '-p1'),
-	],
-	'configure_options' : '{autoconf_prefix_options} --disable-doc --disable-spec --disable-oggtest --disable-vorbistest --disable-examples',
-	'_info' : { 'version' : None, 'fancy_name' : 'theora' },
-}
