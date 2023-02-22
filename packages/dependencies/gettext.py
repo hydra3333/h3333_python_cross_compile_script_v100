c6417b207d687726ec9b5156641911201d6e98a8
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-02-23 08:25:02 +1030

    Add files via upload

--- packages/dependencies/gettext.py
+++ packages/dependencies/gettext.py
@@ -1,9 +1,8 @@
 {
 	'repo_type' : 'archive',
 	'download_locations' : [
-		#{ 'url' : 'https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'd20fcbb537e02dcf1383197ba05bd0734ef7bf5db06bdb241eb69b7d16b73192' }, ], },
-		#{ 'url' : 'https://fossies.org/linux/misc/gettext-0.21.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : 'd20fcbb537e02dcf1383197ba05bd0734ef7bf5db06bdb241eb69b7d16b73192' }, ], },
-		# 0.21.1 fails to build ?  # 2022.12.18 per DEADSIX27 retry it
+		#{ 'url' : 'https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '50dbc8f39797950aa2c98e939947c527e5ac9ebd2c1b99dd7b06ba33a6767ae6' }, ], },
+		#{ 'url' : 'https://fossies.org/linux/misc/gettext-0.21.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '50dbc8f39797950aa2c98e939947c527e5ac9ebd2c1b99dd7b06ba33a6767ae6' }, ], },
 		{ 'url' : 'https://ftp.gnu.org/pub/gnu/gettext/gettext-0.21.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '50dbc8f39797950aa2c98e939947c527e5ac9ebd2c1b99dd7b06ba33a6767ae6' }, ], },
 		{ 'url' : 'https://fossies.org/linux/misc/gettext-0.21.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '50dbc8f39797950aa2c98e939947c527e5ac9ebd2c1b99dd7b06ba33a6767ae6' }, ], },
 	],
@@ -29,5 +28,5 @@
 	#],	
 	'depends_on' : [ 'iconv' ],
 	'update_check' : { 'url' : 'https://ftp.gnu.org/pub/gnu/gettext/?C=M;O=D', 'type' : 'httpindex', 'regex' : r'gettext-(?P<version_num>[\d.]+)\.tar\.xz' },
-	'_info' : { 'version' : '0.21', 'fancy_name' : 'gettext' },
+	'_info' : { 'version' : '0.21.1', 'fancy_name' : 'gettext' },
 }
