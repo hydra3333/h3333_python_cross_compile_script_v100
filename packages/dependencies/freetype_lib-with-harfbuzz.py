6ef1c895ca4cc74e0947264cc2796ff6647ee440
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-02-23 08:23:27 +1030

    update freetype version

--- packages/dependencies/freetype_lib-with-harfbuzz.py
+++ packages/dependencies/freetype_lib-with-harfbuzz.py
@@ -1,14 +1,13 @@
 { # http://git.savannah.gnu.org/cgit/freetype/freetype2.git/
 	'repo_type' : 'archive',
 	'download_locations' : [
-		# freetype 2.11.0 kills mp4box building, so keep freetype 2.10.4 ? mp4box fails anyway so just keep going
-		#{ 'url' : 'https://download.savannah.gnu.org/releases/freetype/freetype-2.11.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3333ae7cfda88429c97a7ae63b7d01ab398076c3b67182e960e5684050f2c5c8' }, ], },
-		#{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.11.1/freetype-2.11.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3333ae7cfda88429c97a7ae63b7d01ab398076c3b67182e960e5684050f2c5c8' }, ], },
-		#{ 'url' : 'https://fossies.org/linux/misc/freetype-2.11.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '3333ae7cfda88429c97a7ae63b7d01ab398076c3b67182e960e5684050f2c5c8' }, ], },
 		# HUH no: freetype 2.12.1 fails to build, with lots of link errors about duplicate definitions
-		{ 'url' : 'https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f' }, ], },
-		{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.12.1/freetype-2.12.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f' }, ], },
-		{ 'url' : 'https://fossies.org/linux/misc/freetype-2.12.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f' }, ], },
+		#{ 'url' : 'https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f' }, ], },
+		#{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.12.1/freetype-2.12.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f' }, ], },
+		#{ 'url' : 'https://fossies.org/linux/misc/freetype-2.12.1.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '4766f20157cc4cf0cd292f80bf917f92d1c439b243ac3018debf6b9140c41a7f' }, ], },
+		{ 'url' : 'https://download.savannah.gnu.org/releases/freetype/freetype-2.13.0.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '5ee23abd047636c24b2d43c6625dcafc66661d1aca64dec9e0d05df29592624c' }, ], },
+		{ 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/2.13.0/freetype-2.13.0.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '5ee23abd047636c24b2d43c6625dcafc66661d1aca64dec9e0d05df29592624c' }, ], },
+		{ 'url' : 'https://fossies.org/linux/misc/freetype-2.13.0.tar.xz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '5ee23abd047636c24b2d43c6625dcafc66661d1aca64dec9e0d05df29592624c' }, ], },
 	],
 	'folder_name' : 'freetype-with-harfbuzz',
 	'rename_folder' : 'freetype-with-harfbuzz',
@@ -51,5 +50,5 @@
 	],
 	#'update_check' : { 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/', 'type' : 'sourceforge', 'regex' : r'(?P<version_num>[\d.]+)'},
     'update_check' : { 'url' : 'https://sourceforge.net/projects/freetype/files/freetype2/', 'type' : 'sourceforge', },
-	'_info' : { 'version' : '2.12.1', 'fancy_name' : 'freetype2-lib-with_harfbuzz' },
+	'_info' : { 'version' : '2.13.0', 'fancy_name' : 'freetype2-lib-with_harfbuzz' },
 }
