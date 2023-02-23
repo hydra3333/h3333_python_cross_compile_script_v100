c5677d48910f10010615f92379b0f9c7b6d576f7
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-02-23 15:14:55 +1030

    Add files via upload

--- packages/dependencies/libaacs.py
+++ packages/dependencies/libaacs.py
@@ -5,6 +5,7 @@
 	'url' : 'https://code.videolan.org/videolan/libaacs.git',
 	'depth_git' : 0, # 2019.12.13 otherwise too shallow for the specfified commit to fetch
 	#'branch' : '9bceea3f0a022010aa15e898bbb1d47f2af45052', # 2020.03.19 try latest GIT instead of this branch # f263376b1e6570556031f420b9df08373e346d76 works for combo libaacs/libgcrypt/libgpg_error
+	'rename_folder' : 'libaacs_git',
 	'configure_options': '--host={target_host} --prefix={target_prefix} --with-libgcrypt-prefix={target_prefix} --with-gpg-error-prefix={target_prefix} --disable-shared --enable-static',
 	'run_post_regexreplace' : (
 		'autoreconf -fiv',
