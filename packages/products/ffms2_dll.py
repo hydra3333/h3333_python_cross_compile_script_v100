305df06a7e7c328a69cc0371fc72bd7fe794ea2d
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-03-07 06:00:00 +1030

    Add files via upload

--- packages/products/ffms2_dll.py
+++ packages/products/ffms2_dll.py
@@ -3,12 +3,6 @@
 	'url' : 'https://github.com/FFMS/ffms2', 
 	'depth_git' : 0,
 	'rename_folder' : 'ffms2_dll',
-	'run_post_regexreplace' : [
-		'rm -fv ./configure',
-		'if [ ! -d "src/config" ]; then mkdir -p src/config ; fi',
-		'autoreconf -fiv',
-		'./configure --help=recursive',
-	],
 	'configure_options': '--host={target_host} --prefix={output_prefix}/ffms2_dll.installed --enable-static -disable-shared --with-zlib={target_prefix}/lib',
 	'env_exports' : {
 		'CXXFLAGS' :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp ',
@@ -16,6 +10,16 @@
 		'CFLAGS'   :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp ',
 		'LDFLAGS'  :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp ',
 	},
+	'patches' : [
+		( 'ffms2/0001-ffmsindex-fix-linking-issues.patch', '-Np1' ),
+	],
+	'run_post_patch' : [ 
+		'sed -i "s/Libs.private.*/& -lstdc++/;s/Cflags.*/& -DFFMS_STATIC/" "ffms2.pc.in"',
+		'if [ -f "./configure" ] ; then rm -fv ./configure ; fi',
+		'if [ ! -d "src/config" ] ; then mkdir -p "src/config" ; fi',
+		'autoreconf -fiv',
+		'./configure --help=recursive',
+	],
 	'depends_on': [
 		'libzimg', 'bzip2', 'libffmpeg_extra',
 	],
