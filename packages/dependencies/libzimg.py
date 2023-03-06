3f96bd97d9d2722e6d133bbd0696aae85435a0f2
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-03-06 20:55:53 +1030

    zimg reinstate mabs patch

--- packages/dependencies/libzimg.py
+++ packages/dependencies/libzimg.py
@@ -6,10 +6,10 @@
 		'git submodule update --init --recursive --remote ', # 2022.12.18 per DEADSIX27 try to add --remote
 		'autoreconf -fiv', # 2020.11.07 from MABS 
 	],
+	'patches' : [
+		('zimg/0001-libm_wrapper-define-__CRT__NO_INLINE-before-math.h-from-MABS.patch', '-Np1' ), # 2020.11.07 from MABS per https://github.com/m-ab-s/media-autobuild_suite/commit/e57877f87abf1a07b79284de257e3cad6bbf7409#diff-adfad29200df8b40f03fd8670a191195ff4f98c0160a7b12ada8caa8cb824c75
+	],
 	# was pre-2023.03.06:
-	#'patches' : [
-	#	('zimg/0001-libm_wrapper-define-__CRT__NO_INLINE-before-math.h-from-MABS.patch', '-Np1' ), # 2020.11.07 from MABS per https://github.com/m-ab-s/media-autobuild_suite/commit/e57877f87abf1a07b79284de257e3cad6bbf7409#diff-adfad29200df8b40f03fd8670a191195ff4f98c0160a7b12ada8caa8cb824c75
-	#],
 	#'run_post_regexreplace' : [
 	#	'git submodule update --init --recursive --remote ',
 	#	#'sh ./autogen.sh',
