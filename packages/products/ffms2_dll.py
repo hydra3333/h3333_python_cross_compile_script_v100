d58415781fc96b89e8e85b33c13fcb14c9b44e7c
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-03-06 13:35:47 +1030

    Add files via upload

---
+++ packages/products/ffms2_dll.py
@@ -0,0 +1,19 @@
+{
+	'repo_type' : 'git',
+	'url' : 'https://github.com/FFMS/ffms2, 
+	'depth_git' : 0,
+	'rename_folder' : 'ffms2_dll',
+	'run_post_regexreplace' : [
+		'rm -fv ./configure',
+		'if [ ! -d "src/config" ]; then mkdir -p src/config ; fi',
+		'autoreconf -fiv',
+		'./configure --help',
+	],
+	'configure_options': '--host={target_host} --prefix={output_prefix}/ffms2_dll.installed --enable-shared ',
+	#'custom_cflag' : '',
+	'depends_on': [
+		'libffmpeg_extra',
+	],
+	'update_check' : { 'type' : 'git', },
+	'_info' : { 'version' : 'git master', 'fancy_name' : 'ffms2_dll' },
+}
