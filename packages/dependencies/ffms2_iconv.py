3d1f21e8a9413b696e568b26bce0aa5a73e467e7
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-03-07 21:31:35 +1030

    Add files via upload

--- packages/dependencies/ffms2_iconv.py
+++ packages/dependencies/ffms2_iconv.py
@@ -6,6 +6,9 @@
 		{ 'url' : 'https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.17.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '8f74213b56238c85a50a5329f77e06198771e70dd9a739779f4c02f65d971313' }, ], },
 		{ 'url' : 'https://fossies.org/linux/misc/libiconv-1.17.tar.gz', 'hashes' : [ { 'type' : 'sha256', 'sum' : '8f74213b56238c85a50a5329f77e06198771e70dd9a739779f4c02f65d971313' }, ], },
 	],
+	'run_post_patch' : [
+		'./configure --help',
+	],
 	## {autoconf_prefix_options} = --with-sysroot="{target_sub_prefix}" --host="{target_host}" --prefix="{output_prefix}/ffms2_dll.installed" --enable-shared --disable-static
 	'configure_options' : '--with-sysroot="{target_sub_prefix}" --host="{target_host}" --prefix="{output_prefix}/ffms2_dll.installed" --enable-shared --disable-static --disable-nls --enable-extra-encodings',
 	'update_check' : { 'url' : 'https://ftp.gnu.org/pub/gnu/libiconv/?C=M;O=D', 'type' : 'httpindex', 'regex' : r'libiconv-(?P<version_num>[\d.]+)\.tar\.gz' },
