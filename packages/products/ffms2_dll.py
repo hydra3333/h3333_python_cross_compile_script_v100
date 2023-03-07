abf03907465a3aaf3367921079d3b865bccc2b9a
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-03-07 15:21:51 +1030

    Add files via upload

--- packages/products/ffms2_dll.py
+++ packages/products/ffms2_dll.py
@@ -10,18 +10,29 @@
 		'CFLAGS'   :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp ',
 		'LDFLAGS'  :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp ',
 	},
+	'run_pre_patch' : [
+		'cp -fv "Makefile.am" "Makefile.am.orig"',
+		'cp -fv "configure.ac" "configure.ac.orig"',
+	]
 	'patches' : [
 		( 'ffms2/0001-ffmsindex-fix-linking-issues.patch', '-Np1' ),
 	],
 	'run_post_patch' : [ 
+		# diff for the patch
+		'diff -U 10 "Makefile.am.orig" "Makefile.am"  && echo "NO difference" || echo "YES differences!"',
+		'diff -U 10 "configure.ac.orig" "configure.ac"  && echo "NO difference" || echo "YES differences!"',
+		# diff for the sed
+		'cp -fv "ffms2.pc.in" "ffms2.pc.in.orig"',
 		'sed -i "s/Libs.private.*/& -lstdc++/;s/Cflags.*/& -DFFMS_STATIC/" "ffms2.pc.in"',
+		'diff -U 10 "ffms2.pc.in.orig" "ffms2.pc.in"  && echo "NO difference" || echo "YES differences!"',
+		#
 		'if [ -f "./configure" ] ; then rm -fv ./configure ; fi',
 		'if [ ! -d "src/config" ] ; then mkdir -p "src/config" ; fi',
 		'autoreconf -fiv',
 		'./configure --help=recursive',
 	],
 	'depends_on': [
-		'libzimg', 'bzip2', 'libffmpeg_extra',
+		'libzimg', 'bzip2','libffmpeg_extra',
 	],
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git master', 'fancy_name' : 'ffms2_dll' },
