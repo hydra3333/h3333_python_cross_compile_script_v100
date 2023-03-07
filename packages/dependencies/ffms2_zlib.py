f3120899d87443f2a380067dda770a03b17d938d
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-03-07 22:11:29 +1030

    Add files via upload

--- packages/dependencies/ffms2_zlib.py
+++ packages/dependencies/ffms2_zlib.py
@@ -2,6 +2,7 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/madler/zlib.git',
 	'conf_system' : 'cmake',
+	'folder_name' : 'ffms2_zlib',
 	'source_subfolder' : '_build',
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DINSTALL_PKGCONFIG_DIR="{target_prefix}/lib/pkgconfig" '
 							'-DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release '
