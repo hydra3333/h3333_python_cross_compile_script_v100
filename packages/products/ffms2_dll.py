873a9cc1cf3b6732d1a3b76c0a790dde8beb697c
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-03-08 00:14:15 +1030

    Add files via upload

--- packages/products/ffms2_dll.py
+++ packages/products/ffms2_dll.py
@@ -4,12 +4,17 @@
 	'depth_git' : 0,
 	'rename_folder' : 'ffms2_dll',
 	'env_exports' : {
-		'CXXFLAGS' :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp ',
-		'CPPFLAGS' :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp ',
-		'CFLAGS'   :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp ',
-		'LDFLAGS'  :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp -Wl,-Bsymbolic ', # to mitigate lock per https://github.com/ffms/ffms2/issues/90
+		#'CXXFLAGS' :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp ',
+		#'CPPFLAGS' :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp ',
+		#'CFLAGS'   :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp ',
+		#'LDFLAGS'  :  ' {original_stack_protector_trim} -I{target_prefix}/include -L{target_prefix}/lib -lintl -liconv -lssp -Wl,-Bsymbolic ', # to mitigate lock per https://github.com/ffms/ffms2/issues/90
+		'CXXFLAGS' :  ' {original_stack_protector_trim} -I{output_prefix}/include -L{output_prefix}/lib -lintl -liconv -lssp ',
+		'CPPFLAGS' :  ' {original_stack_protector_trim} -I{output_prefix}/include -L{output_prefix}/lib -lintl -liconv -lssp ',
+		'CFLAGS'   :  ' {original_stack_protector_trim} -I{output_prefix}/include -L{output_prefix}/lib -lintl -liconv -lssp ',
+		'LDFLAGS'  :  ' {original_stack_protector_trim} -I{output_prefix}/include -L{output_prefix}/lib -lintl -liconv -lssp -Wl,-Bsymbolic ', # to mitigate lock per https://github.com/ffms/ffms2/issues/90
 	},
-	'configure_options': '--host={target_host} --prefix={output_prefix}/ffms2_dll.installed --enable-static -disable-shared --with-pic --with-zlib={target_prefix}/lib ', # --with-pic per https://github.com/ffms/ffms2/issues/90
+	#'configure_options': '--host={target_host} --prefix={output_prefix}/ffms2_dll.installed --enable-static -disable-shared --with-pic --with-zlib={target_prefix}/lib ', # --with-pic per https://github.com/ffms/ffms2/issues/90
+	'configure_options': '--host={target_host} --prefix={output_prefix}/ffms2_dll.installed --disable-static -enable-shared --with-pic --with-zlib={output_prefix}/ffms2_dll.installed/lib ', # --with-pic per https://github.com/ffms/ffms2/issues/90
 	'run_pre_patch' : [
 		'cp -fv "Makefile.am" "Makefile.am.orig"',
 		'cp -fv "configure.ac" "configure.ac.orig"',
@@ -23,17 +28,36 @@
 		'diff -U 10 "configure.ac.orig" "configure.ac"  && echo "NO difference" || echo "YES differences!"',
 		# diff for the sed
 		'cp -fv "ffms2.pc.in" "ffms2.pc.in.orig"',
-		'sed -i "s/Libs.private.*/& -lstdc++/;s/Cflags.*/& -DFFMS_STATIC/" "ffms2.pc.in"',
+		#'sed -i "s/Libs.private.*/& -lstdc++/;s/Cflags.*/& -DFFMS_STATIC/" "ffms2.pc.in"',
 		'diff -U 10 "ffms2.pc.in.orig" "ffms2.pc.in"  && echo "NO difference" || echo "YES differences!"',
 		#
 		'if [ -f "./configure" ] ; then rm -fv ./configure ; fi',
 		'if [ ! -d "src/config" ] ; then mkdir -p "src/config" ; fi',
 		'autoreconf -fiv',
-		#'./configure --help=recursive',
+		'./configure --help=recursive',
 	],
 	'depends_on': [
-		'libzimg', 'bzip2','libffmpeg_extra',
+		'ffms2_libzimg',		# ok
+		'ffms2_xz',				# ok
+		'ffms2_zlib',			# ??? no produces a dll ???
+		#
+		#'ffms2_iconv',			# no fails to build shared
+		#'ffms2_bzip2',			# no does not install .la file
+		#'ffms2_lzma',			
+		#'ffms2_libxml2',
+		#'ffms2_libvpx',
+		#'ffms2_libaom',
+		#'ffms2_libxvid',
+		#'ffms2_libopus',
+		#'ffms2_libmp3lame',
+		#'ffms2_libvorbis',
+		#'ffms2_libtheora',
+		#'ffms2_libsoxr',
+		#'ffms2_librubberband',
+		#'ffms2_libwebp',
 	],
 	'update_check' : { 'type' : 'git', },
 	'_info' : { 'version' : 'git master', 'fancy_name' : 'ffms2_dll' },
 }
+
+
