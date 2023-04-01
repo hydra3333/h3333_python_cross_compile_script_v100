663a77bcffbc4430628be4b1421e9baf9a978dcf
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-04-02 01:33:53 +1030

    Add files via upload

--- packages/dependencies/python3_libs.py
+++ packages/dependencies/python3_libs.py
@@ -11,7 +11,7 @@
 		'sed -i.bak "s;_DEBUG = False;_DEBUG = True;g" "install_python_libs.py"',
 		'diff -U 5 install_python_libs.py.orig install_python_libs.py && echo "NO difference" || echo "YES differences!"',
 	],
-	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool VAPOURSYNTH_VERSION=R62 PYTHON_VERSION=3.11.12', # for vapoursynth R62
+	'build_options' : 'PREFIX={target_prefix} GENDEF={mingw_binpath}/gendef DLLTOOL={mingw_binpath}/{cross_prefix_bare}dlltool VAPOURSYNTH_VERSION=R62 PYTHON_VERSION=3.11.2', # for vapoursynth R62
 	## eg 
 	## make -j 4 PREFIX=/home/u/Desktop/_working/workdir/toolchain/x86_64-w64-mingw32/x86_64-w64-mingw32 GENDEF=/home/u/Desktop/_working/workdir/toolchain/x86_64-w64-mingw32/bin/gendef DLLTOOL=/home/u/Desktop/_working/workdir/toolchain/x86_64-w64-mingw32/bin/x86_64-w64-mingw32-dlltool PYTHON_VERSION=3.8.5
 	'update_check' : { 'type' : 'git', },
