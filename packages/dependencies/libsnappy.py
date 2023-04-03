6955497f1c7a66a79e6ba05cd16109f551f9ca10
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-04-03 17:33:46 +0930

    Add files via upload

--- packages/dependencies/libsnappy.py
+++ packages/dependencies/libsnappy.py
@@ -4,6 +4,7 @@
 	#'branch' : 'main',
 	'branch' : 'f603a020086da1115118eef206ce28a4a5829438',	# 2023.04.02 the commit immediately after this one breaks cross-compilation
 	'depth_git' : 0,
+	'rename_folder' : 'libsnappy',
 	'conf_system' : 'cmake',
 	'configure_options' : '. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DBUILD_SHARED_LIBS=OFF -DBUILD_BINARY=OFF -DSNAPPY_BUILD_BENCHMARKS=OFF -DSNAPPY_REQUIRE_AVX2=ON -DSNAPPY_REQUIRE_AVX=ON -DSNAPPY_BUILD_TESTS=OFF -DCMAKE_BUILD_TYPE=Release',
 	'run_post_install' : [
