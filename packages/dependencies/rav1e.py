06630ea71af231d8032d68d02ed0e8fced68e9c4
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-04-09 18:09:55 +0930

    Add files via upload

--- packages/dependencies/rav1e.py
+++ packages/dependencies/rav1e.py
@@ -3,10 +3,11 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/xiph/rav1e',
 	'depth_git': 1,
+	'rename_folder' : 'rav1e_lib',
 	'needs_configure' : False,
 	'needs_make_install' : False,
 	'build_system' : 'rust',
-    'cpu_count': '',
+    #'cpu_count': '',
 	'build_options' : 
         'cinstall -v '
         # '--manifest-path ./dolby_vision/Cargo.toml '
@@ -26,5 +27,5 @@
 		"TARGET_CXX": "{cross_prefix_bare}g++",
 		"CROSS_COMPILE": "1",
 	},
-	'_info' : { 'version' : None, 'fancy_name' : 'rav1e' },
+	'_info' : { 'version' : None, 'fancy_name' : 'rav1e_lib' },
 }
