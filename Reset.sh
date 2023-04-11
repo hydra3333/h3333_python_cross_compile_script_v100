b6d45d585d0fc6d338b5cff76038a33662bf7c8d
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-04-11 18:43:23 +0930

    Add files via upload

--- Reset.sh
+++ Reset.sh
@@ -6,7 +6,8 @@ set -x
 
 sudo apt -y update
 #sudo apt -y full-upgrade
-sudo pip3 --no-cache-dir install --upgrade --check-build-dependencies --force-reinstall --upgrade-strategy eager certifi # install latest certificatess for python requests.get
+#sudo pip3 --no-cache-dir install --upgrade --check-build-dependencies --force-reinstall --upgrade-strategy eager certifi # install latest certificatess for python requests.get
+sudo pip3 --no-cache-dir install --upgrade --force-reinstall --upgrade-strategy eager certifi # install latest certificatess for python requests.get
 #sudo pip3 install --upgrade pip-review
 #sudo pip3 check
 
