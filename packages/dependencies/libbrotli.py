7988f993583de239f8b5d674f79f20204cb68b3e
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-04-12 14:04:10 +0930

    Add files via upload

--- packages/dependencies/libbrotli.py
+++ packages/dependencies/libbrotli.py
@@ -2,7 +2,7 @@
 	'repo_type' : 'git',
 	'url' : 'https://github.com/google/brotli',
 	# 'depth_git': 0,
-	'rename_folder' : 'libbrotli',
+	'rename_folder' : 'libbrotli_GIT',
 	'conf_system' : 'cmake',
 	'build_system' : 'ninja',
 	'source_subfolder' : 'build',
@@ -13,9 +13,9 @@
 		'-DBROTLI_EMSCRIPTEN=false '
 	,
 	'run_post_install' : [
-		'sed -i.bak "s/-lbrotlienc/-lbrotlicommon -lbrotlienc/" "{pkg_config_path}/libbrotlienc.pc"', 
+		'sed -i.bak "s/-lbrotlienc/-lbrotlicommon -lbrotlienc -lbrotlicommon/" "{pkg_config_path}/libbrotlienc.pc"', 
 		'cat "{pkg_config_path}/libbrotlienc.pc"', 
-		'sed -i.bak "s/-lbrotlidec/-lbrotlicommon -lbrotlidec/" "{pkg_config_path}/libbrotlidec.pc"', 
+		'sed -i.bak "s/-lbrotlidec/-lbrotlicommon -lbrotlidec -lbrotlicommon/" "{pkg_config_path}/libbrotlidec.pc"', 
 		'cat "{pkg_config_path}/libbrotlidec.pc"', 
 		#'ls -al',
 	],
