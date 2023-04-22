8fb45d4fdcc45a43a5162fe5aba7f6bd38944309
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-04-22 14:11:43 +0930

    Add files via upload

--- packages/products/ffmpeg_testonly.py
+++ packages/products/ffmpeg_testonly.py
@@ -86,113 +86,113 @@
 		##
 		## Subtitle/OCR Libs:
 		##
-		'--disable-libass '			# 2021.02.03
+		#'--disable-libass '			# 2021.02.03
 		##
 		## Image libs
 		##
-		'--enable-libwebp '
+		#'--enable-libwebp '
 		##
 		## HW Decoders
 		##
-		'--enable-ffnvcodec '
-		'--enable-cuvid '
-		'--enable-cuda-llvm '
-		'--disable-opengl '		# 2021.02.03
-		'--enable-d3d11va '
-		'--enable-nvenc '
-		'--enable-nvdec '
-		'--enable-dxva2 '
-		'--disable-libmfx '			# 2021.02.03
-		'--disable-amf '			# 2021.02.03
-		'--disable-opencl '			# 2021.02.03
-		'--enable-vulkan --enable-libglslang '  # ERROR: libshaderc and libglslang are mutually exclusive, if in doubt, disable libglslang
-			'--enable-filter=scale_vulkan '
-			'--enable-filter=avgblur_vulkan '
-			'--enable-filter=chromaber_vulkan '
-			'--enable-filter=overlay_vulkan '
-			'--enable-filter=blend_vulkan '
-			'--enable-filter=flip_vulkan '
-			'--enable-filter=gblur_vulkan '
-			'--enable-filter=hflip_vulkan '
-			'--enable-filter=transpose_vulkan '
-			'--enable-filter=vflip_vulkan '
-		'--disable-libplacebo '
-		'--extra-libs="-lpsapi -liconv -lssp" '#  add  -lssp for -fstack-protector-all, # 2021.04.11 removed -lintl  (ex gettext) #2020.10.12 moved here from ffmpeg_extra_config
+		#'--enable-ffnvcodec '
+		#'--enable-cuvid '
+		#'--enable-cuda-llvm '
+		#'--disable-opengl '		# 2021.02.03
+		#'--enable-d3d11va '
+		#'--enable-nvenc '
+		#'--enable-nvdec '
+		#'--enable-dxva2 '
+		#'--disable-libmfx '			# 2021.02.03
+		#'--disable-amf '			# 2021.02.03
+		#'--disable-opencl '			# 2021.02.03
+		#'--enable-vulkan --enable-libglslang '  # ERROR: libshaderc and libglslang are mutually exclusive, if in doubt, disable libglslang
+		#	'--enable-filter=scale_vulkan '
+		#	'--enable-filter=avgblur_vulkan '
+		#	'--enable-filter=chromaber_vulkan '
+		#	'--enable-filter=overlay_vulkan '
+		#	'--enable-filter=blend_vulkan '
+		#	'--enable-filter=flip_vulkan '
+		#	'--enable-filter=gblur_vulkan '
+		#	'--enable-filter=hflip_vulkan '
+		#	'--enable-filter=transpose_vulkan '
+		#	'--enable-filter=vflip_vulkan '
+		#'--disable-libplacebo '
+		#'--extra-libs="-lpsapi -liconv -lssp" '#  add  -lssp for -fstack-protector-all, # 2021.04.11 removed -lintl  (ex gettext) #2020.10.12 moved here from ffmpeg_extra_config
 		##
 		## nonfree
 		##
-		'--enable-nonfree '
-		'--enable-libfdk-aac '
-		'--disable-decklink '		# 2021.02.03
+		#'--enable-nonfree '
+		#'--enable-libfdk-aac '
+		#'--disable-decklink '		# 2021.02.03
 		##
 		## nonfree extra_config
 		##
-		'--enable-libtwolame '
-		'--enable-libzvbi '
-		'--enable-libgsm '
-		'--enable-libopencore-amrnb '
-		'--enable-libopencore-amrwb '
-		'--enable-libvo-amrwbenc '
-		'--enable-libsnappy '
-		'--disable-frei0r '				# 2021.02.03
-		'--enable-libsrt '
-		'--disable-libbs2b ' #'--enable-libbs2b #  2022.04.22 commented out
-		'--enable-libilbc '
-		'--disable-libgme '		# 2021.02.03
-		'--disable-libflite '	# 2021.02.03
-		'--enable-sdl2 ' # 2020.05.13 renabled. Removed '--disable-sdl2 ' # 2020.05.13 removed SDL2 '--enable-sdl2 '
-		'--enable-libmysofa '
-		'--enable-libvidstab '
-		'--disable-libmodplug '	# 2021.02.03
-		'--disable-schannel '
-		'--extra-cflags="-DLIBTWOLAME_STATIC -lssp" '
-		'--extra-cflags="-DMODPLUG_STATIC -lssp"  ' 	# 2021.02.03
-		'--extra-cflags="-DLIBXML_STATIC -lssp" '
-		'--extra-cflags="-DGLIB_STATIC_COMPILATION -lssp" '	# 2021.02.03
-		'--extra-libs=" -lssp " '
+		#'--enable-libtwolame '
+		#'--enable-libzvbi '
+		#'--enable-libgsm '
+		#'--enable-libopencore-amrnb '
+		#'--enable-libopencore-amrwb '
+		#'--enable-libvo-amrwbenc '
+		#'--enable-libsnappy '
+		#'--disable-frei0r '				# 2021.02.03
+		#'--enable-libsrt '
+		#'--disable-libbs2b ' #'--enable-libbs2b #  2022.04.22 commented out
+		#'--enable-libilbc '
+		#'--disable-libgme '		# 2021.02.03
+		#'--disable-libflite '	# 2021.02.03
+		#'--enable-sdl2 ' # 2020.05.13 renabled. Removed '--disable-sdl2 ' # 2020.05.13 removed SDL2 '--enable-sdl2 '
+		#'--enable-libmysofa '
+		#'--enable-libvidstab '
+		#'--disable-libmodplug '	# 2021.02.03
+		#'--disable-schannel '
+		#'--extra-cflags="-DLIBTWOLAME_STATIC -lssp" '
+		#'--extra-cflags="-DMODPLUG_STATIC -lssp"  ' 	# 2021.02.03
+		#'--extra-cflags="-DLIBXML_STATIC -lssp" '
+		#'--extra-cflags="-DGLIB_STATIC_COMPILATION -lssp" '	# 2021.02.03
+		#'--extra-libs=" -lssp " '
 		,
 	'depends_on' : [ 
-		'gnutls_testonly',
-		'bzip2',
-		'zlib',
-		'lzma',
-		'python3_libs',
-		'vapoursynth_libs',
-		'rtmpdump',		# 2021.08.27
-		'librist',      # 2021.08.27
-		'iconv',
-		'libzimg',
-		'libxml2',
-		'libx264_testonly',	# 2020.06.09 only the x264 package, not the dependency depends on libffmpeg_extra being built first
-		'libx265_multibit',
-		'libvpx',
-		'libdav1d',
-		'xvidcore', # was 'libxvid',
-		'libopus',
-		'liblame',
-		'libogg',
-		'libvorbis',
-		'libtheora',
-		'libspeex',
-		'libsoxr_testonly',
-		'rubberband',
-		'libwebp',
-		'nv-codec-headers',
-		'vulkan_from_windows_dll', # 'vulkan_loader',
-		'spirv-headers', 'spirv-cross', 'spirv-tools', 'libplacebo', 'shaderc',  # all these go together
-		'avisynth_plus_headers',
-		'sdl2', # 2020.05.13 re-enabled sdl2 # 2020.05.13 remove sdl2
-		'twolame',
-		'zvbi',
-		'libgsm',
-		'opencore-amr',
-		'vo-amrwbenc',
-		'libsnappy',
-		'srt_testonly',
-		'libilbc',
-		'libmysofa_testonly',
-		'vidstab',
-		'fftw3',
+	#	'gnutls_3333',
+	#	'bzip2',
+	#	'zlib',
+	#	'lzma',
+	#	'python3_libs',
+	#	'vapoursynth_libs',
+	#	'rtmpdump',		# 2021.08.27
+	#	'librist',      # 2021.08.27
+	#	'iconv',
+	#	'libzimg',
+	#	'libxml2',
+	#	'libx264_3333',	# 2020.06.09 only the x264 package, not the dependency depends on libffmpeg_extra being built first
+	#	'libx265_multibit',
+	#	'libvpx',
+	#	'libdav1d',
+	#	'xvidcore', # was 'libxvid',
+	#	'libopus',
+	#	'liblame',
+	#	'libogg',
+	#	'libvorbis',
+	#	'libtheora',
+	#	'libspeex',
+	#	'libsoxr_3333',
+	#	'rubberband',
+	#	'libwebp',
+	#	'nv-codec-headers',
+	#	'vulkan_from_windows_dll', # 'vulkan_loader',
+	#	'spirv-headers', 'spirv-cross', 'spirv-tools', 'libplacebo', 'shaderc',  # all these go together
+	#	'avisynth_plus_headers',
+	#	'sdl2', # 2020.05.13 re-enabled sdl2 # 2020.05.13 remove sdl2
+	#	'twolame',
+	#	'zvbi',
+	#	'libgsm',
+	#	'opencore-amr',
+	#	'vo-amrwbenc',
+	#	'libsnappy',
+	#	'srt_3333',
+	#	'libilbc',
+	#	'libmysofa_3333',
+	#	'vidstab',
+	#	'fftw3',
 		'fdk_aac',
 	],
 	'update_check' : { 'type' : 'git', },
