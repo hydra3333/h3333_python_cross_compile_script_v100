5e0691655d18ee01bac9ba4de7b31cc5bf369e09
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-04-23 10:12:04 +0930

    Add files via upload

--- packages/dependencies/bzip2.py
+++ packages/dependencies/bzip2.py
@@ -1,10 +1,7 @@
-#type:ignore
 {
 	'repo_type' : 'git',
     'url': 'https://github.com/libarchive/bzip2.git',
     'source_subfolder': '_build',
-	'conf_system' : 'cmake',
-	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_STATIC_LIB=ON -DENABLE_SHARED_LIB=OFF -DENABLE_LIB_ONLY=ON',
 	'regex_replace': {
 		'post_patch': [
 			{
@@ -12,8 +9,11 @@
 				1: r'ARCHIVE_OUTPUT_NAME bz2)',
 				'in_file': '../CMakeLists.txt'
 			},
+
 		],
 	},
-	'update_check' : { 'url' : 'ftp://sourceware.org/pub/bzip2/', 'type' : 'ftpindex', 'regex' : r'bzip2-(?P<version_num>[\d.]+)\.tar\.gz' },
-	'_info' : { 'version' : '1.0.8', 'fancy_name' : 'BZip2 (library)' },
+	'conf_system' : 'cmake',
+	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DENABLE_STATIC_LIB=ON -DENABLE_SHARED_LIB=OFF -DENABLE_LIB_ONLY=ON',
+	'update_check' : { 'type' : 'git', },
+	'_info' : { 'version' : 'git (master)', 'fancy_name' : 'BZip2 (library)' },
 }
