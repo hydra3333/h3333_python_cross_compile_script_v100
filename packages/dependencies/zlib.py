708ed100e08ae1419ca2f6d0b14ea913484d1789
hydra3333 <10749357+hydra3333@users.noreply.github.com>
2023-10-01 15:39:32 +1030

    Add files via upload

--- packages/dependencies/zlib.py
+++ packages/dependencies/zlib.py
@@ -1,6 +1,7 @@
 { # 2019.12.13
 	'repo_type' : 'git',
 	'url' : 'https://github.com/madler/zlib.git',
+	'branch' : 'master',
 	'conf_system' : 'cmake',
 	'source_subfolder' : '_build',
 	'configure_options' : '.. {cmake_prefix_options} -DCMAKE_INSTALL_PREFIX={target_prefix} -DINSTALL_PKGCONFIG_DIR="{target_prefix}/lib/pkgconfig" '
